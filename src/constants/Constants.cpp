18586a556c117ed6a56df71985fd2efbc0abed7d
ggarrett13 <g.h.garrett13@gmail.com>
2019-05-21 14:19:13 +0200

    First meeting (fun with flags)

---
+++ src/constants/Constants.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ggarrett on 21-5-19.
+//
+
+#include <boost/python.hpp>
+#include "Tudat/Astrodynamics/BasicAstrodynamics/physicalConstants.h"
+
+using namespace boost::python;
+using namespace tudat::physical_constants;
+
+BOOST_PYTHON_MODULE(constants)
+        {
+                boost::python::scope().attr("JULIAN_DAY") = JULIAN_DAY;
+        }
+
