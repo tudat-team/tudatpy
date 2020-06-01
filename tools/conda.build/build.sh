10a0bab177b97998eadc813ea64da464f88e301d
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-01 18:07:39 +0200

    feat(conda.recipe) Added MVP for conda installation.

--- tools/conda.build/build.sh
+++ tools/conda.build/build.sh
@@ -2,21 +2,31 @@
 
 mkdir build
 cd build
+export TUDATPY_BUILD_DIR=`pwd`
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    export ENABLE_TESTS=no
-else
-    LDFLAGS="-lrt ${LDFLAGS}"
-    export ENABLE_TESTS=yes
-fi
+#git clone https://github.com/pybind/pybind11.git
+#cd pybind11
+#git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
+#mkdir build
+#cd build
+#pwd
+#
+#cmake \
+#    -DPYBIND11_TEST=NO \
+#    -DCMAKE_INSTALL_PREFIX=$TUDATPY_BUILD_DIR \
+#    -DCMAKE_PREFIX_PATH=$TUDATPY_BUILD_DIR \
+#    ..
+#
+#make install
+#cd ../..
 
 cmake \
-    -DCMAKE_CXX_STANDARD=17 \
+    -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPREFIX=$PREFIX \
-    -DUSE_TUDAT_EXAMPLE_APPLICATIONS=off \
+    -DCMAKE_CXX_STANDARD=17 \
+    -Dpybind11_DIR=$DCMAKE_PREFIX_PATH/share/cmake/pybind11/ \
     ..
 
 make -j2
