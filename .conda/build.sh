e63af68720dc7a1fc527a5018c04fc16fe52b456
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-07 14:32:33 +0200

    feat(travis-ci) Added initial travis-ci setup for macos.

---
+++ .conda/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Core deps.
+sudo apt-get install build-essential cmake
+
+mkdir build
+
+cd build
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    export ENABLE_TESTS=no
+else
+    LDFLAGS="-lrt ${LDFLAGS}"
+    export ENABLE_TESTS=yes
+fi
+
+cmake \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPREFIX=$PREFIX \
+    -DTUDAT_CONDA_BUILD=on \
+    -DTUDAT_BUILD_TUDAT_TUTORIALS=off \
+    -DTUDAT_BUILD_WITH_SOFA_INTERFACE=on \
+    -DTUDAT_BUILD_WITH_SPICE_INTERFACE=on \
+    ..
+
+make  # -j2: Kills the remote build server.
+
+make install
