f9da52ad200c34df4d4f3c2d85e49af7994c020d
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-07 15:49:49 +0200

    feat(travis-ci) Added initial travis-ci setup for macos.

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
-# Core deps.
-sudo apt-get install build-essential cmake
-
 mkdir build
-
 cd build
+export TUDATPY_BUILD_DIR=`pwd`
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    export ENABLE_TESTS=no
-else
-    LDFLAGS="-lrt ${LDFLAGS}"
-    export ENABLE_TESTS=yes
-fi
+git clone https://github.com/pybind/pybind11.git
+cd pybind11
+git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
+mkdir build
+cd build
+pwd
+cmake \
+    -DPYBIND11_TEST=NO \
+    -DCMAKE_INSTALL_PREFIX=$TUDATPY_BUILD_DIR \
+    -DCMAKE_PREFIX_PATH=$TUDATPY_BUILD_DIR \
+    ..
+make install
+cd ../..
 
 cmake \
-    -DCMAKE_CXX_STANDARD=17 \
+    -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPREFIX=$PREFIX \
-    -DTUDAT_CONDA_BUILD=on \
-    -DTUDAT_BUILD_TUDAT_TUTORIALS=off \
-    -DTUDAT_BUILD_WITH_SOFA_INTERFACE=on \
-    -DTUDAT_BUILD_WITH_SPICE_INTERFACE=on \
+    -DCMAKE_CXX_STANDARD=17 \
     -DTUDATPY_CONDA_BUILD=on \
+    -Dpybind11_DIR="$TUDATPY_BUILD_DIR/share/cmake/pybind11/" \
     ..
 
-make  # -j2: Kills the remote build server.
+make -j2
 
 make install
