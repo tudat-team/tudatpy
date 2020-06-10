562f76cc1bcfd70f3578f8719d0cbab045d0a05b
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-10 06:53:43 +0200

    bugfix(conda) fied paths.hpp and config.hpp in install

---
+++ .conda/__run_test_.py
@@ -0,0 +1,5 @@
+import tudatpy
+
+# python3 -c "import tudatpy.kernel.constants as c; print(c.SPEED_OF_LIGHT)"
+# python3 -c "import tudatpy.kernel.spice_interface as spice; spice.load_standard_spice_kernels()"
+# python3 -c "import tudatpy.kernel.io as io; print(io.get_tudat_data_path())"
