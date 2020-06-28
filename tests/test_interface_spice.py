62c47b1fd1b6ad5978a5d5e76096e53d73c6e6e0
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-28 19:31:51 +0200

    Fixed outdated test

--- tests/test_interface_spice.py
+++ tests/test_interface_spice.py
@@ -1,5 +1,5 @@
 import tudatpy.kernel.spice_interface as spice_interface
-from tudatpy import load_standard_spice_kernels
+# from tudatpy import load_standard_spice_kernels
 # import pytest
 # from pytest_bdd import scenario, given, when, then
 
@@ -7,8 +7,8 @@ from tudatpy import load_standard_spice_kernels
 # EXAMPLES: https://www.jetbrains.com/help/pycharm/pytest.html
 
 def test_load_default_kernels():
-    load_standard_spice_kernels()
-    # spice_interface.load_standard_kernels()
+    # load_standard_spice_kernels()
+    spice_interface.load_standard_kernels()
 
 
 def test_clear_spice_kernels():
