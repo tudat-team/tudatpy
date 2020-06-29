91f808e92185a719784d1f168fc98525d9ca223f
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-29 22:14:51 +0200

    Moved paths to separate module.
    - bumped version for tudat osx path fix attempt

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -6,11 +6,11 @@ def test_no_null_bytes():
     - https://github.com/tudat-team/tudatpy-feedstock/issues/1
     - https://gitter.im/conda-forge/conda-forge.github.io/archives/2019/01/22
     """
-    assert '\x00' not in kernel.get_tudat_path()
-    assert '\x00' not in kernel.get_tudat_data_path()
-    assert '\x00' not in kernel.get_ephemeris_data_files_path()
-    assert '\x00' not in kernel.get_earth_orientation_data_files_path()
-    assert '\x00' not in kernel.get_spice_kernel_path()
-    assert '\x00' not in kernel.get_atmosphere_tables_path()
-    assert '\x00' not in kernel.get_gravity_models_path()
-    assert '\x00' not in kernel.get_space_weather_data_path()
+    assert '\x00' not in kernel.paths.get_resource_path()
+    assert '\x00' not in kernel.paths.get_ephemeris_path()
+    assert '\x00' not in kernel.paths.get_earth_orientation_path()
+    assert '\x00' not in kernel.paths.get_quadrature_path()
+    assert '\x00' not in kernel.paths.get_spice_kernel_path()
+    assert '\x00' not in kernel.paths.get_atmosphere_tables_path()
+    assert '\x00' not in kernel.paths.get_gravity_models_path()
+    assert '\x00' not in kernel.paths.get_space_weather_data_path()
--- tudatpy/kernel/kernel.cpp
+++ tudatpy/kernel/kernel.cpp
@@ -1,129 +1,121 @@
 #include <pybind11/pybind11.h>
 #include <tudat/config.hpp>
-#include <tudat/resource/resource.h>
 #include <tudat/paths.hpp>
+#include <tudat/resource/resource.h>
 
-#include "expose_constants.h"
-#include "expose_root_finders.h"
-#include "expose_interpolators.h"
-#include "expose_spice_interface.h"
-#include "expose_ephemerides.h"
-#include "expose_reference_frames.h"
 #include "expose_aerodynamics.h"
 #include "expose_basic_astrodynamics.h"
+#include "expose_constants.h"
+#include "expose_ephemerides.h"
 #include "expose_gravitation.h"
+#include "expose_interpolators.h"
+#include "expose_io.h"
 #include "expose_numerical_integrators.h"
 #include "expose_orbital_element_conversions.h"
 #include "expose_propagators.h"
+#include "expose_reference_frames.h"
+#include "expose_root_finders.h"
 #include "expose_simulation_setup.h"
+#include "expose_spice_interface.h"
 #include "expose_unit_tests.h"
-#include "expose_io.h"
 
 #include "tudat/astro/aerodynamics/tests/testApolloCapsuleCoefficients.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE (kernel, m) {
+PYBIND11_MODULE(kernel, m) {
 
-    // Disable automatic function signatures in the docs.
-    // NOTE: the 'options' object needs to stay alive
-    // throughout the whole definition of the module.
-    py::options options;
-    options.disable_function_signatures();
-    options.enable_user_defined_docstrings();
+  // Disable automatic function signatures in the docs.
+  // NOTE: the 'options' object needs to stay alive
+  // throughout the whole definition of the module.
+  py::options options;
+  options.disable_function_signatures();
+  options.enable_user_defined_docstrings();
 
-    // Export the tudatBundle version.
-    m.attr("_tudat_version_major") = TUDAT_VERSION_MAJOR;
-    m.attr("_tudat_version_minor") = TUDAT_VERSION_MINOR;
-    m.attr("_tudat_version_patch") = TUDAT_VERSION_PATCH;
+  // Export the tudatBundle version.
+  m.attr("_tudat_version_major") = TUDAT_VERSION_MAJOR;
+  m.attr("_tudat_version_minor") = TUDAT_VERSION_MINOR;
+  m.attr("_tudat_version_patch") = TUDAT_VERSION_PATCH;
 
-    // constants
-    auto constants = m.def_submodule("constants");
-    tudatpy::expose_constants(constants);
+  // constants
+  auto constants = m.def_submodule("constants");
+  tudatpy::expose_constants(constants);
 
-    // root_finders module
-    auto root_finders = m.def_submodule("root_finders");
-    tudatpy::expose_root_finders(root_finders);
+  // root_finders module
+  auto root_finders = m.def_submodule("root_finders");
+  tudatpy::expose_root_finders(root_finders);
 
-    // interpolators
-    auto interpolators = m.def_submodule("interpolators");
-    tudatpy::expose_interpolators(interpolators);
+  // interpolators
+  auto interpolators = m.def_submodule("interpolators");
+  tudatpy::expose_interpolators(interpolators);
 
-    // spice_interface module
-    auto spice_interface = m.def_submodule("spice_interface");
-    tudatpy::expose_spice_interface(spice_interface);
+  // spice_interface module
+  auto spice_interface = m.def_submodule("spice_interface");
+  tudatpy::expose_spice_interface(spice_interface);
 
-    // ephemerides module
-    auto ephemerides = m.def_submodule("ephemerides");
-    tudatpy::expose_ephemerides(ephemerides);
+  // ephemerides module
+  auto ephemerides = m.def_submodule("ephemerides");
+  tudatpy::expose_ephemerides(ephemerides);
 
-    // reference_frames module
-    auto reference_frames = m.def_submodule("reference_frames");
-    tudatpy::expose_reference_frames(reference_frames);
+  // reference_frames module
+  auto reference_frames = m.def_submodule("reference_frames");
+  tudatpy::expose_reference_frames(reference_frames);
 
-    // aerodynamics module
-    auto aerodynamics = m.def_submodule("aerodynamics");
-    tudatpy::expose_aerodynamics(aerodynamics);
+  // aerodynamics module
+  auto aerodynamics = m.def_submodule("aerodynamics");
+  tudatpy::expose_aerodynamics(aerodynamics);
 
-    // basic_astrodynamics module
-    auto basic_astrodynamics = m.def_submodule("basic_astrodynamics");
-    tudatpy::expose_basic_astrodynamics(basic_astrodynamics);
+  // basic_astrodynamics module
+  auto basic_astrodynamics = m.def_submodule("basic_astrodynamics");
+  tudatpy::expose_basic_astrodynamics(basic_astrodynamics);
 
-    // gravitation module
-    auto gravitation = m.def_submodule("gravitation");
-    tudatpy::expose_gravitation(gravitation);
+  // gravitation module
+  auto gravitation = m.def_submodule("gravitation");
+  tudatpy::expose_gravitation(gravitation);
 
-    // numerical_integrators module
-    auto numerical_integrators = m.def_submodule("numerical_integrators");
-    tudatpy::expose_numerical_integrators(numerical_integrators);
+  // numerical_integrators module
+  auto numerical_integrators = m.def_submodule("numerical_integrators");
+  tudatpy::expose_numerical_integrators(numerical_integrators);
 
-    // orbital_element_conversions module
-    auto orbital_element_conversions = m.def_submodule("orbital_element_conversions");
-    tudatpy::expose_orbital_element_conversions(orbital_element_conversions);
+  // orbital_element_conversions module
+  auto orbital_element_conversions =
+      m.def_submodule("orbital_element_conversions");
+  tudatpy::expose_orbital_element_conversions(orbital_element_conversions);
 
-    // propagators module
-    auto propagators = m.def_submodule("propagators");
-    tudatpy::expose_propagators(propagators);
+  // propagators module
+  auto propagators = m.def_submodule("propagators");
+  tudatpy::expose_propagators(propagators);
 
-    // simulation_setup module
-    auto simulation_setup = m.def_submodule("simulation_setup");
-    tudatpy::expose_simulation_setup(simulation_setup);
+  // simulation_setup module
+  auto simulation_setup = m.def_submodule("simulation_setup");
+  tudatpy::expose_simulation_setup(simulation_setup);
 
-    // io module
-    auto io = m.def_submodule("io");
-    tudatpy::expose_io(io);
+  // io module
+  auto io = m.def_submodule("io");
+  tudatpy::expose_io(io);
 
-    // unit_tests module
-    auto unit_tests = m.def_submodule("unit_tests");
-    tudatpy::expose_unit_tests(unit_tests);
+  // unit_tests module
+  auto unit_tests = m.def_submodule("unit_tests");
+  tudatpy::expose_unit_tests(unit_tests);
 
-    unit_tests.def("get_apollo_coefficient_interface",
-                   tudat::unit_tests::getApolloCoefficientInterface,
-                   "<no doc>");
+  // paths module TODO: Move to external source file.
+  auto paths = m.def_submodule("paths");
+  paths.def("get_resource_path", &tudat::paths::get_resource_path);
+  paths.def("get_ephemeris_path", &tudat::paths::getEphemerisDataFilesPath);
+  paths.def("get_earth_orientation_path", &tudat::paths::getEarthOrientationDataFilesPath);
+  paths.def("get_quadrature_path", &tudat::paths::getQuadratureDataPath);
+  paths.def("get_spice_kernel_path", &tudat::paths::getSpiceKernelPath);
+  paths.def("get_atmosphere_tables_path", &tudat::paths::getAtmosphereTablesPath);
+  paths.def("get_gravity_models_path", &tudat::paths::getGravityModelsPath);
+  paths.def("get_space_weather_path", &tudat::paths::getSpaceWeatherDataPath);
 
-    // Tudat path information. Maybe move to path module or external cpp file.
+  unit_tests.def("get_apollo_coefficient_interface",
+                 &tudat::unit_tests::getApolloCoefficientInterface, "<no doc>");
 
-    m.def("get_tudat_path", &tudat::paths::get_tudat_path);
-    m.def("_get_tudat_path_paths", &tudat::paths::get_tudat_path);
-    m.def("_get_tudat_path_resource", &tudat::paths::get_resources_path);
-
-
-    m.def("get_tudat_data_path", &tudat::paths::get_tudat_data_path);
-    m.def("get_ephemeris_data_files_path", &tudat::paths::getEphemerisDataFilesPath);
-    m.def("get_earth_orientation_data_files_path", &tudat::paths::getEarthOrientationDataFilesPath);
-
-    m.def("get_spice_kernel_path", &tudat::paths::getSpiceKernelPath);
-    m.def("_get_spice_kernel_path_resource", &tudat::paths::get_spice_kernels_path);
-    m.def("_get_spice_kernel_path_paths", &tudat::paths::getSpiceKernelPath);
-
-    m.def("get_atmosphere_tables_path", &tudat::paths::getAtmosphereTablesPath);
-    m.def("get_gravity_models_path", &tudat::paths::getGravityModelsPath);
-    m.def("get_space_weather_data_path", &tudat::paths::getSpaceWeatherDataPath);
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
+  m.attr("__version__") = VERSION_INFO;
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
-
 }
