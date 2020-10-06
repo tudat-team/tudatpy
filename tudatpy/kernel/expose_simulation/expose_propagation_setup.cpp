49dc16152139605216ea9b6a8687ef297e30bc7a
Dominic Dirkx <d.dirkx@tudelft.nl>
2020-10-06 11:48:09 +0200

    Adding dependent variable exposure in new setup

--- tudatpy/kernel/expose_simulation/expose_propagation_setup.cpp
+++ tudatpy/kernel/expose_simulation/expose_propagation_setup.cpp
@@ -28,6 +28,113 @@ namespace tni = tudat::numerical_integrators;
 
 namespace tudatpy {
 
+
+void expose_dependent_variable_setup(py::module &m) {
+
+
+    py::enum_<tp::PropagationDependentVariables>(m, "PropagationDependentVariables")
+        // C++ legacy variable names.
+        .value("mach_number_type", tp::PropagationDependentVariables::mach_number_dependent_variable)
+        .value("altitude_type", tp::PropagationDependentVariables::altitude_dependent_variable)
+        .value("airspeed_type", tp::PropagationDependentVariables::airspeed_dependent_variable)
+        .value("local_density_type", tp::PropagationDependentVariables::local_density_dependent_variable)
+        .value("relative_speed_type", tp::PropagationDependentVariables::relative_speed_dependent_variable)
+        .value("relative_position_type", tp::PropagationDependentVariables::relative_position_dependent_variable)
+        .value("relative_distance_type", tp::PropagationDependentVariables::relative_distance_dependent_variable)
+        .value("relative_velocity_type", tp::PropagationDependentVariables::relative_velocity_dependent_variable)
+        .value("radiation_pressure_type", tp::PropagationDependentVariables::radiation_pressure_dependent_variable)
+        .value("total_acceleration_norm_type", tp::PropagationDependentVariables::total_acceleration_norm_dependent_variable)
+        .value("single_acceleration_norm_type", tp::PropagationDependentVariables::single_acceleration_norm_dependent_variable)
+        .value("total_acceleration_type", tp::PropagationDependentVariables::total_acceleration_dependent_variable)
+        .value("single_acceleration_type", tp::PropagationDependentVariables::single_acceleration_dependent_variable)
+        .value("aerodynamic_force_coefficients_type", tp::PropagationDependentVariables::aerodynamic_force_coefficients_dependent_variable)
+        .value("aerodynamic_moment_coefficients_type", tp::PropagationDependentVariables::aerodynamic_moment_coefficients_dependent_variable)
+        .value("rotation_matrix_to_body_fixed_frame_type", tp::PropagationDependentVariables::rotation_matrix_to_body_fixed_frame_variable)
+        .value("intermediate_aerodynamic_rotation_matrix_type", tp::PropagationDependentVariables::intermediate_aerodynamic_rotation_matrix_variable)
+        .value("relative_body_aerodynamic_orientation_angle_type", tp::PropagationDependentVariables::relative_body_aerodynamic_orientation_angle_variable)
+        .value("body_fixed_airspeed_based_velocity_type", tp::PropagationDependentVariables::body_fixed_airspeed_based_velocity_variable)
+        .value("total_aerodynamic_g_load_type", tp::PropagationDependentVariables::total_aerodynamic_g_load_variable)
+        .value("stagnation_point_heat_flux_type", tp::PropagationDependentVariables::stagnation_point_heat_flux_dependent_variable)
+        .value("local_temperature_type", tp::PropagationDependentVariables::local_temperature_dependent_variable)
+        .value("geodetic_latitude_type", tp::PropagationDependentVariables::geodetic_latitude_dependent_variable)
+        .value("control_surface_deflection_type", tp::PropagationDependentVariables::control_surface_deflection_dependent_variable)
+        .value("total_mass_rate_type", tp::PropagationDependentVariables::total_mass_rate_dependent_variables)
+        .value("lvlh_to_inertial_frame_rotation_type", tp::PropagationDependentVariables::lvlh_to_inertial_frame_rotation_dependent_variable)
+        .value("periapsis_altitude_type", tp::PropagationDependentVariables::periapsis_altitude_dependent_variable)
+        .value("total_torque_norm_type", tp::PropagationDependentVariables::total_torque_norm_dependent_variable)
+        .value("single_torque_norm_type", tp::PropagationDependentVariables::single_torque_norm_dependent_variable)
+        .value("total_torque_type", tp::PropagationDependentVariables::total_torque_dependent_variable)
+        .value("single_torque_type", tp::PropagationDependentVariables::single_torque_dependent_variable)
+        .value("body_fixed_groundspeed_based_velocity_type", tp::PropagationDependentVariables::body_fixed_groundspeed_based_velocity_variable)
+        .value("keplerian_state_type", tp::PropagationDependentVariables::keplerian_state_dependent_variable)
+        .value("modified_equinocial_state_type", tp::PropagationDependentVariables::modified_equinocial_state_dependent_variable)
+        .value("spherical_harmonic_acceleration_terms_type", tp::PropagationDependentVariables::spherical_harmonic_acceleration_terms_dependent_variable)
+        .value("body_fixed_relative_cartesian_position_type", tp::PropagationDependentVariables::body_fixed_relative_cartesian_position)
+        .value("body_fixed_relative_spherical_position_type", tp::PropagationDependentVariables::body_fixed_relative_spherical_position)
+        .value("total_gravity_field_variation_acceleration_type", tp::PropagationDependentVariables::total_gravity_field_variation_acceleration)
+        .value("single_gravity_field_variation_acceleration_type", tp::PropagationDependentVariables::single_gravity_field_variation_acceleration)
+        .value("single_gravity_field_variation_acceleration_terms_type", tp::PropagationDependentVariables::single_gravity_field_variation_acceleration_terms)
+        .value("acceleration_partial_wrt_body_translational_state_type", tp::PropagationDependentVariables::acceleration_partial_wrt_body_translational_state)
+        .value("local_dynamic_pressure_type", tp::PropagationDependentVariables::local_dynamic_pressure_dependent_variable)
+        .value("local_aerodynamic_heat_rate_type", tp::PropagationDependentVariables::local_aerodynamic_heat_rate_dependent_variable)
+        .export_values();
+
+
+    py::class_<tp::VariableSettings,
+               std::shared_ptr<tp::VariableSettings>>
+        VariableSettings_(m, "VariableSettings");
+
+    py::class_<tp::SingleDependentVariableSaveSettings,
+               std::shared_ptr<tp::SingleDependentVariableSaveSettings>,
+               tp::VariableSettings>(m, "SingleDependentVariableSaveSettings")
+        .def(py::init<
+                 const tp::PropagationDependentVariables,
+                 const std::string &,
+                 const std::string &,
+                 const int>(),
+             py::arg("dependent_variable_type"),
+             py::arg("associated_body"),
+             py::arg("secondary_body") = "",
+             py::arg("component_idx") = -1);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // astro/basic_astro/accelerationModels.h   TODO: This needs to be moved in tudat source.
+    //////////////////////////////////////////////////////////////////////////////
+    py::class_<
+        tp::SingleAccelerationDependentVariableSaveSettings,
+        std::shared_ptr<tp::SingleAccelerationDependentVariableSaveSettings>,
+        tp::SingleDependentVariableSaveSettings>(m, "SingleAccelerationDependentVariableSaveSettings")
+        .def(py::init<
+                 const tudat::basic_astrodynamics::AvailableAcceleration,
+                 const std::string &,
+                 const std::string &,
+                 const bool,
+                 const int>(),
+             py::arg("acceleration_model_type"),
+             py::arg("body_undergoing_acceleration"),
+             py::arg("body_exerting_acceleration"),
+             py::arg("use_norm") = 0,
+             py::arg("component_index") = -1);
+
+
+    m.def("mach_number",
+          &tp::machNumberDependentVariable,
+          py::arg("associated_body"),
+          py::arg("central_body"));
+
+
+    m.def("altitude",
+          &tp::altitudeDependentVariable,
+          py::arg("associated_body"),
+          py::arg("central_body"));
+
+    m.def("single_acceleration",
+          &tp::singleAccelerationDependentVariable,
+          py::arg("acceleration_type"),
+          py::arg("body_undergoing_acceleration"),
+          py::arg("body_exerting_acceleration"));
+
+}
 void expose_acceleration_setup(py::module &m) {
 
     /*
@@ -843,89 +950,6 @@ void expose_propagation_setup(py::module &m) {
 
 
 
-  py::enum_<tp::PropagationDependentVariables>(m, "PropagationDependentVariables")
-      // C++ legacy variable names.
-      .value("mach_number", tp::PropagationDependentVariables::mach_number_dependent_variable)
-      .value("altitude", tp::PropagationDependentVariables::altitude_dependent_variable)
-      .value("airspeed", tp::PropagationDependentVariables::airspeed_dependent_variable)
-      .value("local_density", tp::PropagationDependentVariables::local_density_dependent_variable)
-      .value("relative_speed", tp::PropagationDependentVariables::relative_speed_dependent_variable)
-      .value("relative_position", tp::PropagationDependentVariables::relative_position_dependent_variable)
-      .value("relative_distance", tp::PropagationDependentVariables::relative_distance_dependent_variable)
-      .value("relative_velocity", tp::PropagationDependentVariables::relative_velocity_dependent_variable)
-      .value("radiation_pressure", tp::PropagationDependentVariables::radiation_pressure_dependent_variable)
-      .value("total_acceleration_norm", tp::PropagationDependentVariables::total_acceleration_norm_dependent_variable)
-      .value("single_acceleration_norm", tp::PropagationDependentVariables::single_acceleration_norm_dependent_variable)
-      .value("total_acceleration", tp::PropagationDependentVariables::total_acceleration_dependent_variable)
-      .value("single_acceleration", tp::PropagationDependentVariables::single_acceleration_dependent_variable)
-      .value("aerodynamic_force_coefficients", tp::PropagationDependentVariables::aerodynamic_force_coefficients_dependent_variable)
-      .value("aerodynamic_moment_coefficients", tp::PropagationDependentVariables::aerodynamic_moment_coefficients_dependent_variable)
-      .value("rotation_matrix_to_body_fixed_frame", tp::PropagationDependentVariables::rotation_matrix_to_body_fixed_frame_variable)
-      .value("intermediate_aerodynamic_rotation_matrix", tp::PropagationDependentVariables::intermediate_aerodynamic_rotation_matrix_variable)
-      .value("relative_body_aerodynamic_orientation_angle", tp::PropagationDependentVariables::relative_body_aerodynamic_orientation_angle_variable)
-      .value("body_fixed_airspeed_based_velocity", tp::PropagationDependentVariables::body_fixed_airspeed_based_velocity_variable)
-      .value("total_aerodynamic_g_load", tp::PropagationDependentVariables::total_aerodynamic_g_load_variable)
-      .value("stagnation_point_heat_flux", tp::PropagationDependentVariables::stagnation_point_heat_flux_dependent_variable)
-      .value("local_temperature", tp::PropagationDependentVariables::local_temperature_dependent_variable)
-      .value("geodetic_latitude", tp::PropagationDependentVariables::geodetic_latitude_dependent_variable)
-      .value("control_surface_deflection", tp::PropagationDependentVariables::control_surface_deflection_dependent_variable)
-      .value("total_mass_rate", tp::PropagationDependentVariables::total_mass_rate_dependent_variables)
-      .value("lvlh_to_inertial_frame_rotation", tp::PropagationDependentVariables::lvlh_to_inertial_frame_rotation_dependent_variable)
-      .value("periapsis_altitude", tp::PropagationDependentVariables::periapsis_altitude_dependent_variable)
-      .value("total_torque_norm", tp::PropagationDependentVariables::total_torque_norm_dependent_variable)
-      .value("single_torque_norm", tp::PropagationDependentVariables::single_torque_norm_dependent_variable)
-      .value("total_torque", tp::PropagationDependentVariables::total_torque_dependent_variable)
-      .value("single_torque", tp::PropagationDependentVariables::single_torque_dependent_variable)
-      .value("body_fixed_groundspeed_based_velocity", tp::PropagationDependentVariables::body_fixed_groundspeed_based_velocity_variable)
-      .value("keplerian_state", tp::PropagationDependentVariables::keplerian_state_dependent_variable)
-      .value("modified_equinocial_state", tp::PropagationDependentVariables::modified_equinocial_state_dependent_variable)
-      .value("spherical_harmonic_acceleration_terms", tp::PropagationDependentVariables::spherical_harmonic_acceleration_terms_dependent_variable)
-      .value("body_fixed_relative_cartesian_position", tp::PropagationDependentVariables::body_fixed_relative_cartesian_position)
-      .value("body_fixed_relative_spherical_position", tp::PropagationDependentVariables::body_fixed_relative_spherical_position)
-      .value("total_gravity_field_variation_acceleration", tp::PropagationDependentVariables::total_gravity_field_variation_acceleration)
-      .value("single_gravity_field_variation_acceleration", tp::PropagationDependentVariables::single_gravity_field_variation_acceleration)
-      .value("single_gravity_field_variation_acceleration_terms", tp::PropagationDependentVariables::single_gravity_field_variation_acceleration_terms)
-      .value("acceleration_partial_wrt_body_translational_state", tp::PropagationDependentVariables::acceleration_partial_wrt_body_translational_state)
-      .value("local_dynamic_pressure", tp::PropagationDependentVariables::local_dynamic_pressure_dependent_variable)
-      .value("local_aerodynamic_heat_rate", tp::PropagationDependentVariables::local_aerodynamic_heat_rate_dependent_variable)
-      .export_values();
-
-
-  py::class_<tp::VariableSettings,
-             std::shared_ptr<tp::VariableSettings>>
-      VariableSettings_(m, "VariableSettings");
-
-  py::class_<tp::SingleDependentVariableSaveSettings,
-             std::shared_ptr<tp::SingleDependentVariableSaveSettings>,
-             tp::VariableSettings>(m, "SingleDependentVariableSaveSettings")
-      .def(py::init<
-               const tp::PropagationDependentVariables,
-               const std::string &,
-               const std::string &,
-               const int>(),
-           py::arg("dependent_variable_type"),
-           py::arg("associated_body"),
-           py::arg("secondary_body") = "",
-           py::arg("component_idx") = -1);
-
-  //////////////////////////////////////////////////////////////////////////////
-  // astro/basic_astro/accelerationModels.h   TODO: This needs to be moved in tudat source.
-  //////////////////////////////////////////////////////////////////////////////
-  py::class_<
-      tp::SingleAccelerationDependentVariableSaveSettings,
-      std::shared_ptr<tp::SingleAccelerationDependentVariableSaveSettings>,
-      tp::SingleDependentVariableSaveSettings>(m, "SingleAccelerationDependentVariableSaveSettings")
-      .def(py::init<
-               const tudat::basic_astrodynamics::AvailableAcceleration,
-               const std::string &,
-               const std::string &,
-               const bool,
-               const int>(),
-           py::arg("acceleration_model_type"),
-           py::arg("body_undergoing_acceleration"),
-           py::arg("body_exerting_acceleration"),
-           py::arg("use_norm") = 0,
-           py::arg("component_index") = -1);
 
   py::class_<tp::PropagationTerminationSettings,
              std::shared_ptr<tp::PropagationTerminationSettings>>
@@ -956,5 +980,8 @@ void expose_propagation_setup(py::module &m) {
 
   auto propagator_setup = m.def_submodule("propagator");
   expose_propagator_setup(propagator_setup);
+
+  auto dependent_variable_setup = m.def_submodule("dependent_variables");
+  expose_dependent_variable_setup(dependent_variable_setup);
 }
 }// namespace tudatpy
