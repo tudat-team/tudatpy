ea6ce4ccc07121f6e560646a290a0cb71205534a
Dominic Dirkx <d.dirkx@tudelft.nl>
2020-12-10 16:57:06 +0100

    First iteration of commit of interpolators exposure

--- tudatpy/kernel/expose_math/expose_interpolators.cpp
+++ tudatpy/kernel/expose_math/expose_interpolators.cpp
@@ -11,20 +11,84 @@
 #include "expose_interpolators.h"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
 namespace ti = tudat::interpolators;
 
+namespace tudat{
+
+namespace interpolators
+{
+
+template< typename IndependentVariableType, typename DependentVariableType >
+std::shared_ptr< OneDimensionalInterpolator< IndependentVariableType, DependentVariableType > >
+createOneDimensionalInterpolatorBasic(
+        const std::map< IndependentVariableType, DependentVariableType > dataToInterpolate,
+        const std::shared_ptr< InterpolatorSettings > interpolatorSettings )
+{
+    return createOneDimensionalInterpolator( dataToInterpolate, interpolatorSettings );
+}
+
+}
+
+}
 namespace tudatpy {
 
 void expose_interpolators(py::module &m) {
 
-  py::class_<
-      ti::LagrangeInterpolatorSettings,
-      std::shared_ptr<ti::LagrangeInterpolatorSettings>>(m,
-                                                         "LagrangeInterpolatorSettings",
-                                                         "<no doc>");
+    py::class_<ti::InterpolatorSettings,
+            std::shared_ptr<ti::InterpolatorSettings>>(m, "InterpolatorSettings");
+
+    py::class_<ti::LagrangeInterpolatorSettings,
+            std::shared_ptr<ti::LagrangeInterpolatorSettings>,
+            ti::InterpolatorSettings>(m, "LagrangeInterpolatorSettings");
+
+
+
+    m.def("linear_interpolation", &ti::linearInterpolation );
+
+    m.def("cubic_spline_interpolation", &ti::cubicSplineInterpolation );
+
+    m.def("piecewise_constant_interpolation", &ti::piecewiseConstantInterpolation );
+
+    m.def("lagrange_interpolation", &ti::lagrangeInterpolation,
+          py::arg( "order" ) );
+
+
+    m.def("create_one_dimensional_interpolator",
+          &ti::createOneDimensionalInterpolatorBasic< double, Eigen::VectorXd >,
+          py::arg("data_to_interpolate"),
+          py::arg("interpolator_settings") );
+
+    m.def("create_one_dimensional_interpolator",
+          &ti::createOneDimensionalInterpolatorBasic< double, Eigen::MatrixXd >,
+          py::arg("data_to_interpolate"),
+          py::arg("interpolator_settings") );
+
+
+
+    py::class_<
+            ti::OneDimensionalInterpolator<double, Eigen::VectorXd>,
+            std::shared_ptr<ti::OneDimensionalInterpolator<double, Eigen::VectorXd>>>(m, "OneDimensionalInterpolatorVector")
+            .def("interpolate", py::overload_cast< const double >(
+                     &ti::OneDimensionalInterpolator<double, Eigen::VectorXd>::interpolate ),
+                 py::arg("independent_variable_value") )
+            .def("interpolate", py::overload_cast< const std::vector< double >& >(
+                     &ti::OneDimensionalInterpolator<double, Eigen::VectorXd>::interpolate ),
+                 py::arg("independent_variable_values") );
+
+    py::class_<
+            ti::OneDimensionalInterpolator<double, Eigen::MatrixXd>,
+            std::shared_ptr<ti::OneDimensionalInterpolator<double, Eigen::MatrixXd>>>(m, "OneDimensionalInterpolatorMatrix")
+            .def("interpolate", py::overload_cast< const double >(
+                     &ti::OneDimensionalInterpolator<double, Eigen::MatrixXd>::interpolate ),
+                 py::arg("independent_variable_value") )
+            .def("interpolate", py::overload_cast< const std::vector< double >& >(
+                     &ti::OneDimensionalInterpolator<double, Eigen::MatrixXd>::interpolate ),
+                 py::arg("independent_variable_values") );
+
 };
 
 }// namespace tudatpy
