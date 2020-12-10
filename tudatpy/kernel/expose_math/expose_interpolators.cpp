30961bffdb4f82f7490630c3c1fd420f654a3755
Dominic Dirkx <d.dirkx@tudelft.nl>
2020-12-10 19:17:58 +0100

    Added extra exposures (interpolators and propagate Kepler orbit) for NA Assignment 3

--- tudatpy/kernel/expose_math/expose_interpolators.cpp
+++ tudatpy/kernel/expose_math/expose_interpolators.cpp
@@ -12,6 +12,7 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/eigen.h>
 
 namespace py = pybind11;
 
@@ -38,23 +39,66 @@ namespace tudatpy {
 
 void expose_interpolators(py::module &m) {
 
+    py::enum_<ti::BoundaryInterpolationType>(m, "BoundaryInterpolationType")
+         .value("throw_exception_at_boundary", ti::BoundaryInterpolationType::throw_exception_at_boundary)
+         .value("use_boundary_value", ti::BoundaryInterpolationType::use_boundary_value)
+         .value("use_boundary_value_with_warning", ti::BoundaryInterpolationType::use_boundary_value_with_warning)
+         .value("extrapolate_at_boundary", ti::BoundaryInterpolationType::extrapolate_at_boundary)
+         .value("extrapolate_at_boundary_with_warning", ti::BoundaryInterpolationType::extrapolate_at_boundary_with_warning)
+         .value("use_default_value", ti::BoundaryInterpolationType::use_default_value)
+         .value("use_default_value_with_warning", ti::BoundaryInterpolationType::use_default_value_with_warning)
+         .export_values();
+
+    py::enum_<ti::AvailableLookupScheme>(m, "AvailableLookupScheme")
+         .value("hunting_algorithm", ti::AvailableLookupScheme::huntingAlgorithm)
+         .value("binary_search", ti::AvailableLookupScheme::binarySearch)
+         .export_values();
+
+    py::enum_<ti::LagrangeInterpolatorBoundaryHandling>(m, "LagrangeInterpolatorBoundaryHandling")
+         .value("lagrange_cubic_spline_boundary_interpolation", ti::LagrangeInterpolatorBoundaryHandling::lagrange_no_boundary_interpolation)
+         .value("lagrange_no_boundary_interpolation", ti::LagrangeInterpolatorBoundaryHandling::lagrange_no_boundary_interpolation)
+         .export_values();
+
     py::class_<ti::InterpolatorSettings,
             std::shared_ptr<ti::InterpolatorSettings>>(m, "InterpolatorSettings");
 
-    py::class_<ti::LagrangeInterpolatorSettings,
-            std::shared_ptr<ti::LagrangeInterpolatorSettings>,
-            ti::InterpolatorSettings>(m, "LagrangeInterpolatorSettings");
+    py::class_<
+          ti::LagrangeInterpolatorSettings,
+          std::shared_ptr<ti::LagrangeInterpolatorSettings>,
+          ti::InterpolatorSettings>(m,
+                                    "LagrangeInterpolatorSettings",
+                                    "Class for providing settings to creating a Lagrange interpolator.")
+          .def(py::init<
+                   const int,
+                   const bool,
+                   const ti::AvailableLookupScheme,
+                   const ti::LagrangeInterpolatorBoundaryHandling,
+                   const ti::BoundaryInterpolationType>(),
+               py::arg("interpolate_order"),
+               py::arg("use_long_double_time_step") = 0,
+               py::arg("selected_lookup_scheme") = ti::huntingAlgorithm,
+               py::arg("lagrange_boundary_handling") = ti::lagrange_cubic_spline_boundary_interpolation,
+               py::arg("boundary_handling") = ti::extrapolate_at_boundary);
 
 
 
-    m.def("linear_interpolation", &ti::linearInterpolation );
+    m.def("linear_interpolation", &ti::linearInterpolation,
+          py::arg( "lookup_scheme" ) = ti::huntingAlgorithm,
+          py::arg( "boundary_interpolation" ) = ti::extrapolate_at_boundary_with_warning);
 
-    m.def("cubic_spline_interpolation", &ti::cubicSplineInterpolation );
+    m.def("cubic_spline_interpolation", &ti::cubicSplineInterpolation,
+          py::arg( "lookup_scheme" ) = ti::huntingAlgorithm,
+          py::arg( "boundary_interpolation" ) = ti::extrapolate_at_boundary_with_warning);
 
-    m.def("piecewise_constant_interpolation", &ti::piecewiseConstantInterpolation );
+    m.def("piecewise_constant_interpolation", &ti::piecewiseConstantInterpolation,
+          py::arg( "lookup_scheme" ) = ti::huntingAlgorithm,
+          py::arg( "boundary_interpolation" ) = ti::extrapolate_at_boundary_with_warning);
 
     m.def("lagrange_interpolation", &ti::lagrangeInterpolation,
-          py::arg( "order" ) );
+          py::arg( "order" ),
+          py::arg( "lookup_scheme" ) = ti::huntingAlgorithm,
+          py::arg( "boundary_interpolation" ) = ti::extrapolate_at_boundary_with_warning,
+          py::arg( "lagrange_boundary_handling" ) = ti::lagrange_cubic_spline_boundary_interpolation );
 
 
     m.def("create_one_dimensional_interpolator",
