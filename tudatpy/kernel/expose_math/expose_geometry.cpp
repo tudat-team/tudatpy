0e60df5beb37d91e22d513665d6c7464fcf13a1e
Dominic Dirkx <d.dirkx@tudelft.nl>
2021-01-19 10:21:17 +0100

    Added exposure of middle radius function for capsule

--- tudatpy/kernel/expose_math/expose_geometry.cpp
+++ tudatpy/kernel/expose_math/expose_geometry.cpp
@@ -48,6 +48,7 @@ void expose_geometry(py::module &m) {
                  py::arg("rear_length"),
                  py::arg("rear_angle"),
                  py::arg("side_radius") )
+            .def_property_readonly("middle_radius", &tgs::Capsule::getMiddleRadius)
             .def_property_readonly("volume", &tgs::Capsule::getVolume);
 
 
