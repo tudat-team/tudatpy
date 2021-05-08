4fa48477eea142e3f4b66ac0fb5445f727e44067
Geoffrey Garrett <g.h.garrett13@gmail.com>
2021-05-08 15:59:43 +0200

    Update base undocumented docstring file

--- include/tudatpy/docstrings.h
+++ include/tudatpy/docstrings.h
@@ -1,28 +1,18 @@
-
 #include <string>
-#include <exception>
 
 namespace tudatpy {
 
-    static inline std::string get_docstring(std::string name, int variant = 0) {
+static inline std::string get_docstring(std::string name, int variant = 0) {
 
-        if (name == "@docstring_key") {
-            // for testing purposes
-            return "@docstring_value";
-
-        //! @generate_docstrings
-
-        } else {
-
-#ifdef DEBUG
-            return "No documentation.";
-#else
-            throw std::runtime_error(
-                    "Documentation was not found for:" + name + " (variant: " + std::to_string(variant) + ")");
-#endif
-
-        }
-
-    }
+  if (name == "test" && variant == 0) {
+    return "test (variant: 0)";
+  } else if (name == "test" && variant == 0) {
+    return "test (variant: 1)";
+  } else if (name == "test") {
+    return "test (unknown variant)";
+  } else {
+    return "No documentation found.";
+  }
 }
 
+}// namespace tudatpy
