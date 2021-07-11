0afc62f1b5b7f06a503888b3ffb72617d245cc12
Geoffrey Garrett <g.h.garrett13@gmail.com>
2021-07-11 15:36:05 +0200

    Updated `build.sh`

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# build directory
-mkdir build
+# env control arguments
+BUILD_DIR="${build_dir:-build}"
+RUN_TESTS="${run_tests:-0}"
+BUILD_TESTS="${build_tests:-0}"
 
-cd build || {
+# build directory
+mkdir "${BUILD_DIR}"
+
+cd "${BUILD_DIR}" || {
   echo 'entry into build folder failed'
   exit 1
 }
@@ -13,6 +18,7 @@ cmake -DCMAKE_PREFIX_PATH="$CONDA_PREFIX" \
   -DCMAKE_CXX_STANDARD=14 \
   -DBoost_NO_BOOST_CMAKE=ON \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DTUDAT_BUILD_TESTS="${BUILD_TESTS}" \
   ..
 
 # build step
