19d255324b560c62347cbbad956d06afa434c051
foggionni <filippo.oggionni@gmail.com>
2021-08-07 16:29:18 +0200

    Added get_docstring to tudatpy dependent variables.

--- tudatpy/kernel/expose_simulation/expose_propagation_setup/expose_dependent_variable_setup.cpp
+++ tudatpy/kernel/expose_simulation/expose_propagation_setup/expose_dependent_variable_setup.cpp
@@ -33,6 +33,9 @@ namespace propagation_setup {
 
     void expose_dependent_variable_setup(py::module &m) {
 
+    //////////////////////////////////////////////////////////////////////////////////////
+    /// ENUMS ////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
 
         py::enum_<tp::PropagationDependentVariables>(m, "PropagationDependentVariables")
                 // C++ legacy variable names.
@@ -133,13 +136,19 @@ namespace propagation_setup {
                 .export_values();
 
 
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// CLASSES //////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////
+
         py::class_<tp::VariableSettings,
                 std::shared_ptr<tp::VariableSettings>>
-                VariableSettings_(m, "VariableSettings");
+                (m, "VariableSettings", get_docstring("VariableSettings.__docstring__"));
 
         py::class_<tp::SingleDependentVariableSaveSettings,
                 std::shared_ptr<tp::SingleDependentVariableSaveSettings>,
-                tp::VariableSettings>(m, "SingleDependentVariableSaveSettings");
+                tp::VariableSettings>(m,
+                                      "SingleDependentVariableSaveSettings",
+                                      get_docstring("SingleDependentVariableSaveSettings.__docstring__"));
 //            .def(py::init<
 //                 const tp::PropagationDependentVariables,
 //                 const std::string &,
@@ -150,13 +159,12 @@ namespace propagation_setup {
 //                 py::arg("secondary_body") = "",
 //                 py::arg("component_idx") = -1);
 
-        //////////////////////////////////////////////////////////////////////////////
-        // astro/basic_astro/accelerationModels.h   TODO: This needs to be moved in tudat source.
-        //////////////////////////////////////////////////////////////////////////////
         py::class_<
                 tp::SingleAccelerationDependentVariableSaveSettings,
                 std::shared_ptr<tp::SingleAccelerationDependentVariableSaveSettings>,
-                tp::SingleDependentVariableSaveSettings>(m, "SingleAccelerationDependentVariableSaveSettings");
+                tp::SingleDependentVariableSaveSettings>(m,
+                                                         "SingleAccelerationDependentVariableSaveSettings",
+                                                         get_docstring("SingleAccelerationDependentVariableSaveSettings.__docstring__"));
 //            .def(py::init<
 //                 const tudat::basic_astrodynamics::AvailableAcceleration,
 //                 const std::string &,
@@ -169,177 +177,216 @@ namespace propagation_setup {
 //                 py::arg("use_norm") = 0,
 //                 py::arg("component_index") = -1);
 
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        /// FREE FUNCTIONS ///////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////
+
         m.def("create",
               &tp::createDependentVariableSaveSettings,
               py::arg("dependent_variable_list"),
-              py::arg("print_variable_indices") = true);
+              py::arg("print_variable_indices") = true,
+              get_docstring("create").c_str());
 
         m.def("mach_number",
               &tp::machNumberDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("mach_number").c_str());
 
         m.def("altitude",
               &tp::altitudeDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("altitude").c_str());
 
         m.def("airspeed",
               &tp::airspeedDependentVariable,
               py::arg("body"),
-              py::arg("body_with_atmosphere"));
+              py::arg("body_with_atmosphere"),
+              get_docstring("airspeed").c_str());
 
         m.def("density",
               &tp::densityDependentVariable,
               py::arg("body"),
-              py::arg("body_with_atmosphere"));
+              py::arg("body_with_atmosphere"),
+              get_docstring("density").c_str());
 
         m.def("relative_speed",
               &tp::relativeSpeedDependentVariable,
               py::arg("body"),
-              py::arg("relative_body"));
+              py::arg("relative_body"),
+              get_docstring("relative_speed").c_str());
 
         m.def("relative_position",
               &tp::relativePositionDependentVariable,
               py::arg("body"),
-              py::arg("relative_body"));
+              py::arg("relative_body"),
+              get_docstring("relative_position").c_str());
 
         m.def("relative_distance",
               &tp::relativeDistanceDependentVariable,
               py::arg("body"),
-              py::arg("relative_body"));
+              py::arg("relative_body"),
+              get_docstring("relative_distance").c_str());
 
         m.def("relative_velocity",
               &tp::relativeVelocityDependentVariable,
               py::arg("body"),
-              py::arg("relative_body"));
+              py::arg("relative_body"),
+              get_docstring("relative_velocity").c_str());
 
         m.def("keplerian_state",
               &tp::keplerianStateDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("keplerian_state").c_str());
 
         m.def("modified_equinoctial_state",
               &tp::modifiedEquinoctialStateDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("modified_equinoctial_state").c_str());
 
         m.def("single_acceleration",
               &tp::singleAccelerationDependentVariable,
               py::arg("acceleration_type"),
               py::arg("body_undergoing_acceleration"),
-              py::arg("body_exerting_acceleration"));
+              py::arg("body_exerting_acceleration"),
+              get_docstring("single_acceleration").c_str());
 
         m.def("single_acceleration_norm",
               &tp::singleAccelerationNormDependentVariable,
               py::arg("acceleration_type"),
               py::arg("body_undergoing_acceleration"),
-              py::arg("body_exerting_acceleration"));
+              py::arg("body_exerting_acceleration"),
+              get_docstring("single_acceleration_norm").c_str());
 
         m.def("total_acceleration_norm",
               &tp::totalAccelerationNormDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("total_acceleration_norm").c_str());
 
         m.def("total_acceleration",
               &tp::totalAccelerationDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("total_acceleration").c_str());
 
         m.def("single_torque_norm",
               &tp::singleTorqueNormVariable,
               py::arg("torque_type"),
               py::arg("body_undergoing_torque"),
-              py::arg("body_exerting_torque"));
+              py::arg("body_exerting_torque"),
+              get_docstring("single_torque_norm").c_str());
 
         m.def("single_torque",
               &tp::singleTorqueVariable,
               py::arg("torque_type"),
               py::arg("body_undergoing_torque"),
-              py::arg("body_exerting_torque"));
+              py::arg("body_exerting_torque"),
+              get_docstring("single_torque").c_str());
 
         m.def("total_torque_norm",
               &tp::totalTorqueNormDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("total_torque_norm").c_str());
 
         m.def("total_torque",
               &tp::totalTorqueDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("total_torque").c_str());
 
         m.def("spherical_harmonic_terms_acceleration",
               &tp::sphericalHarmonicAccelerationTermsDependentVariable,
               py::arg("body_undergoing_acceleration"),
               py::arg("body_exerting_acceleration"),
-              py::arg("component_indices"));
+              py::arg("component_indices"),
+              get_docstring("spherical_harmonic_terms_acceleration").c_str());
 
         m.def("spherical_harmonic_terms_acceleration_norm",
               &tp::sphericalHarmonicAccelerationTermsNormDependentVariable,
               py::arg("body_undergoing_acceleration"),
               py::arg("body_exerting_acceleration"),
-              py::arg("component_indices"));
+              py::arg("component_indices"),
+              get_docstring("spherical_harmonic_terms_acceleration_norm").c_str());
 
         m.def("aerodynamic_force_coefficients",
               &tp::aerodynamicForceCoefficientDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("aerodynamic_force_coefficients").c_str());
 
         m.def("aerodynamic_moment_coefficients",
               &tp::aerodynamicMomentCoefficientDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("aerodynamic_moment_coefficients").c_str());
 
         m.def("latitude",
               &tp::latitudeDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("latitude").c_str());
 
         m.def("geodetic_latitude",
               &tp::geodeticLatitudeDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("geodetic_latitude").c_str());
 
         m.def("longitude",
               &tp::longitudeDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("longitude").c_str());
 
         m.def("heading_angle",
               &tp::headingDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("heading_angle").c_str());
 
         m.def("flight_path_angle",
               &tp::flightPathAngleDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("flight_path_angle").c_str());
 
         m.def("angle_of_attack",
               &tp::angleOfAttackDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("angle_of_attack").c_str());
 
         m.def("sideslip_angle",
               &tp::sideslipAngleDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("sideslip_angle").c_str());
 
         m.def("bank_angle",
               &tp::bankAngleDependentVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("bank_angle").c_str());
 
         m.def("radiation_pressure",
               &tp::radiationPressureDependentVariable,
               py::arg("body"),
-              py::arg("radiating_body"));
+              py::arg("radiating_body"),
+              get_docstring("radiation_pressure").c_str());
 
         m.def("total_gravity_field_variation_acceleration",
               &tp::totalGravityFieldVariationAccelerationContributionVariable,
               py::arg("body_undergoing_acceleration"),
-              py::arg("body_exerting_acceleration"));
+              py::arg("body_exerting_acceleration"),
+              get_docstring("total_gravity_field_variation_acceleration").c_str());
 
         m.def("single_gravity_field_variation_acceleration",
               &tp::singleGravityFieldVariationAccelerationContributionVariable,
               py::arg("body_undergoing_acceleration"),
               py::arg("body_exerting_acceleration"),
               py::arg("deformation_type"),
-              py::arg("identifier") = "");
+              py::arg("identifier") = "",
+              get_docstring("single_gravity_field_variation_acceleration").c_str());
 
         m.def("single_per_term_gravity_field_variation_acceleration",
               &tp::singleGravityFieldVariationSeparateTermsAccelerationContributionVariable,
@@ -347,107 +394,129 @@ namespace propagation_setup {
               py::arg("body_exerting_acceleration"),
               py::arg("component_indices"),
               py::arg("deformation_type"),
-              py::arg("identifier") = "");
+              py::arg("identifier") = "",
+              get_docstring("single_per_term_gravity_field_variation_acceleration").c_str());
 
         m.def("body_fixed_airspeed_velocity",
               &tp::bodyFixedAirspeedBasedVelocityVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("body_fixed_airspeed_velocity").c_str());
 
         m.def("body_fixed_groundspeed_velocity",
               &tp::bodyFixedGroundspeedBasedVelocityVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("body_fixed_groundspeed_velocity").c_str());
 
         m.def("inertial_to_body_fixed_rotation_frame",
               &tp::inertialToBodyFixedRotationMatrixVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("inertial_to_body_fixed_rotation_frame").c_str());
 
         m.def("lvlh_to_inertial_rotation_matrix",
               &tp::lvlhToInertialFrameRotationMatrixVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("lvlh_to_inertial_rotation_matrix").c_str());
 
         m.def("inertial_to_body_fixed_313_euler_angles",
               &tp::eulerAnglesToBodyFixed313Variable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("inertial_to_body_fixed_313_euler_angles").c_str());
 
         m.def("intermediate_aerodynamic_rotation_matrix_variable",
               &tp::intermediateAerodynamicRotationMatrixVariable,
               py::arg("body"),
               py::arg("base_frame"),
-              py::arg("target_frame"));
+              py::arg("target_frame"),
+              get_docstring("intermediate_aerodynamic_rotation_matrix_variable").c_str());
 
         m.def("periapsis_altitude",
               &tp::periapsisAltitudeVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("periapsis_altitude").c_str());
 
         m.def("control_surface_deflection",
               &tp::controlSurfaceDeflectionDependentVariable,
               py::arg("body"),
-              py::arg("control_surface"));
+              py::arg("control_surface"),
+              get_docstring("control_surface_deflection").c_str());
 
         m.def("central_body_fixed_spherical_position",
               &tp::centralBodyFixedSphericalPositionVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("central_body_fixed_spherical_position").c_str());
 
         m.def("central_body_fixed_cartesian_position",
               &tp::centralBodyFixedCartesianPositionVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("central_body_fixed_cartesian_position").c_str());
 
         m.def("body_mass",
               &tp::bodyMassVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("body_mass").c_str());
 
         m.def("radiation_pressure_coefficient",
               &tp::radiationPressureCoefficientVariable,
               py::arg("body"),
-              py::arg("emitting_body"));
+              py::arg("emitting_body"),
+              get_docstring("radiation_pressure_coefficient").c_str());
 
         m.def("local_temperature",
               &tp::localTemperatureDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("local_temperature").c_str());
 
         m.def("local_dynamic_pressure",
               &tp::localDynamicPressureDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("local_dynamic_pressure").c_str());
 
         m.def("local_aerodynamic_heat_rate",
               &tp::localAerodynamicHeatRateDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("local_aerodynamic_heat_rate").c_str());
 
         m.def("local_aerodynamic_g_load",
               &tp::totalAerodynamicGLoadDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("local_aerodynamic_g_load").c_str());
 
         m.def("stagnation_point_heat_flux",
               &tp::stagnationPointHeatFluxDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("stagnation_point_heat_flux").c_str());
 
         m.def("total_mass_rate",
               &tp::totalMassRateDependentVariable,
-              py::arg("body"));
+              py::arg("body"),
+              get_docstring("total_mass_rate").c_str());
 
         // TODO: duplicate
         m.def("aerodynamic_g_load",
               &tp::aerodynamicGLoadVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("aerodynamic_g_load").c_str());
 
         // TODO: duplicate
         m.def("atmospheric_temperature",
               &tp::atmosphericTemperatureVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("atmospheric_temperature").c_str());
 
         // TODO: duplicate
         m.def("dynamic_pressure",
               &tp::dynamicPressureVariable,
               py::arg("body"),
-              py::arg("central_body"));
+              py::arg("central_body"),
+              get_docstring("dynamic_pressure").c_str());
 
     }
 
