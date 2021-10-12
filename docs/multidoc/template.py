7e00d25657db1f23165a70d9aaa9d4c91a377ba6
ggarrett13 <g.h.garrett13@gmail.com>
2021-10-12 20:46:43 +0100

    Docstring fixes & docs upgrade

--- docs/multidoc/template.py
+++ docs/multidoc/template.py
@@ -3,6 +3,7 @@ from jinja2 import Template
 import jinja2
 from multidoc.utils import indent_line
 # from multidoc.parsing.models import APIDeclaration
+import re
 
 __languages__ = [
     'cpp',
@@ -16,16 +17,81 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
 TEMPLATE_LANGUAGE_MAP = {"cpp": os.path.join(TEMPLATE_DIR, "cpp.jinja2"),
                          "py": os.path.join(TEMPLATE_DIR, "numpydoc.jinja2")}
 
+TEMPLATE_PROPERTY = os.path.join(TEMPLATE_DIR, "property.jinja2")
+
 # template mapping for generated compiled docstrings
 TEMPLATE_DOCSTRING_HEADER = os.path.join(TEMPLATE_DIR, "docstring_h.jinja2")
 
 # base globals required for template functions
 _BASE_GLOBALS = {"indent_line": indent_line}
 
+LINKFY_PATTERN = re.compile(r"([\w.]+)")
+
+
 # functions ------------------------------------------------------------------#
 
 
+def linkify_type(type_str):
+    # You can manually specify the number of replacements by changing the 4th argument
+    # result = re.sub(regex, subst, type_str, 0, re.MULTILINE)
+    matches = set(list(re.findall(LINKFY_PATTERN, type_str)))
+    print(matches)
+    for match in matches:
+        print('---', type_str)
+        type_str = type_str.replace(match, f':class:`{match}`')
+    type_str = type_str.replace('[', '\[')
+    type_str = type_str.replace(']', '\]')
+    return type_str
 
+
+def get_property_template(**kwargs):
+    """
+
+    Parameters
+    ----------
+    kwargs : dict[str, Any]
+        One of the supported :ref:`multidoc.template.__languages__` must be set
+        to true in the **kwargs in order to facilitate successful docstring
+        deduction.
+
+    Returns
+    -------
+
+    """
+    _globals = kwargs.pop("globals", {})
+    _globals.update(_BASE_GLOBALS)
+    _globals["linkify_type"] = linkify_type
+    _lstrip_blocks = kwargs.pop("lstrip_blocks", True)
+    _trim_blocks = kwargs.pop("trim_blocks", True)
+
+    # iterate through supported languages, set as False if not in kwargs.
+    # highly unnecessary here?
+    _locals = {}
+    for j in __languages__:
+        _locals[j] = kwargs.pop(j, False)
+
+    # which language is True
+    aux = list(map(lambda x: x is True, [_locals[i] for i in __languages__]))
+
+    # ensure only one language detected (e.g. cpp / py)
+    assert aux.count(True) == 1
+
+    # finally deduce lang.
+    # TODO: There must be a more straightforward way.
+    lang = __languages__[aux.index(True)]
+
+    # open language template and return.
+    with open(TEMPLATE_PROPERTY) as f:
+        t = Template(
+            f.read(),
+            lstrip_blocks=_lstrip_blocks,
+            trim_blocks=_trim_blocks)
+
+        # update globals dict if globals given through kwargs
+        t.globals.update(_globals)
+        # if _globals else None (currently redundant)
+
+        return t
 
 
 def get_docstring_template(**kwargs):
@@ -33,7 +99,7 @@ def get_docstring_template(**kwargs):
 
     Parameters
     ----------
-    kwargs : Dict[str, Any]
+    kwargs : dict[str, Any]
         One of the supported :ref:`multidoc.template.__languages__` must be set
         to true in the **kwargs in order to facilitate successful docstring
         deduction.
