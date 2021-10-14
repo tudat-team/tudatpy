782635be42ef5dac73f383acdc53f7d36703ef65
ggarrett13 <g.h.garrett13@gmail.com>
2021-10-14 10:53:16 +0100

    Updated `tudat-multidoc` hash

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ if bool(os.getenv("READTHEDOCS")) is True:
     from document import *
 
     multidoc_git_url = 'https://github.com/tudat-team/tudat-multidoc.git'
-    multidoc_git_rev = 'cd3aec7b999403744b2af753ce1966be25639194'
+    multidoc_git_rev = 'b6a629e6840a718ec573b32890a76f298152d129'
 
     # clone repository
     docstring_path = get_docstrings(multidoc_git_url, multidoc_git_rev)
