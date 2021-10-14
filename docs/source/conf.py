a6bdaf370b792684641a95fea65706fc4abed452
ggarrett13 <g.h.garrett13@gmail.com>
2021-10-14 16:17:53 +0100

    Updated hash for `frame_conversion` doc fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ if bool(os.getenv("READTHEDOCS")) is True:
     from document import *
 
     multidoc_git_url = 'https://github.com/tudat-team/tudat-multidoc.git'
-    multidoc_git_rev = 'b6a629e6840a718ec573b32890a76f298152d129'
+    multidoc_git_rev = 'e472c465061802f41e1dd8a6cee2df6e7cd87302'
 
     # clone repository
     docstring_path = get_docstrings(multidoc_git_url, multidoc_git_rev)
