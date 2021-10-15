80e7ec6be9a42300f2882d2c18541fdfa13f5b3b
jhener <jonas.hener@web.de>
2021-10-15 18:29:50 +0200

    found syntax error that prevented kernel from building

--- tudatpy/kernel/expose_numerical_simulation/expose_environment_setup/expose_gravity_field_setup.cpp
+++ tudatpy/kernel/expose_numerical_simulation/expose_environment_setup/expose_gravity_field_setup.cpp
@@ -44,12 +44,12 @@ namespace gravity_field {
         py::enum_<tss::SphericalHarmonicsModel>(m, "SphericalHarmonicsModel",
                                                 get_docstring("SphericalHarmonicsModel").c_str())
                 .value("custom_model", tss::SphericalHarmonicsModel::customModel, get_docstring("SphericalHarmonicsModel.custom_model").c_str())
-                .value("egm96", tss::SphericalHarmonicsModel::egm96, , get_docstring("SphericalHarmonicsModel.egm96").c_str())
-                .value("ggm02c", tss::SphericalHarmonicsModel::ggm02c, , get_docstring("SphericalHarmonicsModel.ggm02c").c_str())
-                .value("ggm02s", tss::SphericalHarmonicsModel::ggm02s, , get_docstring("SphericalHarmonicsModel.ggm02s").c_str())
-                .value("glgm3150", tss::SphericalHarmonicsModel::glgm3150, , get_docstring("SphericalHarmonicsModel.glgm3150").c_str())
-                .value("lpe200", tss::SphericalHarmonicsModel::lpe200, , get_docstring("SphericalHarmonicsModel.lpe200").c_str())
-                .value("jgmro120d", tss::SphericalHarmonicsModel::jgmro120d, , get_docstring("SphericalHarmonicsModel.jgmro120d").c_str())
+                .value("egm96", tss::SphericalHarmonicsModel::egm96, get_docstring("SphericalHarmonicsModel.egm96").c_str())
+                .value("ggm02c", tss::SphericalHarmonicsModel::ggm02c, get_docstring("SphericalHarmonicsModel.ggm02c").c_str())
+                .value("ggm02s", tss::SphericalHarmonicsModel::ggm02s, get_docstring("SphericalHarmonicsModel.ggm02s").c_str())
+                .value("glgm3150", tss::SphericalHarmonicsModel::glgm3150, get_docstring("SphericalHarmonicsModel.glgm3150").c_str())
+                .value("lpe200", tss::SphericalHarmonicsModel::lpe200, get_docstring("SphericalHarmonicsModel.lpe200").c_str())
+                .value("jgmro120d", tss::SphericalHarmonicsModel::jgmro120d, get_docstring("SphericalHarmonicsModel.jgmro120d").c_str())
                 .export_values();
 
         py::class_<tss::GravityFieldSettings, std::shared_ptr<tss::GravityFieldSettings>>(
