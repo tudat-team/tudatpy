0166871b2ba3c57ee54a955d9f35c190d5dfe454
Dominic Dirkx <d.dirkx@tudelft.nl>
2021-12-16 18:59:45 +0100

    Updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ if bool(os.getenv("READTHEDOCS")) is True:
     from document import *
 
     multidoc_git_url = 'https://github.com/tudat-team/tudat-multidoc.git'
-    multidoc_git_rev = 'c57996cdd365976cee65a0c2993e0b39034664fc'
+    multidoc_git_rev = '4660f6095596b83be210e2a801fc4e8b6e0aed2e'
 
     # clone repository
     docstring_path = get_docstrings(multidoc_git_url, multidoc_git_rev)
