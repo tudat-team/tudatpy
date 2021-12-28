a8e3b1db16c687049bb7d75368f4ea2e5968a8e5
jgaffarel <jgaffarel@tudelft.nl>
2021-12-28 15:26:49 +0100

    Added utility to compare the results of two distinct propagations.

--- include/tudatpy/docstrings.h
+++ include/tudatpy/docstrings.h
@@ -9475,6 +9475,13 @@ static inline std::string get_docstring(std::string name, int variant=0) {
 
 
 
+    } else if(name == "compare_results" && variant==0) {
+            return R"(
+
+    )";
+
+
+
     } else {
         return "No documentation found.";
     }
