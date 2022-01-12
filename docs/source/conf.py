5c390afad0a1c96aaf992b65fdb731e20687adba
gaffarelj <jeremie@gaffarel.pro>
2022-01-12 15:56:31 +0100

    Attempt at fixing plotting import error + trigger API build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ if bool(os.getenv("READTHEDOCS")) is True:
     from document import *
 
     multidoc_git_url = 'https://github.com/tudat-team/tudat-multidoc.git'
-    multidoc_git_rev = '6edd2f6abeabfaf9723e9b6463448b0acc807e8d'
+    multidoc_git_rev = '18fcab19ecff5762aad5b0bf27af66fd85fd79fa'
 
     # clone repository
     docstring_path = get_docstrings(multidoc_git_url, multidoc_git_rev)
