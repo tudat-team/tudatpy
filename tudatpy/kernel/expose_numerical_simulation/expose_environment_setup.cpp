bc8d6e1035f75c783a10f0be89e083eb3d567ec2
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-01-14 20:50:29 +0100

    Minor tweaks for auto-tabulated-ephemeris creation

--- tudatpy/kernel/expose_numerical_simulation/expose_environment_setup.cpp
+++ tudatpy/kernel/expose_numerical_simulation/expose_environment_setup.cpp
@@ -124,7 +124,7 @@ namespace environment_setup {
               py::arg("body_settings"),
               get_docstring("create_system_of_bodies").c_str());
 
-        m.def("add_empty_tabulated_ephemeris", &tss::addEmptyTabulateEphemeris,
+        m.def("add_empty_tabulated_ephemeris", &tss::addEmptyTabulatedEphemeris,
               py::arg("bodies"),
               py::arg("body_name"),
               py::arg("ephemeris_origin") = "",
