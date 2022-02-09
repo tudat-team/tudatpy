4347865ea4e69887b1e94bad32ad5322e366fe86
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-02-09 16:50:08 +0100

    Added docstrings to shape-based methods; minor update of interfaces

--- tudatpy/kernel/expose_trajectory_design/expose_shape_based_thrust.h
+++ tudatpy/kernel/expose_trajectory_design/expose_shape_based_thrust.h
@@ -16,15 +16,15 @@
 namespace py = pybind11;
 
 namespace tudatpy {
-//namespace astro {
-//namespace shape_based_thrust {
+namespace trajectory_design {
+namespace shape_based_thrust {
 
 
 void expose_shape_based_thrust(py::module &m);
 
 
-//}// namespace shape_based_thrust
-//}// namespace astro
+}
+}
 }// namespace tudatpy
 
 #endif//TUDATPY_EXPOSE_SHAPE_BASED_THRUST_H
