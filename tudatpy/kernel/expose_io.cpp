e59842f6e6a9a29bbf6bcb7bf1e458ebcb4dc469
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-02-23 01:04:15 +0100

    Added docstrings to enums and classes of io module

--- tudatpy/kernel/expose_io.cpp
+++ tudatpy/kernel/expose_io.cpp
@@ -66,72 +66,83 @@ void expose_io(py::module &m) {
       );
 
       py::class_<tudat::input_output::MissileDatcomData,
-            std::shared_ptr<tudat::input_output::MissileDatcomData>>(m, "missile_DATCOM_data")
+            std::shared_ptr<tudat::input_output::MissileDatcomData>>(m, "missile_DATCOM_data",
+                                                                     get_docstring("missile_DATCOM_data").c_str())
             .def(py::init<
                   const std::string &>(),
-                  py::arg("file_name_and_path"))
+                  py::arg("file_name_and_path"),
+                  get_docstring("missile_DATCOM_data.ctor").c_str())
             .def("get_static_coefficient",
                   &tudat::input_output::MissileDatcomData::getStaticCoefficient,
                   py::arg("mach_index"),
                   py::arg("angle_of_attack_index"),
-                  py::arg("coefficient_index"))
+                  py::arg("coefficient_index"),
+                  get_docstring("missile_DATCOM_data.get_static_coefficient").c_str())
             .def("get_dynamic_coefficient",
                   &tudat::input_output::MissileDatcomData::getDynamicCoefficient,
                   py::arg("mach_index"),
                   py::arg("angle_of_attack_index"),
-                  py::arg("coefficient_index"))
+                  py::arg("coefficient_index"),
+                  get_docstring("missile_DATCOM_data.get_dynamic_coefficient").c_str())
             .def("get_angle_of_attacks",
-                  &tudat::input_output::MissileDatcomData::getAngleOfAttacks)
+                  &tudat::input_output::MissileDatcomData::getAngleOfAttacks,
+                  get_docstring("missile_DATCOM_data.get_angle_of_attacks").c_str())
             .def("get_mach_numbers",
-                  &tudat::input_output::MissileDatcomData::getMachNumbers)
+                  &tudat::input_output::MissileDatcomData::getMachNumbers,
+                  get_docstring("missile_DATCOM_data.get_mach_numbers").c_str())
             .def("get_Reynolds_numbers",
-                  &tudat::input_output::MissileDatcomData::getReynoldsNumbers)
+                  &tudat::input_output::MissileDatcomData::getReynoldsNumbers,
+                  get_docstring("missile_DATCOM_data.get_Reynolds_numbers").c_str())
             .def("write_all_coefficients_to_files",
                   &tudat::input_output::MissileDatcomData::writeAllCoefficientsToFiles,
                   py::arg("file_name_base"),
                   py::arg("base_precision") = 15,
-                  py::arg("exponent_width") = 2 )
+                  py::arg("exponent_width") = 2,
+                  get_docstring("missile_DATCOM_data.write_all_coefficients_to_files").c_str() )
             .def("write_force_and_moment_coefficients_to_files",
                   &tudat::input_output::MissileDatcomData::writeForceAndMomentCoefficientsToFiles,
                   py::arg("file_name_base"),
                   py::arg("base_precision") = 15,
-                  py::arg("exponent_width") = 2 );
+                  py::arg("exponent_width") = 2,
+                  get_docstring("missile_DATCOM_data.write_force_and_moment_coefficients_to_files").c_str() );
 
-      py::enum_<tudat::input_output::MissileDatcomData::DynamicCoefficientNames>(m, "DynamicCoefficientNames")
-            .value("cnq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnq)
-            .value("cmq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmq)
-            .value("caq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::caq)
-            .value("cyq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyq)
-            .value("clnq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnq)
-            .value("cllq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllq)
-            .value("cnr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnr)
-            .value("cmr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmr)
-            .value("car", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::car)
-            .value("cyr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyr)
-            .value("clnr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnr)
-            .value("cllr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllr)
-            .value("cnp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnp)
-            .value("cmp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmp)
-            .value("cap", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cap)
-            .value("cyp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyp)
-            .value("clnp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnp)
-            .value("cllp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllp)
-            .value("cnad", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnad)
-            .value("cmad", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmad)
+      py::enum_<tudat::input_output::MissileDatcomData::DynamicCoefficientNames>(m, "DynamicCoefficientNames",
+                                                                                 get_docstring("DynamicCoefficientNames").c_str())
+            .value("cnq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnq, get_docstring("DynamicCoefficientNames.cnq").c_str())
+            .value("cmq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmq, get_docstring("DynamicCoefficientNames.cmq").c_str())
+            .value("caq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::caq, get_docstring("DynamicCoefficientNames.caq").c_str())
+            .value("cyq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyq, get_docstring("DynamicCoefficientNames.cyq").c_str())
+            .value("clnq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnq, get_docstring("DynamicCoefficientNames.clnq").c_str())
+            .value("cllq", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllq, get_docstring("DynamicCoefficientNames.cllq").c_str())
+            .value("cnr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnr, get_docstring("DynamicCoefficientNames.cnr").c_str())
+            .value("cmr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmr, get_docstring("DynamicCoefficientNames.cmr").c_str())
+            .value("car", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::car, get_docstring("DynamicCoefficientNames.car").c_str())
+            .value("cyr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyr, get_docstring("DynamicCoefficientNames.cyr").c_str())
+            .value("clnr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnr, get_docstring("DynamicCoefficientNames.clnr").c_str())
+            .value("cllr", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllr, get_docstring("DynamicCoefficientNames.cllr").c_str())
+            .value("cnp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnp, get_docstring("DynamicCoefficientNames.cnp").c_str())
+            .value("cmp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmp, get_docstring("DynamicCoefficientNames.cmp").c_str())
+            .value("cap", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cap, get_docstring("DynamicCoefficientNames.cap").c_str())
+            .value("cyp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cyp, get_docstring("DynamicCoefficientNames.cyp").c_str())
+            .value("clnp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::clnp, get_docstring("DynamicCoefficientNames.clnp").c_str())
+            .value("cllp", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cllp, get_docstring("DynamicCoefficientNames.cllp").c_str())
+            .value("cnad", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cnad, get_docstring("DynamicCoefficientNames.cnad").c_str())
+            .value("cmad", tudat::input_output::MissileDatcomData::DynamicCoefficientNames::cmad, get_docstring("DynamicCoefficientNames.cmad").c_str())
             .export_values();
 
-      py::enum_<tudat::input_output::MissileDatcomData::StaticCoefficientNames>(m, "StaticCoefficientNames")
-            .value("cn", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cn)
-            .value("cm", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cm)
-            .value("ca", tudat::input_output::MissileDatcomData::StaticCoefficientNames::ca)
-            .value("cy", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cy)
-            .value("cln", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cln)
-            .value("cll", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cll)
-            .value("cna", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cna)
-            .value("cma", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cma)
-            .value("cyb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cyb)
-            .value("cnb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cnb)
-            .value("clb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::clb)
+      py::enum_<tudat::input_output::MissileDatcomData::StaticCoefficientNames>(m, "StaticCoefficientNames",
+                                                                                get_docstring("StaticCoefficientNames").c_str())
+            .value("cn", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cn, get_docstring("StaticCoefficientNames.cn").c_str())
+            .value("cm", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cm, get_docstring("StaticCoefficientNames.cm").c_str())
+            .value("ca", tudat::input_output::MissileDatcomData::StaticCoefficientNames::ca, get_docstring("StaticCoefficientNames.ca").c_str())
+            .value("cy", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cy, get_docstring("StaticCoefficientNames.cy").c_str())
+            .value("cln", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cln, get_docstring("StaticCoefficientNames.cln").c_str())
+            .value("cll", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cll, get_docstring("StaticCoefficientNames.cll").c_str())
+            .value("cna", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cna, get_docstring("StaticCoefficientNames.cna").c_str())
+            .value("cma", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cma, get_docstring("StaticCoefficientNames.cma").c_str())
+            .value("cyb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cyb, get_docstring("StaticCoefficientNames.cyb").c_str())
+            .value("cnb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::cnb, get_docstring("StaticCoefficientNames.cnb").c_str())
+            .value("clb", tudat::input_output::MissileDatcomData::StaticCoefficientNames::clb, get_docstring("StaticCoefficientNames.clb").c_str())
             .export_values();
 
 };
