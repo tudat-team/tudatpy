74f69012817928cca36a9c906f675c4fd5604be5
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-05-18 19:18:32 +0200

    Edited docstrings variable name (dangerously similar to another one)

--- docs/source/build_docstrings.py
+++ docs/source/build_docstrings.py
@@ -2,8 +2,6 @@ from document import *
 
 import os
 
-docstrings_path = "/".join(os.path.abspath(__file__).split("/")[:-3])+"/include/tudatpy/docstrings.h"
-
 # clone repository
 docstring_path = get_docstrings('https://github.com/tudat-team/tudat-multidoc.git')
 
@@ -11,4 +9,5 @@ docstring_path = get_docstrings('https://github.com/tudat-team/tudat-multidoc.gi
 api_declaration = parse_api_declaration(docstring_path, py=True)
 
 # generate docstring header
-generate_docstring_header(api_declaration, docstrings_path)
+docstrings_header_path = "/".join(os.path.abspath(__file__).split("/")[:-3])+"/include/tudatpy/docstrings.h"
+generate_docstring_header(api_declaration, docstrings_header_path)
