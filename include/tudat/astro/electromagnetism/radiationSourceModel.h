7492c52966d7229783a41bd36ff4174e02a7f8e6
Dominik Stiller <domi.stiller@gmail.com>
2022-08-15 01:29:56 +0200

    Add paneled radiation source with albedo panels

--- include/tudat/astro/electromagnetism/radiationSourceModel.h
+++ include/tudat/astro/electromagnetism/radiationSourceModel.h
@@ -1,14 +1,31 @@
+/*    Copyright (c) 2010-2019, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ *
+ *    References
+ *      E. B. Saff, et al. "Distributing many points on a sphere". The Mathematical Intelligencer 19. 1(1997): 5–11.
+ */
+
 #ifndef TUDAT_RADIATIONSOURCEMODEL_H
 #define TUDAT_RADIATIONSOURCEMODEL_H
 
 #include <vector>
+#include <map>
 #include <utility>
 #include <functional>
 #include <memory>
 
 #include <Eigen/Core>
+#include <Eigen/Geometry>
 
 #include "tudat/astro/electromagnetism/luminosityModel.h"
+#include "tudat/astro/electromagnetism/reflectionLaw.h"
+#include "tudat/astro/basic_astro/bodyShapeModel.h"
 
 namespace tudat
 {
@@ -28,12 +45,17 @@ public:
     void updateMembers(double currentTime );
 
     /*!
-     * Evaluate the irradiance [W/m²] at a certain positition due to this source.
+     * Evaluate the irradiance [W/m²] at a certain position due to this source.
      * @param targetPosition Position where to evaluate the irradiance in local (i.e. source-fixed) coordinates
-     * @return A list of irradiances and their source-fixed origin. Single element for point sources, multiple
-     * elements for paneled sources.
+     * @param originalSourceIrradiance Irradiance from the original source (if applicable)
+     * @param originalSourceToSourceDirection Direction of incoming radiation in local (i.e. source-fixed) coordinates
+     * @return A list of irradiances and their source-fixed origin. Every element can be thought of as a ray.
+     * Single element for point sources, multiple elements for paneled sources.
      */
-    virtual IrradianceWithSourceList evaluateIrradianceAtPosition(Eigen::Vector3d targetPosition) const = 0;
+    virtual IrradianceWithSourceList evaluateIrradianceAtPosition(
+            Eigen::Vector3d targetPosition,
+            double originalSourceIrradiance,
+            Eigen::Vector3d originalSourceToSourceDirection) const = 0;
 
 protected:
     virtual void updateMembers_(const double currentTime) {};
@@ -41,6 +63,9 @@ protected:
     double currentTime_{TUDAT_NAN};
 };
 
+//*********************************************************************************************
+//   Isotropic point radiation source
+//*********************************************************************************************
 
 class IsotropicPointRadiationSourceModel : public RadiationSourceModel
 {
@@ -49,7 +74,12 @@ public:
             const std::shared_ptr<LuminosityModel> luminosityModel) :
         luminosityModel_(luminosityModel) {}
 
-    IrradianceWithSourceList evaluateIrradianceAtPosition(Eigen::Vector3d targetPosition) const override;
+    IrradianceWithSourceList evaluateIrradianceAtPosition(
+            Eigen::Vector3d targetPosition,
+            double originalSourceIrradiance,
+            Eigen::Vector3d originalSourceToSourceDirection) const override;
+
+    double evaluateIrradianceAtPosition(Eigen::Vector3d targetPosition) const;
 
     std::shared_ptr<LuminosityModel> getLuminosityModel() const
     {
@@ -61,6 +91,162 @@ private:
 
     std::shared_ptr<LuminosityModel> luminosityModel_;
 };
+
+//*********************************************************************************************
+//   Paneled radiation source
+//*********************************************************************************************
+
+class PaneledRadiationSourceModel : public RadiationSourceModel
+{
+public:
+    class Panel;
+    class PanelRadiosityModel; // cannot make this nested class of Panel since forward declaration is impossible
+
+    explicit PaneledRadiationSourceModel(
+            const std::shared_ptr<basic_astrodynamics::BodyShapeModel> sourceBodyShapeModel) :
+        sourceBodyShapeModel_(sourceBodyShapeModel) {}
+
+    explicit PaneledRadiationSourceModel() :
+            PaneledRadiationSourceModel(nullptr) {}
+
+    IrradianceWithSourceList evaluateIrradianceAtPosition(
+            Eigen::Vector3d targetPosition,
+            double originalSourceIrradiance,
+            Eigen::Vector3d originalSourceToSourceDirection) const override;
+
+    virtual std::vector<Panel> getPanels() const = 0;
+
+protected:
+    void updateMembers_(double currentTime) override;
+
+    std::shared_ptr<basic_astrodynamics::BodyShapeModel> sourceBodyShapeModel_;
+};
+
+class StaticallyPaneledRadiationSourceModel : public PaneledRadiationSourceModel
+{
+public:
+    explicit StaticallyPaneledRadiationSourceModel(
+            const std::vector<Panel> panels) :
+            PaneledRadiationSourceModel(),
+            n_(panels.size()),
+            panels_(panels) {}
+
+    explicit StaticallyPaneledRadiationSourceModel(
+            const std::shared_ptr<basic_astrodynamics::BodyShapeModel> sourceBodyShapeModel,
+            const std::function<std::vector<std::shared_ptr<PanelRadiosityModel>>(double, double)> radiosityModelFunction,
+            int n) :
+            PaneledRadiationSourceModel(sourceBodyShapeModel),
+            n_(n),
+            radiosityModelFunction_(radiosityModelFunction) {}
+
+    explicit StaticallyPaneledRadiationSourceModel(
+            const std::shared_ptr<basic_astrodynamics::BodyShapeModel> sourceBodyShapeModel,
+            std::vector<std::shared_ptr<PanelRadiosityModel>> radiosityModel,
+            int n) :
+            StaticallyPaneledRadiationSourceModel(sourceBodyShapeModel, [=](double, double) { return radiosityModel; }, n) {}
+
+    std::vector<Panel> getPanels() const override
+    {
+        return panels_;
+    }
+
+private:
+    void updateMembers_(double currentTime) override;
+
+    unsigned int n_;
+    std::function<std::vector<std::shared_ptr<PanelRadiosityModel>>(double, double)> radiosityModelFunction_;
+    std::vector<Panel> panels_;
+};
+
+class DynamicallyPaneledRadiationSourceModel : public PaneledRadiationSourceModel
+{
+private:
+    // keep target-specific panel lists, otherwise may have to regenerate a lot
+    std::map<std::string, std::vector<Panel>> panels_;
+};
+
+class PaneledRadiationSourceModel::Panel
+{
+public:
+    explicit Panel(
+            double area,
+            const Eigen::Vector3d relativeCenter,
+            const Eigen::Vector3d surfaceNormal,
+            const std::vector<std::shared_ptr<PanelRadiosityModel>> radiosityModels) :
+        area_(area),
+        relativeCenter_(relativeCenter),
+        surfaceNormal_(surfaceNormal),
+        radiosityModels_(radiosityModels) {}
+
+    double getArea() const
+    {
+        return area_;
+    }
+
+    Eigen::Vector3d getRelativeCenter() const
+    {
+        return relativeCenter_;
+    }
+
+    Eigen::Vector3d getSurfaceNormal() const
+    {
+        return surfaceNormal_;
+    }
+
+    std::vector<std::shared_ptr<PanelRadiosityModel>> getRadiosityModels() const
+    {
+        return radiosityModels_;
+    }
+
+private:
+    double area_;
+    Eigen::Vector3d relativeCenter_;
+    Eigen::Vector3d surfaceNormal_;
+    std::vector<std::shared_ptr<PanelRadiosityModel>> radiosityModels_;
+};
+
+class PaneledRadiationSourceModel::PanelRadiosityModel
+{
+public:
+    virtual ~PanelRadiosityModel() = default;
+
+    /*!
+     * Evaluate the irradiance [W/m²] at a certain position due to this panel.
+     * @param panel The panel this radiosity model belongs to
+     * @param targetPosition Position where to evaluate the irradiance in local coordinates (source rotation, centered in panel)
+     * @return The irradiance
+     */
+    virtual double evaluateIrradianceAtPosition(
+            Panel panel,
+            Eigen::Vector3d targetPosition,
+            double originalSourceIrradiance,
+            Eigen::Vector3d originalSourceToSourceDirection) const = 0;
+};
+
+class AlbedoPanelRadiosityModel : public PaneledRadiationSourceModel::PanelRadiosityModel
+{
+public:
+    explicit AlbedoPanelRadiosityModel(
+            const std::shared_ptr<ReflectionLaw> reflectionLaw) :
+            reflectionLaw_(reflectionLaw) {}
+
+    double evaluateIrradianceAtPosition(
+            PaneledRadiationSourceModel::Panel panel,
+            Eigen::Vector3d targetPosition,
+            double originalSourceIrradiance,
+            Eigen::Vector3d originalSourceToSourceDirection) const override;
+
+private:
+    std::shared_ptr<ReflectionLaw> reflectionLaw_;
+};
+
+/*!
+ * Generate evenly spaced points on a sphere using algorithm from Saff (1997).
+ * @param n number of points to generate
+ * @return a pair of vectors, first vector are polar angles, second vector are azimuth angles
+ */
+std::pair<std::vector<double>, std::vector<double>> generateEvenlySpacedPoints(unsigned int n);
+
 
 } // tudat
 } // electromagnetism
--- tests/src/astro/electromagnetism/unitTestRadiationSourceModel.cpp
+++ tests/src/astro/electromagnetism/unitTestRadiationSourceModel.cpp
@@ -12,6 +12,7 @@
 #include "tudat/astro/basic_astro/physicalConstants.h"
 #include "tudat/math/basic/coordinateConversions.h"
 #include "tudat/astro/electromagnetism/radiationSourceModel.h"
+#include "tudat/astro/basic_astro/sphericalBodyShapeModel.h"
 
 
 namespace tudat
@@ -23,12 +24,15 @@ using namespace tudat::electromagnetism;
 
 BOOST_AUTO_TEST_SUITE(test_radiation_source_model)
 
+//*********************************************************************************************
+//   Isotropic point radiation source
+//*********************************************************************************************
+
 //! Test if solar constant and source position is returned when using solar constant-based Sun radiation model
 BOOST_AUTO_TEST_CASE( testIsotropicPointRadiationSourceModel )
 {
     const auto expectedIrradiance = 1360.8;
 
-    auto sourcePosition = Eigen::Vector3d(0, 0, 0);
     auto targetPosition = Eigen::Vector3d(physical_constants::ASTRONOMICAL_UNIT, 0, 0);
 
     auto luminosityModel = std::make_shared<IrradianceBasedLuminosityModel>(
@@ -36,12 +40,9 @@ BOOST_AUTO_TEST_CASE( testIsotropicPointRadiationSourceModel )
     auto radiationSourceModel = std::make_shared<IsotropicPointRadiationSourceModel>(luminosityModel);
     radiationSourceModel->updateMembers(TUDAT_NAN);
 
-    const auto ret = radiationSourceModel->evaluateIrradianceAtPosition(targetPosition).front();
-    const auto actualIrradiance = std::get<0>(ret);
-    const auto actualSourcePosition = std::get<1>(ret);
+    const auto actualIrradiance = radiationSourceModel->evaluateIrradianceAtPosition(targetPosition);
 
     BOOST_CHECK_CLOSE_FRACTION(actualIrradiance, expectedIrradiance, 1.0e-15);
-    TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePosition, sourcePosition, 1.0e-15);
 }
 
 //! Test if isotropic point source is invariant w.r.t. position at given distance
@@ -51,7 +52,7 @@ BOOST_AUTO_TEST_CASE( testIsotropicPointRadiationSourceModelPositionInvariance )
 
     std::vector<double> actualIrradiances;
 
-    const auto radius = 1;
+    const auto radius = 3;
 
     // Iterate over arbitrary values for zenith and azimuth angles
     for (double zenithAngle : {0., 0.743, 1.903, PI})
@@ -64,16 +65,312 @@ BOOST_AUTO_TEST_CASE( testIsotropicPointRadiationSourceModelPositionInvariance )
             auto radiationSourceModel = std::make_shared<IsotropicPointRadiationSourceModel>(luminosityModel);
             radiationSourceModel->updateMembers(TUDAT_NAN);
 
-            const auto ret = radiationSourceModel->evaluateIrradianceAtPosition(targetPosition).front();
-            actualIrradiances.push_back(std::get<0>(ret));
+            const auto actualIrradiance = radiationSourceModel->evaluateIrradianceAtPosition(targetPosition);
+            actualIrradiances.push_back(actualIrradiance);
         }
     }
 
     // Check that all calculated irradiances are identical
-    BOOST_CHECK(std::all_of(
-            actualIrradiances.begin(),
-            actualIrradiances.end(),
-            [&] (double const &e) { return e == actualIrradiances.front(); }));
+    for (auto& e : actualIrradiances)
+    {
+        BOOST_CHECK_CLOSE_FRACTION(e, actualIrradiances.front(), 1e-15);
+    }
+}
+
+//*********************************************************************************************
+//   Paneled radiation source
+//*********************************************************************************************
+
+//! Test generation of evenly spaced points on sphere by comparison with Python implementation
+BOOST_AUTO_TEST_CASE( testStaticallyPaneledRadiationSourceModel_Generation )
+{
+    // First panel is South pole, last panel is North pole
+    const auto expectedSurfaceNormalFirstPanel = -Eigen::Vector3d::UnitZ();
+    const auto expectedSurfaceNormalLastPanel = Eigen::Vector3d::UnitZ();
+
+    const auto n = 10;
+    const auto radius = 1000;
+
+    StaticallyPaneledRadiationSourceModel radiationSourceModel(
+            std::make_shared<basic_astrodynamics::SphericalBodyShapeModel>(radius),
+            std::vector<std::shared_ptr<StaticallyPaneledRadiationSourceModel::PanelRadiosityModel>>(),
+            n);
+    radiationSourceModel.updateMembers(TUDAT_NAN);
+    const auto panels = radiationSourceModel.getPanels();
+
+    BOOST_CHECK_EQUAL(panels.size(), n);
+
+    // Check orientation of first and last panel
+    const auto actualSurfaceNormalFirstPanel = panels.front().getSurfaceNormal();
+    const auto actualSurfaceNormalLastPanel = panels.back().getSurfaceNormal();
+
+    for (int i = 0; i < 3; ++i)
+    {
+        BOOST_CHECK_SMALL(actualSurfaceNormalFirstPanel[i] - expectedSurfaceNormalFirstPanel[i], 1.0e-15);
+        BOOST_CHECK_SMALL(actualSurfaceNormalLastPanel[i] - expectedSurfaceNormalLastPanel[i], 1.0e-15);
+    }
+
+    // Check radial position of all panels
+    for (auto& panel : panels)
+    {
+        BOOST_CHECK_CLOSE(panel.getRelativeCenter().norm(), radius, 1.0e-10);
+    }
+}
+
+//! Test generation of evenly spaced points on sphere by comparison with Python implementation
+BOOST_AUTO_TEST_CASE( testStaticallyPaneledRadiationSourceModel_Albedo_Basic )
+{
+    std::vector<std::shared_ptr<PaneledRadiationSourceModel::PanelRadiosityModel>> fullAlbedoRadiosityModel {
+            std::make_shared<AlbedoPanelRadiosityModel>(
+                    std::make_shared<LambertianReflectionLaw>(1))
+    };
+    std::vector<std::shared_ptr<PaneledRadiationSourceModel::PanelRadiosityModel>> halfAlbedoRadiosityModel {
+            std::make_shared<AlbedoPanelRadiosityModel>(
+                    std::make_shared<LambertianReflectionLaw>(0.5))
+    };
+
+    {
+        // Target in front of panel, 1 away orthogonal to panel, albedo 1
+        const auto expectedIrradiance = 1 / mathematical_constants::PI;
+        const auto expectedSourcePosition = Eigen::Vector3d(1, 0, 0);
+
+        StaticallyPaneledRadiationSourceModel radiationSourceModel({
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePosition,
+                Eigen::Vector3d(1, 0, 0).normalized(),
+                fullAlbedoRadiosityModel)
+        });
+        const auto irradianceList = radiationSourceModel.evaluateIrradianceAtPosition(
+                Eigen::Vector3d(2, 0, 0),
+                1,
+                -Eigen::Vector3d::UnitX());
+
+        BOOST_CHECK_EQUAL(irradianceList.size(), 1);
+
+        const auto actualIrradiance = std::get<0>(irradianceList.front());
+        const auto actualSourcePosition = std::get<1>(irradianceList.front());
+
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiance, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePosition, expectedSourcePosition, 1e-15);
+    }
+
+    {
+        // Target in front of panel, 1 away at 45° angle, area 1, albedo 1
+        const auto expectedIrradiance = 1 / (mathematical_constants::PI * sqrt(2));
+        const auto expectedSourcePosition = Eigen::Vector3d(-1, 0, 0);
+
+        StaticallyPaneledRadiationSourceModel radiationSourceModel({
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePosition,
+                Eigen::Vector3d(1, 0, 0).normalized(),
+                fullAlbedoRadiosityModel)
+        });
+        const auto irradianceList = radiationSourceModel.evaluateIrradianceAtPosition(
+                expectedSourcePosition + Eigen::Vector3d(1, 1, 0).normalized(),
+                1,
+                -Eigen::Vector3d::UnitX());
+
+        BOOST_CHECK_EQUAL(irradianceList.size(), 1);
+
+        const auto actualIrradiance = std::get<0>(irradianceList.front());
+        const auto actualSourcePosition = std::get<1>(irradianceList.front());
+
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiance, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePosition, expectedSourcePosition, 1e-15);
+    }
+
+    {
+        // Panels arranged symmetrically around z axis, angled 45° angle upwards, area 1, albedo 0.5
+        // Target is above at same distance from all, use panel 1 for expected values
+        const auto targetPositionRelativeToPanel = Eigen::Vector3d(-1, 0, 3);
+        const auto cosPanelNormalAndTargetPosition =
+                targetPositionRelativeToPanel.normalized().dot(Eigen::Vector3d(1, 0, 1).normalized());
+        const auto cosPanelNormalAndSourcePosition = 1 / sqrt(2);
+        const auto expectedIrradiance = 0.5 * cosPanelNormalAndSourcePosition * cosPanelNormalAndTargetPosition /
+                (mathematical_constants::PI * targetPositionRelativeToPanel.squaredNorm());
+
+        const auto expectedSourcePositionPanel1 = Eigen::Vector3d(1, 0, -1);
+        const auto expectedSourcePositionPanel2 = Eigen::Vector3d(0, 1, -1);
+        const auto expectedSourcePositionPanel3 = Eigen::Vector3d(-1, 0, -1);
+        const auto expectedSourcePositionPanel4 = Eigen::Vector3d(0, -1, -1);
+
+        StaticallyPaneledRadiationSourceModel radiationSourceModel({
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePositionPanel1,
+                Eigen::Vector3d(1, 0, 1).normalized(),
+                halfAlbedoRadiosityModel),
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePositionPanel2,
+                Eigen::Vector3d(0, 1, 1).normalized(),
+                halfAlbedoRadiosityModel),
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePositionPanel3,
+                Eigen::Vector3d(-1, 0, 1).normalized(),
+                halfAlbedoRadiosityModel),
+            PaneledRadiationSourceModel::Panel(
+                1,
+                expectedSourcePositionPanel4,
+                Eigen::Vector3d(0, -1, 1).normalized(),
+                halfAlbedoRadiosityModel)
+        });
+        const auto irradianceList = radiationSourceModel.evaluateIrradianceAtPosition(
+                Eigen::Vector3d(0, 0, 2),
+                1,
+                -Eigen::Vector3d::UnitZ());
+
+        BOOST_CHECK_EQUAL(irradianceList.size(), 4);
+
+        const auto actualIrradiancePanel1 = std::get<0>(irradianceList[0]);
+        const auto actualSourcePositionPanel1 = std::get<1>(irradianceList[0]);
+        const auto actualIrradiancePanel2 = std::get<0>(irradianceList[1]);
+        const auto actualSourcePositionPanel2 = std::get<1>(irradianceList[1]);
+        const auto actualIrradiancePanel3 = std::get<0>(irradianceList[2]);
+        const auto actualSourcePositionPanel3 = std::get<1>(irradianceList[2]);
+        const auto actualIrradiancePanel4 = std::get<0>(irradianceList[3]);
+        const auto actualSourcePositionPanel4 = std::get<1>(irradianceList[3]);
+
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiancePanel1, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePositionPanel1, expectedSourcePositionPanel1, 1e-15);
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiancePanel2, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePositionPanel2, expectedSourcePositionPanel2, 1e-15);
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiancePanel3, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePositionPanel3, expectedSourcePositionPanel3, 1e-15);
+        BOOST_CHECK_CLOSE_FRACTION(actualIrradiancePanel4, expectedIrradiance, 1e-15);
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(actualSourcePositionPanel4, expectedSourcePositionPanel4, 1e-15);
+    }
+}
+
+//! Test basic cases for paneled albedo source
+BOOST_AUTO_TEST_CASE( testAlbedoPanelRadiosityModel_Basic )
+{
+    const auto radiosityModel = std::make_shared<AlbedoPanelRadiosityModel>(
+            std::make_shared<LambertianReflectionLaw>(1));
+
+    const auto panel = PaneledRadiationSourceModel::Panel(
+            1,
+            Eigen::Vector3d(1, 0, 0), // not used
+            Eigen::Vector3d::UnitX(),
+            { radiosityModel } // not used
+    );
+
+    {
+        // Target in front of panel, 1 away orthogonal to panel
+        const auto expectedReflectedIrradiance = 1 / mathematical_constants::PI;
+        const auto actualReflectedIrradiance = radiosityModel->evaluateIrradianceAtPosition(
+                panel,
+                Eigen::Vector3d::UnitX(),
+                1,
+                -Eigen::Vector3d::UnitX());
+        BOOST_CHECK_CLOSE(actualReflectedIrradiance, expectedReflectedIrradiance, 1e-15);
+    }
+
+    {
+        // Target in front of panel, 1 away at 45° angle
+        const auto expectedReflectedIrradiance = 1 / (mathematical_constants::PI * sqrt(2));
+        const auto actualReflectedIrradiance = radiosityModel->evaluateIrradianceAtPosition(
+                panel,
+                Eigen::Vector3d(1, 1, 0).normalized(),
+                1,
+                -Eigen::Vector3d::UnitX());
+        BOOST_CHECK_CLOSE(actualReflectedIrradiance, expectedReflectedIrradiance, 1e-10);
+    }
+
+    {
+        // Target on back of panel
+        const auto expectedReflectedIrradiance = 0;
+        const auto actualReflectedIrradiance = radiosityModel->evaluateIrradianceAtPosition(
+                panel,
+                -Eigen::Vector3d::UnitX(),
+                1,
+                -Eigen::Vector3d::UnitX());
+        BOOST_CHECK_CLOSE(actualReflectedIrradiance, expectedReflectedIrradiance, 1e-15);
+    }
+
+    {
+        // Original source on back of panel
+        const auto expectedReflectedIrradiance = 0;
+        const auto actualReflectedIrradiance = radiosityModel->evaluateIrradianceAtPosition(
+                panel,
+                Eigen::Vector3d::UnitX(),
+                1,
+                Eigen::Vector3d::UnitX());
+        BOOST_CHECK_CLOSE(actualReflectedIrradiance, expectedReflectedIrradiance, 1e-15);
+    }
+}
+
+//! Test generation of evenly spaced points on sphere by comparison with Python implementation
+BOOST_AUTO_TEST_CASE( testGenerateEvenlySpacedPoints_Values )
+{
+    auto n = 10;
+    const auto pairOfAngleVectors = generateEvenlySpacedPoints(n);
+    const auto polarAngles = std::get<0>(pairOfAngleVectors);
+    const auto azimuthAngles = std::get<1>(pairOfAngleVectors);
+
+    BOOST_CHECK_EQUAL(polarAngles.size(), n);
+    BOOST_CHECK_EQUAL(azimuthAngles.size(), n);
+
+    // Generated with Python script with visually verified results
+    const std::vector<double> actualPolarAngles{
+        3.141592653589793,
+        2.4619188346815495,
+        2.1598272970111707,
+        1.9106332362490186,
+        1.6821373411358607,
+        1.4594553124539327,
+        1.2309594173407747,
+        0.9817653565786227,
+        0.6796738189082441,
+        0.0
+    };
+    const std::vector<double> actualAzimuthAngles{
+        0.0,
+        1.8112150617748297,
+        3.180364954435027,
+        4.387841662284913,
+        5.53335464780712,
+        0.39568232614974086,
+        1.6031590339996273,
+        2.9723089266598244,
+        4.783523988434654,
+        0.0
+    };
+
+    for (int i = 0; i < n; ++i)
+    {
+        BOOST_CHECK_CLOSE(polarAngles[i], actualPolarAngles[i], 1e-15);
+        BOOST_CHECK_CLOSE(azimuthAngles[i], actualAzimuthAngles[i], 1e-15);
+    }
+}
+
+//! Test generation of evenly spaced points on sphere by checking bounds
+BOOST_AUTO_TEST_CASE( testGenerateEvenlySpacedPoints_Bounds )
+{
+    auto n = 100;
+    const auto pairOfAngleVectors = generateEvenlySpacedPoints(n);
+    const auto polarAngles = std::get<0>(pairOfAngleVectors);
+    const auto azimuthAngles = std::get<1>(pairOfAngleVectors);
+
+    BOOST_CHECK_EQUAL(polarAngles.size(), n);
+    BOOST_CHECK_EQUAL(azimuthAngles.size(), n);
+
+    // Check if polar and azimuth angles are valid spherical coordinates
+    for (int i = 0; i < n; ++i)
+    {
+        BOOST_CHECK_GE(polarAngles[i], 0);
+        BOOST_CHECK_LE(polarAngles[i], mathematical_constants::PI);
+
+        BOOST_CHECK_GE(azimuthAngles[i], 0);
+        BOOST_CHECK_LE(azimuthAngles[i], 2*mathematical_constants::PI);
+    }
+
+    // Check if first and last point are poles
+    BOOST_CHECK_CLOSE(polarAngles.front(), mathematical_constants::PI, 1e-15);
+    BOOST_CHECK_CLOSE(polarAngles.back(), 0, 1e-15);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
