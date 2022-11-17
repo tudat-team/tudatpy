b5ec7d471c8061c2c78cc2f12afeed1ebb119e47
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-11-17 16:06:08 +0100

    Added interface functions for constellation dependent variables

--- tudatpy/kernel/expose_numerical_simulation/expose_propagation_setup/expose_dependent_variable_setup.cpp
+++ tudatpy/kernel/expose_numerical_simulation/expose_propagation_setup/expose_dependent_variable_setup.cpp
@@ -660,6 +660,21 @@ namespace dependent_variable {
               py::arg("body_exerting_acceleration"),
               get_docstring("gravity_field_laplacian_of_potential").c_str());
 
+        m.def("minimum_body_distance",
+              &tp::minimumConstellationDistanceDependentVariableSaveSettings,
+              py::arg("body_name"),
+              py::arg("bodies_to_check"),
+              get_docstring("minimum_body_distance").c_str());
+
+        m.def("minimum_visible_station_body_distances",
+              &tp::minimumConstellationStationDistanceDependentVariableSaveSettings,
+              py::arg("body_name"),
+              py::arg("station_name"),
+              py::arg("bodies_to_check"),
+              py::arg("minimum_elevation_angle"),
+              get_docstring("minimum_visible_station_body_distances").c_str());
+
+
     }
 
 }// namespace dependent_variable
