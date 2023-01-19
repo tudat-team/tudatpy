4a8d933893eeb021c2de06a6a19fc014ffb21c26
Dominik Stiller <domi.stiller@gmail.com>
2023-01-19 21:02:32 +0100

    Rename occultation evaluation functions

--- include/tudat/astro/electromagnetism/occultationModel.h
+++ include/tudat/astro/electromagnetism/occultationModel.h
@@ -29,6 +29,11 @@ namespace electromagnetism
 /*!
  * Class modeling the occultation of an occulted body due to occulting bodies as seen from a target position. This class
  * is only aware of the occulting bodies, not the occulted body or target.
+ *
+ * evaluateReceivedFractionFromExtendedSource() is used for occultation of an extended source (e.g., a sphere like the
+ * sun), while evaluateReceivedFractionFromPointSource() is used for occultation of a point source (e.g., the center of
+ * a panel on a paneled source). Therefore, both are used as occultation factor for received irradiance calculations in
+ * the radiation pressure acceleration.
  */
 class OccultationModel
 {
@@ -46,26 +51,27 @@ public:
     void updateMembers(double currentTime);
 
     /*!
-     *  Evaluate how much of the occulted source is visible (i.e. the shadow function).
+     *  Evaluate how much of an occulted extended source is visible (i.e. the shadow function).
      *
      * @param occultedSourcePosition Position of the occulted source in global coordinates
      * @param occultedSourceShapeModel Shape model of the occulted source
      * @param targetPosition Position of the target from which occultation is observed in global coordinates
      * @return Visible fraction of the occulted source (between 0 and 1)
      */
-    virtual double evaluateReceivedFraction(
+    virtual double evaluateReceivedFractionFromExtendedSource(
             const Eigen::Vector3d& occultedSourcePosition,
             const std::shared_ptr<basic_astrodynamics::BodyShapeModel>& occultedSourceShapeModel,
             const Eigen::Vector3d& targetPosition) = 0;
 
     /*!
-     * Evaluate if the source is visible from the target position.
+     * Evaluate how much of an occulted point source is visible (i.e. point-to-point visibility). This function is
+     * commutative w.r.t. sourcePosition and targetPosition.
      *
      * @param sourcePosition Position of the occulted source in global coordinates
      * @param targetPosition Position of the target from which occultation is observed in global coordinates
-     * @return Whether the target can see the source
+     * @return Visible fraction of the occulted source (either 0 and 1)
      */
-    virtual bool evaluateVisibility(
+    virtual double evaluateReceivedFractionFromPointSource(
             const Eigen::Vector3d& sourcePosition,
             const Eigen::Vector3d& targetPosition) = 0;
 
@@ -90,7 +96,7 @@ public:
     explicit NoOccultingBodyOccultationModel() :
             OccultationModel({}) {}
 
-    double evaluateReceivedFraction(
+    double evaluateReceivedFractionFromExtendedSource(
             const Eigen::Vector3d& occultedSourcePosition,
             const std::shared_ptr<basic_astrodynamics::BodyShapeModel>& occultedSourceShapeModel,
             const Eigen::Vector3d& targetPosition) override
@@ -98,16 +104,17 @@ public:
         return 1.0;
     }
 
-    bool evaluateVisibility(
+    double evaluateReceivedFractionFromPointSource(
             const Eigen::Vector3d& sourcePosition,
             const Eigen::Vector3d& targetPosition) override
     {
-        return true;
+        return 1.0;
     }
 };
 
 /*!
- * Occultation model with a single spherical occulting body. This evaluates the standard shadow function.
+ * Occultation model with a single occulting body. This evaluates the standard shadow function. The occulting body
+ * shape is approximated as sphere of its actual shape model's average radius.
  */
 class SingleOccultingBodyOccultationModel : public OccultationModel
 {
@@ -120,14 +127,12 @@ public:
             occultingBodyPositionFunction_(occultingBodyPositionFunction),
             occultingBodyShapeModel_(occultingBodyShapeModel) {}
 
-    // TODO-DOMINIK rename so that is clear both functions do the same thing
-    // e.g. receivedFractionExtendedSource and receivedFractionPointSource
-    double evaluateReceivedFraction(
+    double evaluateReceivedFractionFromExtendedSource(
             const Eigen::Vector3d& occultedSourcePosition,
             const std::shared_ptr<basic_astrodynamics::BodyShapeModel>& occultedSourceShapeModel,
             const Eigen::Vector3d& targetPosition) override;
 
-    bool evaluateVisibility(
+    double evaluateReceivedFractionFromPointSource(
             const Eigen::Vector3d& sourcePosition,
             const Eigen::Vector3d& targetPosition) override;
 
@@ -140,8 +145,8 @@ private:
 };
 
 // TODO Multi-body occultation (DOI: 10.1016/j.asr.2018.02.002)
-// evaluateVisibility() for multiple occulting bodies is just the logical conjunction (AND-ing) of the individual
-// visibilities
+// evaluateReceivedFractionFromPointSource() for multiple occulting bodies is just the logical conjunction (AND-ing) of the
+// individual visibilities
 
 /*!
  * Evaluate whether two points have a line of sight with an occulting spherical body in between. There is a line of
@@ -157,7 +162,7 @@ private:
  * @param targetPosition Position of the target from which occultation is observed in global coordinates
  * @return Whether the target can see the source
  */
-bool evaluateVisibilityWithOccultation(
+bool evaluatePointToPointVisibilityWithOccultation(
         const Eigen::Vector3d& sourcePosition,
         const Eigen::Vector3d& occultingBodyPosition,
         double occultingBodyRadius,
--- tests/src/astro/electromagnetism/unitTestOccultationModel.cpp
+++ tests/src/astro/electromagnetism/unitTestOccultationModel.cpp
@@ -33,62 +33,62 @@ BOOST_AUTO_TEST_SUITE(test_luminosity_model)
 BOOST_AUTO_TEST_CASE( testEvaluateVisibilityWithOccultation )
 {
     // Target behind occulting body
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(3, 1, 0)
-        ));
+    ));
 
     // Target behind occulting body
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(3, 1, 1)
-        ));
+    ));
 
     // Target behind occulting body but to the side
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(3, 2, 0)
-        ));
+    ));
 
     // Target behind occulting body but to the side
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(3, 0, 2)
-        ));
+    ));
 
     // Target in front of occulting body
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0.5, 0, 0)
-        ));
+    ));
 
     // Target inside occulting body but in front of its center
     // (unrealistic but should be visible by convention)
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(1.5, 0, 0)
-        ));
+    ));
 
     // Target inside occulting body but behind its center
     // (unrealistic but should not be visible by convention)
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(2.5, 0, 0)
-        ));
+    ));
 }
 
 // Test inverse cases of previous test to check commutativity
@@ -96,62 +96,62 @@ BOOST_AUTO_TEST_CASE( testEvaluateVisibilityWithOccultation )
 BOOST_AUTO_TEST_CASE( testEvaluateVisibilityWithOccultation_Commutativity )
 {
     // Target behind occulting body
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(3, 1, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target behind occulting body
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(3, 1, 1),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target behind occulting body but to the side
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(3, 2, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target behind occulting body but to the side
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(3, 0, 2),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target in front of occulting body
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(0.5, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target inside occulting body but in front of its center
     // (unrealistic but should be visible by convention)
-    BOOST_CHECK(evaluateVisibilityWithOccultation(
+    BOOST_CHECK(evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(1.5, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 
     // Target inside occulting body but behind its center
     // (unrealistic but should not be visible by convention)
-    BOOST_CHECK(!evaluateVisibilityWithOccultation(
+    BOOST_CHECK(!evaluatePointToPointVisibilityWithOccultation(
             Eigen::Vector3d(2.5, 0, 0),
             Eigen::Vector3d(2, 0, 0),
             1,
             Eigen::Vector3d(0, 0, 0)
-        ));
+    ));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
