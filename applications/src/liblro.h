80edd75c608f79048f7ba55c7486bfba938ab165
Dominik Stiller <domi.stiller@gmail.com>
2023-05-06 11:16:18 +0200

    Extract common LRO functions into library

---
+++ applications/src/liblro.h
@@ -0,0 +1,15 @@
+#ifndef TUDATBUNDLE_LIBLRO_H
+#define TUDATBUNDLE_LIBLRO_H
+
+#include <memory>
+#include <string>
+
+#include "tudat/simulation/propagation_setup/propagationResults.h"
+
+
+void loadLROSpiceKernels();
+void saveSimulationResults(
+        const std::shared_ptr<tudat::propagators::SingleArcSimulationResults<>>& propagationResults,
+        const std::string resultsFolder);
+
+#endif //TUDATBUNDLE_LIBLRO_H
