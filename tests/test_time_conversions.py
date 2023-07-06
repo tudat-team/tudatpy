0ae7a06d0095aca3c95856a1ed0a0dd939be1e2d
Dominic Dirkx <d.dirkx@tudelft.nl>
2023-07-06 16:08:59 +0200

    Correction to time conversions

--- tests/test_time_conversions.py
+++ tests/test_time_conversions.py
@@ -1,5 +1,9 @@
+# import sys
+# sys.path.insert(0, "/home/dominic/Tudat/tudat-bundle/tudat-bundle/cmake-build-default/tudatpy")
+
 from datetime import  datetime
 from tudatpy.kernel.astro import time_conversion
+
 
 def test_datetime_conversions():
 
@@ -10,13 +14,15 @@ def test_datetime_conversions():
 
     while (tudat_datetime_string[-1] == '0'):
         tudat_datetime_string = tudat_datetime_string[:-1]
-    print( tudat_datetime_string )
 
     assert tudat_datetime_string == str( python_datetime )
 
     assert str( python_datetime_reconstructed ) == str( python_datetime )
 
-    assert '2023-06-20 00:05:23.281765' == str( python_datetime )
+    assert '2023-06-20 00:05:23.281765' == str(python_datetime)
 
+    julian_day = 2443494.5
+    tudat_datettime = time_conversion.datetime_to_tudat(time_conversion.julian_day_to_calendar_date(julian_day))
+    assert julian_day == tudat_datettime.julian_day( )
 
 
