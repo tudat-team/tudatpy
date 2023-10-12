935acd69d7a48fb5e18a3e6639289ab7ea3aee24
alopezrivera <antonlopezr99@gmail.com>
2023-10-12 15:56:58 +0200

    BUG SOLVED: ΔV calculation function, porkchop component imports
    - ΔV calculation function default type and default value correctly set
    - Refactored porkchop components to solve import bug

--- tudatpy/kernel_hybrid/trajectory_design/porkchop/__init__.py
+++ tudatpy/kernel_hybrid/trajectory_design/porkchop/__init__.py
@@ -2,4 +2,4 @@
 Tudat Porkchop Tool
 """
 from tudatpy.trajectory_design.porkchop.porkchop import porkchop
-from tudatpy.trajectory_design.porkchop.plot_porkchop import plot_porkchop
+from tudatpy.trajectory_design.porkchop._plot_porkchop import plot_porkchop
