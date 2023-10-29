a9958f2670e725a762ac3167f013dc1cc7331bf1
alopezrivera <antonlopezr99@gmail.com>
2023-10-29 08:45:24 +0100

    Dependent variable dictionary final draft
    - Added asarray function
    - Documentation complete
    - Relocated to hybrid kernel module `numerical_simulation.propagation`
    - Name chosen

---
+++ tudatpy/kernel_hybrid/numerical_simulation/propagation/dependent_variable_dictionary/__init__.py
@@ -0,0 +1,5 @@
+"""
+Semantic variable history retrieval
+-----------------------------------
+"""
+from tudatpy.numerical_simulation.propagation.dependent_variable_dictionary.dependent_variable_dictionary import create_dependent_variable_dictionary, DependentVariableDictionary
