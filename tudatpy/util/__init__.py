c78e2f5266d7934fd12c4efa15b122cf55226681
alopezrivera <antonlopezr99@gmail.com>
2023-11-01 21:32:08 +0100

    Drafted definitive tudatpy module architecture

--- tudatpy/util/__init__.py
+++ tudatpy/util/__init__.py
@@ -1,5 +1,5 @@
-from ._support import *
-from ._dse_functions import *
+from tudatpy.util._support import *
+from tudatpy.util._dse_functions import *
 
 __all__ = ["result2array", "compare_results", "redirect_std", "pareto_optimums", "split_history", "vector2matrix",
            "get_orthogonal_array", "get_yates_array", "anova_analysis"]
