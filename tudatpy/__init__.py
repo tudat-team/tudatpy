4933da8e7f94d4d6a9e18ca864d6c812b123a248
Antonio López Rivera <antonlopezr99@gmail.com>
2023-11-08 20:12:24 +0100

    Definitive exposure+autocompletions for all kernel modules!

--- tudatpy/__init__.py
+++ tudatpy/__init__.py
@@ -1,27 +1,20 @@
+'''
+Copyright (c) 2010-2023, Delft University of Technology
+All rigths reserved
+
+This file is part of the Tudat. Redistribution and use in source and
+binary forms, with or without modification, are permitted exclusively
+under the terms of the Modified BSD license. You should have received
+a copy of the license with this file. If not, please or visit:
+http://tudat.tudelft.nl/LICENSE.
+'''
+
+# Tudatpy version information
 from tudatpy._version import *
-from tudatpy.kernel import constants
-from tudatpy.kernel import astro
-from tudatpy.kernel import interface
-from tudatpy.kernel import math
-from tudatpy.kernel import utils
-from tudatpy.kernel import numerical_simulation
-from tudatpy.kernel import trajectory_design
-#from tudatpy.kernel import io
 
-__all__ = [
-    '__version__',
-    'apps',
-    'bodies',
-    'utils',
-    #'io',
-    'prototype',
-    'constants',
-    'astro',
-    'interface',
-    'kernel',
-    'math',
-    'numerical_simulation'
-]
-
-# Clean up namespace
-# del modify_simulation_setup
+# Import Tudat version information
+from tudatpy.kernel import \
+    _tudat_version, \
+    _tudat_version_major, \
+    _tudat_version_minor, \
+    _tudat_version_patch
