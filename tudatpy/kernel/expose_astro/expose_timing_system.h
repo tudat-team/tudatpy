003d3b437c0e096659f2bd1547c9b108b8d31dfd
kimonito98 <plumarismichael@gmail.com>
2023-12-10 00:02:48 +0100

    exposed clock noise functions

---
+++ tudatpy/kernel/expose_astro/expose_timing_system.h
@@ -0,0 +1,30 @@
+/*    Copyright (c) 2010-2018, Delft University of Technology
+  *    All rights reserved
+  *
+  *    This file is part of the Tudat. Redistribution and use in source and
+  *    binary forms, with or without modification, are permitted exclusively
+  *    under the terms of the Modified BSD license. You should have received
+  *    a copy of the license with this file. If not, please or visit:
+  *    http://tudat.tudelft.nl/LICENSE.
+  */
+
+#ifndef TUDATPY_EXPOSE_TIMING_SYSTEM_H
+#define TUDATPY_EXPOSE_TIMING_SYSTEM_H
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+namespace tudatpy {
+
+    namespace astro {
+        namespace timing_system {
+
+            void expose_timing_system(py::module &m);
+
+        } // namespace timing_system
+    } // namespace astro
+}
+
+
+#endif// TUDATPY_EXPOSE_TIMING_SYSTEM_H
