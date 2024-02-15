0d48e0cbe801f426825788649f705cd4354e44d9
soufyanR1 <s.roubiou@student.tudelft.nl>
2024-02-15 16:00:30 +0100

    Typo in argument, not sure if this needs modification in cpp as well

--- tudatpy/kernel/expose_numerical_simulation/expose_propagation_setup/expose_acceleration_setup.cpp
+++ tudatpy/kernel/expose_numerical_simulation/expose_propagation_setup/expose_acceleration_setup.cpp
@@ -260,7 +260,7 @@ void expose_acceleration_setup(py::module &m) {
           py::arg("time_lag"),
           py::arg("include_direct_radial_component") = true,
           py::arg("use_tide_raised_on_planet") = true,
-          py::arg("explicit_libraional_tide_on_satellite" ) = false,
+          py::arg("explicit_librational_tide_on_satellite" ) = false,
           get_docstring("direct_tidal_dissipation_acceleration").c_str());
 
 //     m.def("direct_tidal_dissipation_acceleration", &tss::directTidalDissipationAccelerationFromInvQ,
