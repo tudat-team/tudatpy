f6680e2da10732042fbc298e5a19d0bb5e1177ea
Tristan <tristan.dijkstra@gmail.com>
2024-02-15 08:17:07 +0100

    add horizons tests for input validation, methods and parameters

--- tests/test_data_horizons.py
+++ tests/test_data_horizons.py
@@ -47,3 +47,201 @@ def test_compare_horizons_spice(pos_tolerance=1e3, vel_tolerance=1e-3):
     assert max_diff_pos < pos_tolerance
     assert max_diff_vel < vel_tolerance
 
+
+targets = [
+    ("433;", "500@399", "J2000", "Earth"),
+    ("299", "500@SSB", "J2000", "SSB"),
+    ("-64", "500@SSB", "J2000", "SSB"),
+    ("C/1995 O1", "500@SSB", "J2000", "SSB"),
+]
+
+user_input = [
+    # start end stepsize + epoch formats
+    (
+        datetime.datetime(2022, 1, 1),
+        datetime.datetime(2022, 2, 1),
+        "5d",
+        None,
+        False,
+        does_not_raise(),
+    ),
+    (
+        datetime.datetime(2022, 1, 1),
+        datetime.datetime(2022, 5, 1),
+        "1m",
+        None,
+        True,
+        does_not_raise(),
+    ),
+    (
+        694267200.000,
+        datetime.datetime(2022, 5, 1),
+        "1m",
+        None,
+        True,
+        does_not_raise(),
+    ),
+    (
+        694267200.000,
+        datetime.datetime(2022, 2, 1),
+        "5d",
+        None,
+        False,
+        does_not_raise(),
+    ),
+    (
+        datetime.datetime(2022, 1, 1),
+        696945600.000,
+        "5d",
+        None,
+        False,
+        does_not_raise(),
+    ),
+    (
+        694267200.000,
+        704635200.000000,
+        "1m",
+        None,
+        True,
+        does_not_raise(),
+    ),
+    # start end num steps
+    (
+        datetime.datetime(2022, 1, 1),
+        datetime.datetime(2022, 2, 1),
+        "20",
+        None,
+        False,
+        does_not_raise(),
+    ),
+    (
+        datetime.datetime(2022, 1, 1),
+        datetime.datetime(2022, 2, 1),
+        "100000",
+        None,
+        True,
+        pytest.raises(NotImplementedError),
+    ),
+    # list of times
+    (
+        None,
+        None,
+        None,
+        [(x * 86400) + 694267200.0 for x in range(40)],
+        False,
+        does_not_raise(),
+    ),
+    (
+        None,
+        None,
+        None,
+        [(x * 86400) + 694267200.0 for x in range(40)],
+        True,
+        does_not_raise(),
+    ),
+    (
+        None,
+        None,
+        None,
+        [(x * 86400) + 694267200.0 for x in range(130)],
+        False,
+        pytest.raises(ValueError),
+    ),
+    (
+        None,
+        None,
+        None,
+        [(x * 86400) + 694267200.0 for x in range(130)],
+        True,
+        does_not_raise(),
+    ),
+]
+
+user_input_short = [
+    (
+        datetime.datetime(2022, 1, 1),
+        datetime.datetime(2022, 2, 1),
+        "5d",
+        None,
+        False,
+        does_not_raise(),
+    ),
+    (
+        None,
+        None,
+        None,
+        [(x * 86400) + 694267200.0 for x in range(90)],
+        True,
+        does_not_raise(),
+    ),
+]
+
+
+@pytest.mark.parametrize("query_id,location,frame_orientation,frame_origin", targets)
+@pytest.mark.parametrize(
+    "epoch_start,epoch_end,epoch_step,epoch_list,extended_query,expectation", user_input
+)
+def test_JPL_user_input(
+    query_id,
+    location,
+    frame_orientation,
+    frame_origin,
+    epoch_start,
+    epoch_end,
+    epoch_step,
+    epoch_list,
+    extended_query,
+    expectation,
+):
+    """Check if input handling is working as expected"""
+    with expectation as exc_info:
+        query = HorizonsQuery(
+            query_id=query_id,
+            location=location,
+            epoch_start=epoch_start,
+            epoch_end=epoch_end,
+            epoch_step=epoch_step,
+            epoch_list=epoch_list,
+            extended_query=extended_query,
+        )
+
+
+@pytest.mark.parametrize("query_id,location,frame_orientation,frame_origin", targets)
+@pytest.mark.parametrize(
+    "epoch_start,epoch_end,epoch_step,epoch_list,extended_query,expectation",
+    user_input_short,
+)
+def test_JPL_methods(
+    query_id,
+    location,
+    frame_orientation,
+    frame_origin,
+    epoch_start,
+    epoch_end,
+    epoch_step,
+    epoch_list,
+    extended_query,
+    expectation,
+):
+    """Check if the HorizonsQuery methods and parameters are working"""
+    with expectation as exc_info:
+        query = HorizonsQuery(
+            query_id=query_id,
+            location=location,
+            epoch_start=epoch_start,
+            epoch_end=epoch_end,
+            epoch_step=epoch_step,
+            epoch_list=epoch_list,
+            extended_query=extended_query,
+        )
+
+        query.cartesian(frame_orientation=frame_orientation)
+        query.create_ephemeris_tabulated(
+            frame_orientation=frame_orientation,
+            frame_origin=frame_origin,
+        )
+        query.interpolated_observations(reference_system=frame_orientation)
+
+        query.MPC_number
+        query.designation
+        query.name
