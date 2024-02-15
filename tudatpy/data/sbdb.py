80cbd73523c32c4a69f54bcc5a117f0da1b09707
Tristan <tristan.dijkstra@gmail.com>
2024-02-15 09:58:47 +0100

    updates to the SBDBquery class

--- tudatpy/data/sbdb.py
+++ tudatpy/data/sbdb.py
@@ -1,47 +1,59 @@
-from astroquery.jplsbdb import SBDB
+from astroquery.jplsbdb import SBDB as astroquerySBDB
 from astropy import units as u
-from typing import Union
+from typing import Any, Union
+
 
 class SBDBquery:
-    """Small-Body Database Query for retrieving various properties of a small body. 
+    """Small-Body Database Query for retrieving various properties of a small body.
     Usefull for retrieving names and masses in conjunction with the MPC module.
     """
-    def __init__(self, MPCcode:Union[str, int]) -> None:
+
+    def __init__(self, MPCcode: Union[str, int], *args, **kwargs) -> None:
         """Create a Small-Body Database Query
+
+        Additional parameters are available through args and kwards, see:
+        https://astroquery.readthedocs.io/en/latest/jplsbdb/jplsbdb.html
 
         Parameters
         ----------
         MPCcode : Union[str, int]
             MPC code for the object.
+
         """
-        self.query = SBDB.query(MPCcode)
+        self.MPCcode = MPCcode
+        self.query = astroquerySBDB.query(MPCcode, phys=True, *args, **kwargs)
 
     def __str__(self) -> str:
-        return SBDB.schematic(self.query)
-    
+        return astroquerySBDB.schematic(self.query)
+
+    def __getitem__(self, index) -> Any:
+        # makes the instance directly subscriptable, i.e.: query["phys_par"]
+        return dict(self.query[index])
+
     @property
     def name(self):
         """Short name in the format: `MPC NAME DESIGNATION`"""
         return self.query["object"]["fullname"]
-    
+
     @property
     def shortname(self):
         """Short name in the format: `MPC NAME`"""
         return self.query["object"]["shortname"]
-    
+
     @property
     def spkid(self):
         """Returns the JPL SPKID, the related codes_300_spkid method returns a modified ID for the Tudat standard kernel"""
         return self.query["object"]["spkid"]
-    
+
     @property
     def codes_300_spkid(self):
-        """Returns spice kernel number for the codes_300ast_20100725.bsp spice kernel. 
+        """Returns spice kernel number for the codes_300ast_20100725.bsp spice kernel.
 
         Some Objects may return a name instead of a number.
         These are objects specifically specified by name in the codes_300ast_20100725.bsp kernel.
-        
-        See https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/asteroids/aa_summaries.txt for a list of exceptions"""
+
+        See https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/asteroids/aa_summaries.txt for a list of exceptions
+        """
         spkid = self.spkid[0] + self.spkid[2:]
         if spkid == "2000001":
             return "Ceres"
@@ -55,8 +67,35 @@ class SBDBquery:
             return "Eros"
         else:
             return spkid
-    
+
     @property
     def gravitational_parameter(self):
-        """Returns the gravitational parameter for the small body"""
-        return self.query["phys_par"]["GM"].to(u.meter**3/u.second**2)
+        """Returns the gravitational parameter for the small body if available"""
+        try:
+            res = self.query["phys_par"]["GM"].to(u.meter**3 / u.second**2)
+            return res.value
+        except Exception as _:
+            raise ValueError(
+                f"Gravitational parameter is not available for object {self.name}"
+            )
+
+    @property
+    def object_info(self):
+        """Returns info about the object, including its designation and orbit class"""
+        return self.query["object"]
+
+    @property
+    def object_classification(self):
+        """Returns the orbit class of the object for example Amor Group for Eros"""
+        return self.object_info["orbit_class"]["name"]
+
+    @property
+    def diameter(self):
+        """Returns diameter of the small body if available"""
+        try:
+            res = self.query["phys_par"]["diameter"].to(u.meter)
+            return res.value
+        except Exception as _:
+            raise ValueError(
+                f"Gravitational parameter is not available for object {self.name}"
+            )
