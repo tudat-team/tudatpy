09cce28771a4c6af9257afa794f374d8b9af2ecf
Antonio López Rivera <antonlopezr99@gmail.com>
2024-07-24 12:58:45 +0200

    Kernel exposure without list of kernel modules not to be exposed
    In the case of conflicting object (modules, classes, functions, variables) names, the `tudatpy` object will be kept. Thus:
    - `tudatpy.kernel.io` vs `tudatpy.io`
        - `tudatpy.io` will point to `tudatpy.io`, and not `tudatpy.kernel.io`
        - This applies to **objects**, **not their children**, so all children of `tudatpy.kernel.io` will become available under `tudatpy.io`, unless `tudatpy.io` already has objects with the same name:
            - `tudatpy.kernel.io.abc`: will become available through `tudatpy.io` as `tudatpy.io.abc`
            - `tudatpy.kernel.io.abc`, **but** an object `tudatpy.io.abc` is defined in the Python `tudatpy.io` module: `tudatpy.io.abc` will point to the Python object. `tudatpy.kernel.io.abc` is not exposed from `tudatpy.io`

--- tudatpy/__init__.py
+++ tudatpy/__init__.py
@@ -29,22 +29,13 @@ from tudatpy.kernel import \
 Expose all tudat kernel modules directly from tudatpy
 """
 
-# Modules which are not to be exposed
-kernel_modules_not_to_be_exposed = [
-    'tudatpy.kernel.io'
-]
-
 # Retrieve all tudat kernel objects
 kernel_objects = {name: object for name, object in sys.modules.items() if name.startswith('tudatpy.kernel')}
 
 # Modify sys.modules so that all kernel objects can be imported directly from tudatpy
 for name, kernel_object in kernel_objects.items():
-    # If the object does not belong to any of the modules not to be exposed
-    if not any([module_not_to_be_exposed in name for module_not_to_be_exposed in kernel_modules_not_to_be_exposed]):
-        new_name = name.replace('tudatpy.kernel', 'tudatpy', 1)
-        # If the new name does not overwrite any existing tudatpy modules
-        if new_name not in sys.modules.keys():
-            # Expose kernel object directly from tudatpy
-            sys.modules[new_name] = kernel_object
-        else:
-            raise ImportError(f'Cannot expose {name} as {new_name}, as {new_name} already exists in sys.modules')
+    new_name = name.replace('tudatpy.kernel', 'tudatpy', 1)
+    # If the new name does not overwrite any existing tudatpy modules
+    if new_name not in sys.modules.keys():
+        # Expose kernel object directly from tudatpy
+        sys.modules[new_name] = kernel_object
