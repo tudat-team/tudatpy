48fe01c6e0ab705323a1f17fdb33a7a83c21b51b
Antonio López Rivera <antonlopezr99@gmail.com>
2024-08-02 17:42:03 +0200

    Removed explicit kernel exposure from gravity_field Python module __init__.py

--- tudatpy/numerical_simulation/environment_setup/gravity_field/__init__.py
+++ tudatpy/numerical_simulation/environment_setup/gravity_field/__init__.py
@@ -2,5 +2,4 @@
 Gravity field module
 """
 
-from tudatpy.kernel.numerical_simulation.environment_setup.gravity_field import *
 from tudatpy.numerical_simulation.environment_setup.gravity_field.sbdb_wrapper import central_sbdb, central_sbdb_density
