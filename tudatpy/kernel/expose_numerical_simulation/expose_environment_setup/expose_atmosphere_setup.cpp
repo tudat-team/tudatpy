188bccc5a0bfe4054d101912f9c896db8a190300
Dominic Dirkx <d.dirkx@tudelft.nl>
2024-09-04 11:52:49 +0200

    Corrected includes

--- tudatpy/kernel/expose_numerical_simulation/expose_environment_setup/expose_atmosphere_setup.cpp
+++ tudatpy/kernel/expose_numerical_simulation/expose_environment_setup/expose_atmosphere_setup.cpp
@@ -16,7 +16,7 @@
 #include <tudat/astro/reference_frames/referenceFrameTransformations.h>
 #include <tudat/simulation/environment_setup.h>
 
-#include "tudatpy/docstrings.h"
+#include "docstrings.h"
 
 // #include <pybind11/chrono.h>
 #include <pybind11/eigen.h>
