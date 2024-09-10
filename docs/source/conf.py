a33d4370e4c5a2dfcb71ad38082208078b52552a
Niket <15953349+niketagrawal@users.noreply.github.com>
2024-09-10 15:20:21 +0200

    Debug: build docs only from the tudatpy dev conda package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("."))
 
 # When the docs are being built on ReadTheDocs, the binaries are in the tudatpy conda package installed in site-packages directory.
 if bool(os.getenv("READTHEDOCS")) is True:
