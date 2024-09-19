d3b2ee61ffecb60df7ce535deb810d699280a932
DominicDirkx <d.dirkx@tudelft.nl>
2024-09-19 21:28:23 +0200

    Update unitTestCovariancePropagation.cpp

--- tests/src/astro/orbit_determination/unitTestCovariancePropagation.cpp
+++ tests/src/astro/orbit_determination/unitTestCovariancePropagation.cpp
@@ -243,21 +243,21 @@ BOOST_AUTO_TEST_CASE( test_EstimationInputAndOutput )
             if( test == 0 )
             {
                 Eigen::MatrixXd manualCovariance = currentStateTransition * estimationOutput->getUnnormalizedCovarianceMatrix( ) * currentStateTransition.transpose( );
-                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( it.second,  manualCovariance, 1.0E-12 );
+                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( it.second,  manualCovariance, 1.0E-10 );
 
                 Eigen::Matrix3d rotationToRsw = reference_frames::getInertialToRswSatelliteCenteredFrameRotationMatrix( stateHistory.at( it.first ) );
                 Eigen::Matrix6d rotationToRswFull = Eigen::Matrix6d::Zero( );
                 rotationToRswFull.block( 0, 0, 3, 3 ) = rotationToRsw;
                 rotationToRswFull.block( 3, 3, 3, 3 ) = rotationToRsw;
                 Eigen::MatrixXd manualRswCovariance = rotationToRswFull * it.second * rotationToRswFull.transpose( );
-                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( rswPropagatedCovariance.at( it.first ),  manualRswCovariance, 1.0E-10 );
+                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( rswPropagatedCovariance.at( it.first ),  manualRswCovariance, 1.0E-9 );
 
                 Eigen::Matrix3d rotationToTnw = reference_frames::getInertialToTnwRotation( stateHistory.at( it.first ) );
                 Eigen::Matrix6d rotationToTnwFull = Eigen::Matrix6d::Zero( );
                 rotationToTnwFull.block( 0, 0, 3, 3 ) = rotationToTnw;
                 rotationToTnwFull.block( 3, 3, 3, 3 ) = rotationToTnw;
                 Eigen::MatrixXd manualTnwCovariance = rotationToTnwFull * it.second * rotationToTnwFull.transpose( );
-                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( tnwPropagatedCovariance.at( it.first ),  manualTnwCovariance, 1.0E-10 );
+                TUDAT_CHECK_MATRIX_CLOSE_FRACTION( tnwPropagatedCovariance.at( it.first ),  manualTnwCovariance, 1.0E-9 );
 
                 Eigen::VectorXd rswFormalError = rswPropagatedCovariance.at( it.first ).diagonal( ).cwiseSqrt( );
                 Eigen::VectorXd tnwFormalError = tnwPropagatedCovariance.at( it.first ).diagonal( ).cwiseSqrt( );
