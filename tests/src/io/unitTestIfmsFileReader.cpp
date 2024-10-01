d3376aa0821b04acb622fa6bb23b0c5c1793c84e
Dominic Dirkx <d.dirkx@tudelft.nl>
2024-10-01 21:47:57 +0200

    Progress towards unifying ODF and IFMS file processing

--- tests/src/io/unitTestIfmsFileReader.cpp
+++ tests/src/io/unitTestIfmsFileReader.cpp
@@ -27,6 +27,7 @@
 
 // Some simplifications for shorter lines
 using namespace tudat::input_output;
+using namespace tudat::basic_astrodynamics;
 using namespace tudat::observation_models;
 using namespace tudat::simulation_setup;
 using namespace tudat;
@@ -59,50 +60,74 @@ BOOST_AUTO_TEST_CASE(testIfmsFileReader)
 
     std::vector< std::shared_ptr< TrackingTxtFileContents > > rawIfmsFiles;
 
-    rawIfmsFiles.push_back( readIfmsFile( "/home/dominic/Downloads/M32ICL2L02_D2X_133630120_00.TAB.txt" ) );
-    rawIfmsFiles.push_back( readIfmsFile( "/home/dominic/Downloads/M32ICL2L02_D2X_133630203_00.TAB.txt" ) );
-    rawIfmsFiles.push_back( readIfmsFile( "/home/dominic/Downloads/M32ICL2L02_D2X_133631902_00.TAB.txt" ) );
-    rawIfmsFiles.push_back( readIfmsFile( "/home/dominic/Downloads/M32ICL2L02_D2X_133632221_00.TAB.txt" ) );
-    rawIfmsFiles.push_back( readIfmsFile( "/home/dominic/Downloads/M32ICL2L02_D2X_133632301_00.TAB.txt" ) );
+    rawIfmsFiles.push_back( readIfmsFile( paths::getTudatTestDataPath( )  + "/estrack_n_way_doppler_observation_model/M32ICL2L02_D2X_133630120_00.TAB.txt" ) );
+    rawIfmsFiles.push_back( readIfmsFile( paths::getTudatTestDataPath( )  + "/estrack_n_way_doppler_observation_model/M32ICL2L02_D2X_133630203_00.TAB.txt" ) );
+    rawIfmsFiles.push_back( readIfmsFile( paths::getTudatTestDataPath( )  + "/estrack_n_way_doppler_observation_model/M32ICL2L02_D2X_133631902_00.TAB.txt" ) );
+
+    std::vector< int > rawIfmsFileSizes = { 2509, 5934, 8460 };
+    std::vector< double > tenthObservation = { 8421021939.244300, 8420857180.689899, 8420892014.600800};
+    std::vector< std::string > tenthObservationTimeUtc = { "2013-12-29T01:20:55.500", "2013-12-29T02:03:50.500", "2013-12-29T19:02:57.500" };
+
+    Eigen::Vector3d stationPosition = getCombinedApproximateGroundStationPositions( ).at( "NWNORCIA" );
+    auto timeConverter = earth_orientation::defaultTimeConverter;
 
     for( unsigned int i = 0; i < rawIfmsFiles.size( ) + 1; i++ )
     {
-        std::vector< std::shared_ptr< ProcessedTrackingTxtFileContents< > > > processedIfmsFiles;
-        std::cout<<i<<std::endl;
+        std::vector< std::shared_ptr< ProcessedTrackingTxtFileContents< double, Time > > > processedIfmsFiles;
         if( i < rawIfmsFiles.size( ) )
         {
             rawIfmsFiles.at( i )->addMetaData( TrackingDataType::receiving_station_name, "NWNORCIA" );
             rawIfmsFiles.at( i )->addMetaData( TrackingDataType::transmitting_station_name, "NWNORCIA" );
-            processedIfmsFiles.push_back( std::make_shared<observation_models::ProcessedTrackingTxtFileContents<> >(
-                rawIfmsFiles.at( i ), "MeX", simulation_setup::getCombinedApproximateGroundStationPositions( )));
+            processedIfmsFiles.push_back( std::make_shared<observation_models::ProcessedTrackingTxtFileContents< double, Time > >(
+                rawIfmsFiles.at( i ), "MeX", simulation_setup::getCombinedApproximateGroundStationPositions( ) ) );
         }
         else
         {
             for( unsigned int j = 0; j < rawIfmsFiles.size( ); j++ )
             {
-                processedIfmsFiles.push_back( std::make_shared<observation_models::ProcessedTrackingTxtFileContents<> >(
-                    rawIfmsFiles.at( j ), "MeX", simulation_setup::getCombinedApproximateGroundStationPositions( )));
+                processedIfmsFiles.push_back( std::make_shared<observation_models::ProcessedTrackingTxtFileContents< double, Time  > >(
+                    rawIfmsFiles.at( j ), "MeX", simulation_setup::getCombinedApproximateGroundStationPositions( ) ) );
             }
         }
 
-
-
         setTrackingDataInformationInBodies( processedIfmsFiles, bodies, { dsn_n_way_averaged_doppler } );
 
-        auto observationCollection = observation_models::createTrackingTxtFilesObservationCollection<double, double>(
+        auto observationCollection = observation_models::createTrackingTxtFilesObservationCollection<double, Time >(
             processedIfmsFiles, { dsn_n_way_averaged_doppler } );
 
 
-        Eigen::VectorXd observationTimes =
-            utilities::convertStlVectorToEigenVector( observationCollection->getConcatenatedTimeVector( ));
+        std::vector< Time > observationTimes = observationCollection->getConcatenatedTimeVector( );
         Eigen::VectorXd observationValues = observationCollection->getObservationVector( );
 
-        std::cout << "TEST: " << observationTimes( 0 ) << " " << observationValues( 1 ) << std::endl;
-//    input_output::writeMatrixToFile( observationValues, "ifms_doppler.dat", 16 );
-//    input_output::writeMatrixToFile( observationTimes, "ifms_times.dat", 16 );
+        if( i < rawIfmsFiles.size( ) )
+        {
+            BOOST_CHECK_EQUAL( observationValues.rows( ), rawIfmsFileSizes.at( i ) );
+            BOOST_CHECK_CLOSE_FRACTION( observationValues( 9 ), tenthObservation.at( i ), std::numeric_limits< double >::epsilon( ) );
+            Time utcTime = timeConverter->getCurrentTime< Time >( tdb_scale, utc_scale, observationTimes.at( 9 ), stationPosition );
+            Time utcTimeTest = timeFromIsoString< Time >( tenthObservationTimeUtc.at( i ) );
+            BOOST_CHECK_SMALL( static_cast< double >( utcTime - utcTimeTest ), 1.0E-12 );
+        }
+        else
+        {
+            BOOST_CHECK_EQUAL( observationValues.rows( ), rawIfmsFileSizes.at( 0 ) + rawIfmsFileSizes.at( 1 ) + rawIfmsFileSizes.at( 2 ) );
+            BOOST_CHECK_CLOSE_FRACTION( observationValues( 9 ), tenthObservation.at( 0 ), std::numeric_limits< double >::epsilon( ) );
+            BOOST_CHECK_CLOSE_FRACTION( observationValues( 9 + rawIfmsFileSizes.at( 0 ) ), tenthObservation.at( 1 ), std::numeric_limits< double >::epsilon( ) );
+            BOOST_CHECK_CLOSE_FRACTION( observationValues( 9 + rawIfmsFileSizes.at( 0 )  + rawIfmsFileSizes.at( 1 ) ), tenthObservation.at( 2 ), std::numeric_limits< double >::epsilon( ) );
 
+            Time utcTime1 =  timeConverter->getCurrentTime< Time >( tdb_scale, utc_scale, observationTimes.at( 9 ), stationPosition );
+            Time utcTimeTest1 = timeFromIsoString< Time >( tenthObservationTimeUtc.at( 0 ) );
+            BOOST_CHECK_SMALL( static_cast< double >( utcTime1 - utcTimeTest1 ), 1.0E-12 );
+
+            Time utcTime2 =  timeConverter->getCurrentTime< Time >( tdb_scale, utc_scale, observationTimes.at( 9 + rawIfmsFileSizes.at( 0 ) ), stationPosition );
+            Time utcTimeTest2 = timeFromIsoString< Time >( tenthObservationTimeUtc.at( 1 ) );
+            BOOST_CHECK_SMALL( static_cast< double >( utcTime2 - utcTimeTest2 ), 1.0E-12 );
+
+            Time utcTime3 =  timeConverter->getCurrentTime< Time >( tdb_scale, utc_scale, observationTimes.at( 9 + rawIfmsFileSizes.at( 0 ) + rawIfmsFileSizes.at( 1 ) ), stationPosition );
+            Time utcTimeTest3 = timeFromIsoString< Time >( tenthObservationTimeUtc.at( 2 ) );
+            BOOST_CHECK_SMALL( static_cast< double >( utcTime3 - utcTimeTest3 ), 1.0E-12 );
+
+        }
     }
-
 }
 
 // End test suite
