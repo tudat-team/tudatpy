5529f673e8789cfb505f22e3bbb27a43b8d8bfed
DominicDirkx <d.dirkx@tudelft.nl>
2024-10-14 13:16:58 +0200

    Update scalarTypes.h
    Putting default types to double

--- tudatpy/kernel/scalarTypes.h
+++ tudatpy/kernel/scalarTypes.h
@@ -13,8 +13,8 @@
 
 #include "tudat/basics/timeType.h"
 
-#define STATE_SCALAR_TYPE long double
-#define TIME_TYPE tudat::Time
-#define INTERPOLATOR_TIME_TYPE tudat::Time
+#define STATE_SCALAR_TYPE double // long double
+#define TIME_TYPE double // tudat::Time
+#define INTERPOLATOR_TIME_TYPE double // tudat::Time
 
 #endif//TUDATPY_SCALAR_TYPES_H
