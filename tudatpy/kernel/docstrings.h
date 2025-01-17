0411ee8a38e96893b5586fa9cbdcdc6d6b1cd043
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-01-17 19:29:20 +0100

    Docs updates

--- tudatpy/kernel/docstrings.h
+++ tudatpy/kernel/docstrings.h
@@ -1302,7 +1302,7 @@ static inline std::string get_docstring(std::string name) {
 
 
 
-    } else if(name == "TerrestrialTimeScaleConverter" ) {
+    } else if(name == "TimeScaleConverter" ) {
             return R"(
 
         Class to convert between different time scales (TAI, TT, TDB, UTC, UT1)
@@ -1322,7 +1322,7 @@ static inline std::string get_docstring(std::string name) {
 
 
 
-    } else if(name == "TerrestrialTimeScaleConverter.convert_time" ) {
+    } else if(name == "TimeScaleConverter.convert_time" ) {
             return R"(
 
         Function to convert an epoch from one time scale to another.
@@ -1353,12 +1353,12 @@ static inline std::string get_docstring(std::string name) {
 
 
 
-    } else if(name == "TerrestrialTimeScaleConverter.get_time_difference" ) {
+    } else if(name == "TimeScaleConverter.get_time_difference" ) {
             return R"(
 
         Function to get the difference in an epoch represented in two different time scales.
 
-        Function to get the difference in an epoch represented in two different time scales. Functionality is as in :func:`TerrestrialTimeScaleConverter.convert_time`, which
+        Function to get the difference in an epoch represented in two different time scales. Functionality is as in :func:`TimeScaleConverter.convert_time`, which
         converts :math:`t_{\text{in}}` to  :math:`t_{\text{out}}`. The present function then returns :math:`t_{\text{out}}-t_{\text{in}}`
 
 
@@ -1387,7 +1387,7 @@ static inline std::string get_docstring(std::string name) {
     }else if(name == "TimeScales") {
          return R"(
 
-        Enumeration of available time scales between which the :class:`~TerrestrialTimeScaleConverter` can automaticaly convert.
+        Enumeration of available time scales between which the :class:`~TimeScaleConverter` can automaticaly convert.
 
      )";
 
@@ -2443,15 +2443,15 @@ DateTime
 
 Function to create a time-scale converter object with default settings. In particular, it uses default settings for conversion between UT1 and UTC:
 
-* Corrections for semi-diurnal variations due to libration for a non-rigid Earth as per Table 5.1b of 2010 IERS Conventions
-* Corrections diurnal and semidiurnal variations due to ocean tides as per Tables 8.2a and 8.2b of the IERS 2010 Conventions
+* Corrections for semi-diurnal variations due to libration for a non-rigid Earth as per Table 5.1b of IERS Conventions 2010
+* Corrections diurnal and semidiurnal variations due to ocean tides as per Tables 8.2a and 8.2b of the IERS Conventions 2010
 * Linear interpolation (correcting for discontunities during days with leap seconds) of daily corrections for UTC-UT1 from the eopc04_14_IAU2000.62-now.txt file in the tudat-resources directory
 
-See :class:`~TerrestrialTimeScaleConverter` for specific functionality and options for time-scale conversions.
+See :class:`~TimeScaleConverter` for specific functionality and options for time-scale conversions.
 
 Returns
 -------
-TerrestrialTimeScaleConverter
+TimeScaleConverter
     Object to convert between different terrestrial time scales.
 
 
@@ -3356,7 +3356,7 @@ Computes rotation matrix between two frames.
 This function computes the rotation matrix
 between two frames at a given time instant. Kernels defining the
 two frames, as well as any required intermediate frames, at the
-requested time must have been loaded. Wrapper for (part of) `pxform_c` spice function.
+requested time must have been loaded. Wrapper for `pxform_c` spice function.
 
 
 Parameters
@@ -16276,7 +16276,7 @@ using full rotation model data from Spice:
         
 Function for creating high-accuracy Earth rotation model settings.
 
-Function for settings object, defining high-accuracy Earth rotation model according to the IERS 2010 Conventions.
+Function for settings object, defining high-accuracy Earth rotation model according to the IERS Conventions 2010.
 This settings class has various options to deviate from the default settings, typical applications will use default.
 Note that for this model the original frame must be J2000 or GCRS (in the case of the former, the frame bias between GCRS and J2000 is automatically corrected for). The target frame (e.g. body-fixed frame) name is ITRS.
 The precession-nutation theory may be any member of :class:`~tudatpy.numerical_simulation.environment_setup.rotation_model.IAUConventions` (``iau_2000a`` / ``iau_2000b`` or ``iau_2006``).
@@ -16302,7 +16302,7 @@ GcrsToItrsRotationModelSettings
 Examples
 --------
 In this example, we create :class:`~tudatpy.numerical_simulation.environment_setup.rotation_model.RotationModelSettings` for Earth,
-using a high-accuracy Earth rotation model as defined by IERS 2010 conventions:
+using a high-accuracy Earth rotation model as defined by IERS Conventions 2010:
 
 
 .. code-block:: python 
@@ -17102,7 +17102,7 @@ static inline std::string get_docstring(std::string name) {
         
 Function for creating basic tidal solid-body shape deformation
 
-Function for creating basic tidal solid-body shape deformation, computing the tidal shape variation due to any number of bodies causing the deformation, and a tidal response define by the deformation Love and Shida numbers :math:`h_{m}` and :math:`l_{m}` (with only :math:`m=2,3` currently supported). This function implements equations (7.5) and (7.6) of the `IERS 2010 Conventions <https://iers-conventions.obspm.fr/conventions_material.php>`_.
+Function for creating basic tidal solid-body shape deformation, computing the tidal shape variation due to any number of bodies causing the deformation, and a tidal response define by the deformation Love and Shida numbers :math:`h_{m}` and :math:`l_{m}` (with only :math:`m=2,3` currently supported). This function implements equations (7.5) and (7.6) of the `IERS Conventions 2010 <https://iers-conventions.obspm.fr/conventions_material.php>`_.
 
 
 Parameters
@@ -17150,7 +17150,7 @@ In this example, we create a settings for degree 2 tidal deformation of the Eart
         
 Function for creating degree 2 basic tidal solid-body shape deformation
 
-Function for creating basic tidal solid-body shape deformation, computing the tidal shape variation due to any number of bodies causing the deformation, and a tidal response define by the deformation Love and Shida numbers :math:`h_{2}` and :math:`l_{2}`. This function implements equations (7.5) of the IERS 2010 Conventions, and provides a simplified interface (for degree 2 only) of :func:`basic_solid_body_tidal`.
+Function for creating basic tidal solid-body shape deformation, computing the tidal shape variation due to any number of bodies causing the deformation, and a tidal response define by the deformation Love and Shida numbers :math:`h_{2}` and :math:`l_{2}`. This function implements equations (7.5) of the IERS Conventions 2010, and provides a simplified interface (for degree 2 only) of :func:`basic_solid_body_tidal`.
 
 
 Parameters
@@ -17200,7 +17200,7 @@ In this example, we create a settings for degree 2 tidal deformation of the Eart
         
 Function for creating full IERS 2010 shape deformation model
 
-Function for creating full IERS 2010 shape deformation model, computing the tidal shape variation due to the full model defined in Section 7.1.1 of the 2010 IERS conventions, implementing Eqs. (7.5)-(7.13), including all terms from Tables 7.3a and 7.3b. At present, none of the input parameters of the model can be varied.
+Function for creating full IERS 2010 shape deformation model, computing the tidal shape variation due to the full model defined in Section 7.1.1 of the IERS Conventions 2010, implementing Eqs. (7.5)-(7.13), including all terms from Tables 7.3a and 7.3b. At present, none of the input parameters of the model can be varied.
 
 Returns
 -------
@@ -27984,6 +27984,7 @@ object as a model for the rotation. The body-fixed frame from which the conversi
 the (assumedly) inertial frame to which the conversion is done is :attr:`~tudatpy.numerical_simulation.environment.RotationalEphemeris.inertial_frame_name`.
 
 This function computes:
+
 .. math::
    \mathbf{r}^{(I)}=\mathbf{R}^{(I/B)}\mathbf{r}^{(B)}+\dot{\mathbf{R}}^{(I/B)}\mathbf{v}^{(B)}
    \mathbf{v}^{(I)}=\mathbf{R}^{(I/B)}\mathbf{v}^{(B)}
