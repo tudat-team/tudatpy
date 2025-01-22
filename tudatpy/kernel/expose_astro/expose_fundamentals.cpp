c1a25f666b092d675b05ffa2b2ac6a7d4565b48a
alfonso <alfonso.sr.trabajo@gmail.com>
2025-01-22 17:54:12 +0100

    Reformated C++

--- tudatpy/kernel/expose_astro/expose_fundamentals.cpp
+++ tudatpy/kernel/expose_astro/expose_fundamentals.cpp
@@ -8,34 +8,31 @@
  *    http://tudat.tudelft.nl/LICENSE.
  */
 
-#include "docstrings.h"
 #include "expose_fundamentals.h"
-
-#include <tudat/astro/basic_astro.h>
 
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
+#include <tudat/astro/basic_astro.h>
+
+#include "docstrings.h"
 
 namespace py = pybind11;
 namespace tmg = tudat::mission_geometry;
 
 namespace tudatpy {
-namespace astro {
-namespace fundamentals {
+    namespace astro {
+        namespace fundamentals {
 
-    void expose_fundamentals(py::module &m) {
+            void expose_fundamentals(py::module &m) {
+                m.def("compute_shadow_function", &tmg::computeShadowFunction,
+                      py::arg("occulted_body_position"),
+                      py::arg("occulted_body_radius"),
+                      py::arg("occulting_body_position"),
+                      py::arg("occulting_body_radius"),
+                      py::arg("satellite_position"),
+                      get_docstring("compute_shadow_function").c_str());
+            }
 
-        m.def("compute_shadow_function",
-              &tmg::computeShadowFunction,
-              py::arg("occulted_body_position"),
-              py::arg("occulted_body_radius"),
-              py::arg("occulting_body_position"),
-              py::arg("occulting_body_radius"),
-              py::arg("satellite_position"),
-              get_docstring("compute_shadow_function").c_str());
-
-    }
-
-} // namespace fundamentals
-} // namespace astro
-} // namespace tudatpy
+        }  // namespace fundamentals
+    }      // namespace astro
+}  // namespace tudatpy
