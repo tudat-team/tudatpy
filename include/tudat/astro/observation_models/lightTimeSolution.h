c7b5e79b47f9029f9642956fe100fe9f1a77008d
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-02-19 21:35:00 +0100

    Moved numerical aprtials into base class; added fudgeb factor in test (for now)

--- include/tudat/astro/observation_models/lightTimeSolution.h
+++ include/tudat/astro/observation_models/lightTimeSolution.h
@@ -235,60 +235,6 @@ public:
         return lightTimeCorrectionFunction_( linkEndsStates, linkEndsTimes, currentMultiLegTransmitterIndex, ancillarySettings );
     }
 
-    //! Function to compute the partial derivative of the light-time correction w.r.t. observation time
-    /*!
-     * Function to compute the partial derivative of the light-time correction w.r.t. observation time. NOTE: FUNCTION IS NOT
-     * YET IMPLEMENTED, EACH OBJECT PRINTS A WARNING ONCE WHEN THIS FUNCTION IS CALLED.
-     * \param transmitterState State of transmitted at transmission time
-     * \param receiverState State of receiver at reception time
-     * \param transmissionTime Time of signal transmission
-     * \param receptionTime Time of singal reception
-     * \param fixedLinkEnd Reference link end for observation
-     * \param linkEndAtWhichPartialIsEvaluated Link end at which the time partial is to be taken
-     * \return Light-time correction w.r.t. observation time
-     */
-    double calculateLightTimeCorrectionPartialDerivativeWrtLinkEndTime( const Eigen::Vector6d& transmitterState,
-                                                                        const Eigen::Vector6d& receiverState,
-                                                                        const double transmissionTime,
-                                                                        const double receptionTime,
-                                                                        const LinkEndType linkEndAtWhichPartialIsEvaluated ) override
-    {
-        if( !isWarningProvided_ )
-        {
-            std::cerr << "Warning, light-time partial not yet implemented in LightTimeCorrectionFunctionWrapper." << std::endl;
-            isWarningProvided_ = true;
-        }
-
-        return 0.0;
-    }
-
-    //! Function to compute the partial derivative of the light-time correction w.r.t. link end position
-    /*!
-     * Function to compute the partial derivative of the light-time correction w.r.t. link end position. NOTE: FUNCTION IS NOT
-     * YET IMPLEMENTED, EACH OBJECT PRINTS A WARNING ONCE WHEN THIS FUNCTION IS CALLED.
-     * \param transmitterState State of transmitted at transmission time
-     * \param receiverState State of receiver at reception time
-     * \param transmissionTime Time of signal transmission
-     * \param receptionTime Time of singal reception
-     * \param linkEndAtWhichPartialIsEvaluated Link end at which the position partial is to be taken
-     * \return Light-time correction w.r.t. link end position
-     */
-    Eigen::Matrix< double, 3, 1 > calculateLightTimeCorrectionPartialDerivativeWrtLinkEndPosition(
-            const Eigen::Vector6d& transmitterState,
-            const Eigen::Vector6d& receiverState,
-            const double transmissionTime,
-            const double receptionTime,
-            const LinkEndType linkEndAtWhichPartialIsEvaluated ) override
-    {
-        if( !isWarningProvided_ )
-        {
-            std::cerr << "Warning, light-time partial not yet implemented in LightTimeCorrectionFunctionWrapper." << std::endl;
-            isWarningProvided_ = true;
-        }
-
-        return Eigen::Matrix< double, 3, 1 >::Zero( );
-    }
-
 private:
     //! Custom light-time correction functions, as a function of transmitter and receiver state and time.
     LightTimeCorrectionFunctionMultiLeg lightTimeCorrectionFunction_;
