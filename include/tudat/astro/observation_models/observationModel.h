13015dd61cc22eeaa8a4768e1728e065e835e275
Valerio <v.filice@tudelft.nl>
2025-03-03 16:14:47 +0100

    Remove range ancillary setting "reference_frequency" and correct the conversion factor computation. The associated unit_test has been updated to reflect the changes.
    Changes:
    - include/tudat/astro/observation_models/dsnNWayRangeObservationModel.h
    - include/tudat/astro/observation_models/observationModel.h
    - include/tudat/simulation/estimation_setup/processOdfFile.h
    - tests/src/astro/observation_models/unitTestProcessOdfData.cpp

--- include/tudat/astro/observation_models/observationModel.h
+++ include/tudat/astro/observation_models/observationModel.h
@@ -37,7 +37,6 @@ enum ObservationAncilliarySimulationVariable {
     reception_reference_frequency_band,
     doppler_integration_time,
     doppler_reference_frequency,
-    sequential_range_reference_frequency,
     sequential_range_lowest_ranging_component,
     range_conversion_factor,
 };
@@ -55,7 +54,6 @@ public:
             case doppler_integration_time:
             case doppler_reference_frequency:
             case reception_reference_frequency_band:
-            case sequential_range_reference_frequency:
             case sequential_range_lowest_ranging_component:
             case range_conversion_factor:
                 doubleData_[ variableType ] = variable;
@@ -94,7 +92,6 @@ public:
                 case doppler_integration_time:
                 case doppler_reference_frequency:
                 case reception_reference_frequency_band:
-                case sequential_range_reference_frequency:
                 case sequential_range_lowest_ranging_component:
                 case range_conversion_factor:
                     returnVariable = doubleData_.at( variableType );
@@ -179,9 +176,6 @@ public:
                 break;
             case reception_reference_frequency_band:
                 name = "DSN reference frequency band at reception";
-                break;
-            case sequential_range_reference_frequency:
-                name = "DSN sequential range reference frequency";
                 break;
             case sequential_range_lowest_ranging_component:
                 name = "DSN sequential range lowest ranging component";
@@ -287,7 +281,6 @@ inline std::shared_ptr< ObservationAncilliarySimulationSettings > getDsnNWayAver
 
 inline std::shared_ptr< ObservationAncilliarySimulationSettings > getDsnNWayRangeAncillarySettings(
         const std::vector< FrequencyBands > &frequencyBands,
-        const double referenceFrequency,
         const double lowestRangingComponent,
         const std::vector< double > linkEndsDelays = std::vector< double >( ) )
 
@@ -296,7 +289,7 @@ inline std::shared_ptr< ObservationAncilliarySimulationSettings > getDsnNWayRang
             std::make_shared< ObservationAncilliarySimulationSettings >( );
 
     ancillarySettings->setAncilliaryDoubleData( sequential_range_lowest_ranging_component, lowestRangingComponent );
-    ancillarySettings->setAncilliaryDoubleData( sequential_range_reference_frequency, referenceFrequency );
+    // ancillarySettings->setAncilliaryDoubleData( sequential_range_reference_frequency, referenceFrequency );
 
     ancillarySettings->setAncilliaryDoubleVectorData( frequency_bands, convertFrequencyBandsToDoubleVector( frequencyBands ) );
     ancillarySettings->setAncilliaryDoubleVectorData( link_ends_delays, linkEndsDelays );
