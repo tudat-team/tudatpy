bead559ac4c141c698c972ba396ea610f3858019
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2025-03-06 00:40:16 +0100

    first minimum working example: generic TudatError

---
+++ include/tudat/basics/tudatExceptions.h
@@ -0,0 +1,36 @@
+/*    Copyright (c) 2010-2025, Delft University of Technology
+ *    All rights reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#ifndef TUDAT_EXCEPTIONS_H
+#define TUDAT_EXCEPTIONS_H
+
+#include <iostream>
+#include <string>
+
+namespace tudat
+{
+
+namespace exceptions
+{
+
+class TudatError : public std::runtime_error
+{
+private:
+    /* data */
+public:
+    TudatError( const std::string& errorMessage ): std::runtime_error( errorMessage ) { }
+    ~TudatError( ) { }
+};
+
+}  // namespace exceptions
+
+}  // namespace tudat
+
+#endif  // TUDAT_EXCEPTIONS_H
