93d9644cd19aaaab211b91b2d8a86a60d4cce2a9
Luigi Gisolfi <141409120+luigigisolfi@users.noreply.github.com>
2025-03-13 09:59:32 +0100

    Update scalarTypes.h

--- tudatpy/kernel/scalarTypes.h
+++ tudatpy/kernel/scalarTypes.h
@@ -14,7 +14,7 @@
 #include "tudat/basics/timeType.h"
 
 #define STATE_SCALAR_TYPE double       // long double
-#define TIME_TYPE double
-#define INTERPOLATOR_TIME_TYPE double
+#define TIME_TYPE double               // tudat::Time
+#define INTERPOLATOR_TIME_TYPE double  // tudat::Time
 
 #endif  // TUDATPY_SCALAR_TYPES_H
