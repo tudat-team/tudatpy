d699b1c6b58a4f4b98e7e2b707c473e151be4a36
alfonso <alfonso.sr.trabajo@gmail.com>
2025-03-18 14:32:02 +0100

    WIP: Fixed dependencies in propagation_setup

--- src/tudatpy/numerical_simulation/environment_setup/__init__.py
+++ src/tudatpy/numerical_simulation/environment_setup/__init__.py
@@ -1,1 +1,15 @@
 from .expose_environment_setup import *
+from . import (
+    aerodynamic_coefficients,
+    atmosphere,
+    ephemeris,
+    gravity_field,
+    gravity_field_variation,
+    ground_station,
+    radiation_pressure,
+    rigid_body,
+    rotation_model,
+    shape,
+    shape_deformation,
+    vehicle_systems,
+)
