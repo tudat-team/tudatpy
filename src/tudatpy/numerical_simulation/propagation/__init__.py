2643390274b1341d70b91e70e8c733ae275b91b4
alfonso <alfonso.sr.trabajo@gmail.com>
2025-03-20 20:27:19 +0100

    Make dependent_variable_dictionary visible again

--- src/tudatpy/numerical_simulation/propagation/__init__.py
+++ src/tudatpy/numerical_simulation/propagation/__init__.py
@@ -1,6 +1,6 @@
 from .expose_propagation import *
 
-# from .dependent_variable_dictionary import (
-#     DependentVariableDictionary,
-#     create_dependent_variable_dictionary,
-# )
+from .dependent_variable_dictionary import (
+    DependentVariableDictionary,
+    create_dependent_variable_dictionary,
+)
