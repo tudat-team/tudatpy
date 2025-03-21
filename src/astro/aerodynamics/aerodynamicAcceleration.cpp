87e63a87c0bc4708d0bcb16c4e956a790939a7b5
DominicDirkx <d.dirkx@tudelft.nl>
2025-03-21 17:07:55 +0100

    Update aerodynamicAcceleration.cpp

--- src/astro/aerodynamics/aerodynamicAcceleration.cpp
+++ src/astro/aerodynamics/aerodynamicAcceleration.cpp
@@ -22,7 +22,8 @@ Eigen::Vector3d computeAerodynamicAcceleration( const double dynamicPressure,
                                                 const Eigen::Vector3d& aerodynamicCoefficients,
                                                 const double vehicleMass )
 {
-    return computeAerodynamicForce( dynamicPressure, referenceArea, aerodynamicCoefficients ) / vehicleMass;
+  
+    return  computeAerodynamicForce( dynamicPressure, referenceArea, aerodynamicCoefficients ) / vehicleMass;
 }
 
 //! Compute the aerodynamic acceleration in same reference frame as input coefficients.
