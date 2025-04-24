1b11217c4fd489549a7f954d408dc4fbcf5bede7
Valerio <v.filice@tudelft.nl>
2025-04-24 14:10:44 +0200

    Update the processTrk234 module to use the new kernel structure
    Changes:
    - modified:   src/tudatpy/data/__init__.py
    - renamed:    tudatpy/data/processTrk234/__init__.py -> src/tudatpy/data/processTrk234/__init__.py
    - renamed:    tudatpy/data/processTrk234/converters/__init__.py -> src/tudatpy/data/processTrk234/converters/__init__.py
    - renamed:    tudatpy/data/processTrk234/converters/converter.py -> src/tudatpy/data/processTrk234/converters/converter.py
    - renamed:    tudatpy/data/processTrk234/converters/derivedDoppler.py -> src/tudatpy/data/processTrk234/converters/derivedDoppler.py
    - renamed:    tudatpy/data/processTrk234/converters/derivedSraRange.py -> src/tudatpy/data/processTrk234/converters/derivedSraRange.py
    - renamed:    tudatpy/data/processTrk234/converters/radioBase.py -> src/tudatpy/data/processTrk234/converters/radioBase.py
    - renamed:    tudatpy/data/processTrk234/converters/ramp.py -> src/tudatpy/data/processTrk234/converters/ramp.py
    - renamed:    tudatpy/data/processTrk234/processor.py -> src/tudatpy/data/processTrk234/processor.py
    - modified:   src/tudatpy/numerical_simulation/environment_setup/ground_station/expose_ground_station.cpp
    - deleted:    tudatpy/kernel/expose_data/expose_processTrk234.cpp
    - deleted:    tudatpy/kernel/expose_data/expose_processtrk234.h

--- src/tudatpy/data/processTrk234/converters/radioBase.py
+++ src/tudatpy/data/processTrk234/converters/radioBase.py
@@ -2,7 +2,8 @@
 Base class for radiometric converters sharing helper functions.
 """
 
-from tudatpy.numerical_simulation.environment import (
+from tudatpy.numerical_simulation.environment_setup.ground_station import (
+    # from tudatpy.numerical_simulation.environment import (
     get_approximate_dsn_ground_station_positions,
 )
 from tudatpy.numerical_simulation.estimation_setup import observation  # type:ignore
