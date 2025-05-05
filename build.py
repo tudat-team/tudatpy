ae2a80f0ed6de39be2663e91156ae236f54448ff
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2025-05-04 22:33:50 -0600

    add comment for -E flag in sphinx-build

--- build.py
+++ build.py
@@ -193,6 +193,7 @@ class Builder:
             api_docs_build_dir = Path(self.build_dir) / "api-docs"
             api_docs_build_dir.mkdir(parents=True, exist_ok=True)
 
+            # -E is added for a clean build, as changes in the compiled kernel are not detected otherwise
             api_docs_build_command = ["sphinx-build", "-b", "html", "tudatpy/docs/source", str(api_docs_build_dir), "-E"]
 
             if self.args.verbose:
