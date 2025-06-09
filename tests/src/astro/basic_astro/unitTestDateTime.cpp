fc6c4dde5009ca6a27a7b1e89b84c6b9a4a394f2
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2025-06-09 13:14:12 -0600

    add debugging outputs, disable unrelated tests

--- tests/src/astro/basic_astro/unitTestDateTime.cpp
+++ tests/src/astro/basic_astro/unitTestDateTime.cpp
@@ -184,6 +184,7 @@ BOOST_AUTO_TEST_CASE( testTimePointConversions )
                     currentDateTime.epoch< double >( ) > DateTime::maximumChronoRepresentableEpoch( ) )
                 {
                     // For the years 2373 and 1621, the date is out of range for std::chrono::system_clock
+                    std::cout << "Epoch out of chrono range: " << currentDateTime.epoch< double >( ) << std::endl;
                     BOOST_CHECK_THROW( currentDateTime.timePoint( ), std::runtime_error );
                     continue;
                 }
