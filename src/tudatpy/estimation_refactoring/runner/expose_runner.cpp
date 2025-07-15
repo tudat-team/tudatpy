46cfa97329a3a2cf01d6b5b352ed882805aa1448
SamFayolle <m.s.fayolle-chambe@tudelft.nl>
2025-07-15 16:29:59 +0200

    prepared bare skelelon for new module architecture + restructured dynamics.environement as an example.

---
+++ src/tudatpy/estimation_refactoring/runner/expose_runner.cpp
@@ -0,0 +1,44 @@
+/*    Copyright (c) 2010-2019, Delft University of Technology
+ *    All rights reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+#define PYBIND11_DETAILED_ERROR_MESSAGES
+#include "expose_runner.h"
+
+#include <pybind11/chrono.h>
+#include <pybind11/eigen.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "scalarTypes.h"
+
+namespace py = pybind11;
+
+namespace tudatpy
+{
+namespace estimation_refactoring
+{
+namespace runner
+{
+
+void expose_runner( py::module& m )
+{
+
+    // ************** Modules ***************
+
+    // TO BE MODIFIED ACCORDINGLY - this was for the observable_models_setup module
+    // auto biases = m.def_submodule( "biases" );
+    // biases::expose_biases( biases );
+
+}
+
+}  // namespace runner
+}  // namespace estimation_refactoring
+}  // namespace tudatpy
