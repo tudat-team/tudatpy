21442b28ebf35cf414e641db073eecc37fcbd319
minervino96 <minervino96@hotmail.it>
2025-07-15 18:52:13 +0200

    (successful) backward compatibility for numerical_simulation.environment

--- src/tudatpy/numerical_simulation/environment/__init__.py
+++ src/tudatpy/numerical_simulation/environment/__init__.py
@@ -1,1 +1,8 @@
-from tudatpy.kernel.numerical_simulation.environment import *
+import warnings
+warnings.warn(
+    "tudatpy.numerical_simulation.environment is deprecated. Use tudatpy.dynamics.environment instead.",
+    FutureWarning,
+    stacklevel=1
+)
+
+from tudatpy.dynamics.environment import *
