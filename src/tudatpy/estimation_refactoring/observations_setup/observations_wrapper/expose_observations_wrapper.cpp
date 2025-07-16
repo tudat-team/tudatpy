f681fc7ff96e229c53d5610e2c076fdfa28aaec5
SamFayolle <m.s.fayolle-chambe@tudelft.nl>
2025-07-16 18:18:49 +0200

    finished redistributing content of numerical_simulation.estimation_setup.observation.

--- src/tudatpy/estimation_refactoring/observations_setup/observations_wrapper/expose_observations_wrapper.cpp
+++ src/tudatpy/estimation_refactoring/observations_setup/observations_wrapper/expose_observations_wrapper.cpp
@@ -12,6 +12,11 @@
 #include <pybind11/functional.h>
 #include "scalarTypes.h"
 
+#include "tudat/simulation/estimation_setup/processOdfFile.h"
+#include "tudat/simulation/estimation_setup/processTrackingTxtFile.h"
+
+namespace tom = tudat::observation_models;
+namespace tss = tudat::simulation_setup;
 
 namespace tudatpy
 {
@@ -25,7 +30,142 @@ namespace observations_wrapper
 
 void expose_observations_wrapper( py::module& m )
 {
-    
+    // Create wrapper function
+    py::cpp_function getDsnDefaultTurnaroundRatios_wrapper = []( tudat::observation_models::FrequencyBands band1,
+                                                                 tudat::observation_models::FrequencyBands band2 ) {
+        return tom::getDsnDefaultTurnaroundRatios( band1, band2 );
+    };
+
+
+    py::class_< tom::ProcessedOdfFileContents< TIME_TYPE >, std::shared_ptr< tom::ProcessedOdfFileContents< TIME_TYPE > > >(
+            m, "ProcessedOdfFileContents", R"doc(No documentation found.)doc" )
+            .def_property_readonly( "ground_station_names",
+                                    &tom::ProcessedOdfFileContents< TIME_TYPE >::getGroundStationsNames,
+                                    R"doc(No documentation found.)doc" )
+            .def_property_readonly( "processed_observable_types",
+                                    &tom::ProcessedOdfFileContents< TIME_TYPE >::getProcessedObservableTypes,
+                                    R"doc(No documentation found.)doc" )
+            .def_property_readonly( "start_and_end_time",
+                                    &tom::ProcessedOdfFileContents< TIME_TYPE >::getStartAndEndTime,
+                                    R"doc(No documentation found.)doc" )
+            .def_property_readonly( "ignored_odf_observable_types",
+                                    &tom::ProcessedOdfFileContents< TIME_TYPE >::getIgnoredRawOdfObservableTypes,
+                                    R"doc(No documentation found.)doc" )
+            .def_property_readonly( "ignored_ground_stations",
+                                    &tom::ProcessedOdfFileContents< TIME_TYPE >::getIgnoredGroundStations,
+                                    R"doc(No documentation found.)doc" )
+            .def_property_readonly(
+                    "raw_odf_data", &tom::ProcessedOdfFileContents< TIME_TYPE >::getRawOdfData, R"doc(No documentation found.)doc" )
+            .def( "define_antenna_id",
+                  py::overload_cast< const std::string&, const std::string& >(
+                          &tom::ProcessedOdfFileContents< TIME_TYPE >::defineSpacecraftAntennaId ),
+                  py::arg( "spacecraft_name" ),
+                  py::arg( "antenna_name" ),
+                  R"doc(No documentation found.)doc" );
+
+    m.def( "process_odf_data_multiple_files",
+           py::overload_cast< const std::vector< std::string >&,
+                              const std::string&,
+                              const bool,
+                              const std::map< std::string, Eigen::Vector3d >& >( &tom::processOdfData< TIME_TYPE > ),
+           py::arg( "file_names" ),
+           py::arg( "spacecraft_name" ),
+           py::arg( "verbose" ) = true,
+           py::arg( "earth_fixed_ground_station_positions" ) = tss::getApproximateDsnGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "process_odf_data_single_file",
+           py::overload_cast< const std::string&, const std::string&, const bool, const std::map< std::string, Eigen::Vector3d >& >(
+                   &tom::processOdfData< TIME_TYPE > ),
+           py::arg( "file_name" ),
+           py::arg( "spacecraft_name" ),
+           py::arg( "verbose" ) = true,
+           py::arg( "earth_fixed_ground_station_positions" ) = tss::getApproximateDsnGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+
+    m.def( "set_odf_information_in_bodies",
+           &tom::setOdfInformationInBodies< TIME_TYPE >,
+           py::arg( "processed_odf_file" ),
+           py::arg( "bodies" ),
+           py::arg( "body_with_ground_stations_name" ) = "Earth",
+           py::arg( "turnaround_ratio_function" ) = getDsnDefaultTurnaroundRatios_wrapper,
+           R"doc(No documentation found.)doc" );
+
+    m.def( "create_odf_observed_observation_collection",
+           &tom::createOdfObservedObservationCollection< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "processed_odf_file" ),
+           py::arg( "observable_types_to_process" ),
+           py::arg( "start_and_end_times_to_process" ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "observations_from_odf_files",
+           &tom::createOdfObservedObservationCollectionFromFile< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "bodies" ),
+           py::arg( "odf_file_names" ),
+           py::arg( "target_name" ),
+           py::arg( "verbose_output" ) = true,
+           py::arg( "earth_fixed_station_positions" ) = tss::getApproximateDsnGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "observations_from_ifms_files",
+           &tom::createIfmsObservedObservationCollectionFromFiles< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "ifms_file_names" ),
+           py::arg( "bodies" ),
+           py::arg( "target_name" ),
+           py::arg( "ground_station_name" ),
+           py::arg( "reception_band" ),
+           py::arg( "transmission_band" ),
+           py::arg( "apply_troposphere_correction" ) = true,
+           py::arg( "earth_fixed_station_positions" ) = tss::getCombinedApproximateGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "observations_from_multi_station_ifms_files",
+           &tom::createMultiStationIfmsObservedObservationCollectionFromFiles< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "ifms_file_names" ),
+           py::arg( "bodies" ),
+           py::arg( "target_name" ),
+           py::arg( "ground_station_names" ),
+           py::arg( "reception_band" ),
+           py::arg( "transmission_band" ),
+           py::arg( "apply_troposphere_correction" ) = true,
+           py::arg( "earth_fixed_station_positions" ) = tss::getCombinedApproximateGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "observations_from_fdets_files",
+           &tom::createFdetsObservedObservationCollectionFromFile< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "ifms_file_name" ),
+           py::arg( "base_frequency" ),
+           py::arg( "column_types" ),
+           py::arg( "target_name" ),
+           py::arg( "transmitting_station_name" ),
+           py::arg( "receiving_station_name" ),
+           py::arg( "reception_band" ),
+           py::arg( "transmission_band" ),
+           py::arg( "earth_fixed_station_positions" ) = tss::getCombinedApproximateGroundStationPositions( ),
+           R"doc(No documentation found.)doc" );
+
+    m.def( "create_compressed_doppler_collection",
+           &tom::createCompressedDopplerCollection< STATE_SCALAR_TYPE, TIME_TYPE >,
+           py::arg( "original_observation_collection" ),
+           py::arg( "compression_ratio" ),
+           py::arg( "minimum_number_of_observations" ) = 10,
+           R"doc(No documentation found.)doc" );
+
+
+    m.def( "create_tracking_txtfile_observation_collection",
+           py::overload_cast< const std::shared_ptr< tudat::input_output::TrackingTxtFileContents >,
+                              const std::string,
+                              const std::vector< tom::ObservableType >,
+                              const std::map< std::string, Eigen::Vector3d >,
+                              const tom::ObservationAncilliarySimulationSettings& >(
+                   &tom::createTrackingTxtFileObservationCollection< double, TIME_TYPE > ),
+           py::arg( "raw_tracking_txtfile_contents" ),
+           py::arg( "spacecraft_name" ),
+           py::arg( "observable_types_to_process" ) = std::vector< tom::ObservableType >( ),
+           py::arg( "earth_fixed_ground_station_positions" ) = tss::getApproximateDsnGroundStationPositions( ),
+           py::arg( "ancillary_settings" ) = tom::ObservationAncilliarySimulationSettings( ),
+           R"doc(No documentation found.)doc" );
 }
 
 }
