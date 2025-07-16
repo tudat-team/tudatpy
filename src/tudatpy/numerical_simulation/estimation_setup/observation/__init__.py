817e7ea52a7d7333cb8d9fd1849b131e260477bd
SamFayolle <m.s.fayolle-chambe@tudelft.nl>
2025-07-16 18:35:59 +0200

    numerical_simulation.estimation.setup.observation now deprecated

--- src/tudatpy/numerical_simulation/estimation_setup/observation/__init__.py
+++ src/tudatpy/numerical_simulation/estimation_setup/observation/__init__.py
@@ -1,1 +1,10 @@
-from tudatpy.kernel.numerical_simulation.estimation_setup.observation import *
+import warnings
+warnings.warn(
+    "tudatpy.numerical_simulation.estimation_setup.observation is deprecated. " \
+    "Use tudatpy.estimation_refactoring.observable_models_setup and/or tudatpy.estimation_refactoring.observations_setup instead.",
+    FutureWarning,
+    stacklevel=1
+)
+
+from tudatpy.kernel.estimation_refactoring.observable_models_setup import *
+from tudatpy.kernel.estimation_refactoring.observations_setup import *
