e72f7fc0f5c8c2bda820e0a2d8748ef4d5e94761
SamFayolle <m.s.fayolle-chambe@tudelft.nl>
2025-07-18 09:51:54 +0200

    everything moved to from numerical_simulation.estimation module

--- src/tudatpy/numerical_simulation/estimation_setup/__init__.py
+++ src/tudatpy/numerical_simulation/estimation_setup/__init__.py
@@ -1,1 +1,18 @@
 from tudatpy.kernel.numerical_simulation.estimation_setup import *
+# this is kept for now because of a few remaining expose functions
+
+import warnings
+warnings.warn(
+    "tudatpy.numerical_simulation.estimation_setup is deprecated. " \
+    "Use tudatpy.estimation_refactoring.observable_models_setup, tudatpy.estimation_refactoring.observable_models, "
+    " tudatpy.estimation_refactoring.observations_setup, tudatpy.estimation_refactoring.observations, " \
+    "and/or tudatpy.estimation_refactoring.estimation_analysis instead.",
+    FutureWarning,
+    stacklevel=1
+)
+
+from tudatpy.kernel.estimation_refactoring.observable_models_setup import *
+from tudatpy.kernel.estimation_refactoring.observable_models import *
+from tudatpy.kernel.estimation_refactoring.observations_setup import *
+from tudatpy.kernel.estimation_refactoring.observations import *
+from tudatpy.kernel.estimation_refactoring.estimation_analysis import *
