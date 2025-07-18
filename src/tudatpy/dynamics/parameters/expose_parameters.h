5f73ac50cfc03cd77edcdfc4c4cea9df082c5e9d
SamFayolle <m.s.fayolle-chambe@tudelft.nl>
2025-07-18 12:09:51 +0200

    created dynamics.parameters and dynamics.parameters_setup submodules.

--- src/tudatpy/dynamics/parameters/expose_parameters.h
+++ src/tudatpy/dynamics/parameters/expose_parameters.h
@@ -1,4 +1,4 @@
-/*    Copyright (c) 2010-2018, Delft University of Technology
+/*    Copyright (c) 2010-2019, Delft University of Technology
  *    All rights reserved
  *
  *    This file is part of the Tudat. Redistribution and use in source and
@@ -8,23 +8,15 @@
  *    http://tudat.tudelft.nl/LICENSE.
  */
 
-#ifndef TUDATPY_EXPOSE_DYNAMICS_H
-#define TUDATPY_EXPOSE_DYNAMICS_H
+#ifndef TUDATPY_EXPOSE_PARAMETERS_H
+#define TUDATPY_EXPOSE_PARAMETERS_H
 
-#include <pybind11/chrono.h>
 #include <pybind11/eigen.h>
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "environment/expose_environment.h"
-#include "environment_setup/expose_environment_setup.h"
-#include "propagation/expose_propagation.h"
-#include "propagation_setup/expose_propagation_setup.h"
-#include "simulator/expose_simulator.h"
-#include "parameters_setup/expose_parameters_setup.h"
-#include "parameters/expose_parameters.h"
 
 namespace py = pybind11;
 
@@ -32,10 +24,13 @@ namespace tudatpy
 {
 namespace dynamics
 {
+namespace parameters
+{
 
-void expose_dynamics( py::module &m );
+void expose_parameters( py::module &m );
 
+}  // namespace parameters
 }  // namespace dynamics
 }  // namespace tudatpy
 
-#endif  // TUDATPY_EXPOSE_DYNAMICS_H
+#endif  // TUDATPY_EXPOSE_PARAMETERS_H
--- src/tudatpy/dynamics/parameters_setup/expose_parameters_setup.cpp
+++ src/tudatpy/dynamics/parameters_setup/expose_parameters_setup.cpp
@@ -1,4 +1,4 @@
-/*    Copyright (c) 2010-2018, Delft University of Technology
+/*    Copyright (c) 2010-2019, Delft University of Technology
  *    All rights reserved
  *
  *    This file is part of the Tudat. Redistribution and use in source and
@@ -7,9 +7,8 @@
  *    a copy of the license with this file. If not, please or visit:
  *    http://tudat.tudelft.nl/LICENSE.
  */
-
-#ifndef TUDATPY_EXPOSE_DYNAMICS_H
-#define TUDATPY_EXPOSE_DYNAMICS_H
+#define PYBIND11_DETAILED_ERROR_MESSAGES
+#include "expose_parameters_setup.h"
 
 #include <pybind11/chrono.h>
 #include <pybind11/eigen.h>
@@ -18,13 +17,9 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "environment/expose_environment.h"
-#include "environment_setup/expose_environment_setup.h"
-#include "propagation/expose_propagation.h"
-#include "propagation_setup/expose_propagation_setup.h"
-#include "simulator/expose_simulator.h"
-#include "parameters_setup/expose_parameters_setup.h"
-#include "parameters/expose_parameters.h"
+#include "scalarTypes.h"
+
+
 
 namespace py = pybind11;
 
@@ -32,10 +27,14 @@ namespace tudatpy
 {
 namespace dynamics
 {
+namespace parameters_setup
+{
 
-void expose_dynamics( py::module &m );
+void expose_parameters_setup( py::module& m )
+{
+ 
+}
 
+}  // namespace parameters_setup
 }  // namespace dynamics
 }  // namespace tudatpy
-
-#endif  // TUDATPY_EXPOSE_DYNAMICS_H
