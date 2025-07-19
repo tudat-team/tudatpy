aaf3f6f1dd4483276fb9dd8238c471e825af8fe7
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2025-07-18 21:51:46 -0600

    fix direct imports from kernel

--- tests/test_tudatpy/test_time_conversions.py
+++ tests/test_tudatpy/test_time_conversions.py
@@ -1,14 +1,14 @@
 import math
 from datetime import datetime
-from tudatpy.kernel.astro import time_conversion
+from tudatpy.astro import time_representation
 
 
 def test_datetime_conversions():
 
     python_datetime = datetime.fromisoformat('2023-06-20T00:05:23.281765')
-    tudat_datetime = time_conversion.datetime_to_tudat( python_datetime )
+    tudat_datetime = time_representation.datetime_to_tudat( python_datetime )
     tudat_datetime_string = tudat_datetime.iso_string( False, 12 )
-    python_datetime_reconstructed = time_conversion.datetime_to_python( tudat_datetime )
+    python_datetime_reconstructed = time_representation.datetime_to_python( tudat_datetime )
 
     while (tudat_datetime_string[-1] == '0'):
         tudat_datetime_string = tudat_datetime_string[:-1]
@@ -20,7 +20,7 @@ def test_datetime_conversions():
     assert '2023-06-20 00:05:23.281765' == str(python_datetime)
 
     julian_day = 2443494.5
-    tudat_datettime = time_conversion.datetime_to_tudat(time_conversion.julian_day_to_calendar_date(julian_day))
+    tudat_datettime = time_representation.datetime_to_tudat(time_representation.julian_day_to_calendar_date(julian_day))
     assert julian_day == tudat_datettime.julian_day( )
 
     # pre_1970_date = datetime(1960,1,1,13,7,23,891234)
