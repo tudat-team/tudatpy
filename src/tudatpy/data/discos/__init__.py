463c8d10029cffb3ee0b4afebb96666153762a22
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-07-25 15:17:46 +0200

    Add tle downloading example

---
+++ src/tudatpy/data/discos/__init__.py
@@ -0,0 +1,5 @@
+from tudatpy.kernel.data import *
+from ._support import save2txt, save_time_history_to_file
+from .mission_data_downloader import LoadPDS
+from .processTrk234 import Trk234Processor
+from . import horizons, mpc, sbdb, spacetrack
