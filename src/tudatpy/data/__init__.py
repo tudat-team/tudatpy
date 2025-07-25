a7c8bc77c189ef4c20095ed5bab569de89dcf714
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-07-25 12:08:11 +0200

    First commit

--- src/tudatpy/data/__init__.py
+++ src/tudatpy/data/__init__.py
@@ -3,3 +3,4 @@ from ._support import save2txt, save_time_history_to_file
 from .mission_data_downloader import LoadPDS
 from .processTrk234 import Trk234Processor
 from . import horizons, mpc, sbdb
+from . import spacetrack, discos
