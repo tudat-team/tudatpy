dd24e93371f4e0cab0cd3fa6b609438c52059264
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-08-04 11:56:05 +0200

    Fix init

--- src/tudatpy/dynamics/environment_setup/ephemeris/__init__.py
+++ src/tudatpy/dynamics/environment_setup/ephemeris/__init__.py
@@ -1,3 +1,3 @@
 from tudatpy.kernel.dynamics.environment_setup.ephemeris import *
 from .horizons_wrapper import jpl_horizons, HorizonsBatch, HorizonsQuery
-from .create_ephemeris_settings import *
+from .create_ephemeris_settings import CreateEphemerisSettings
