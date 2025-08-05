6170ee9efad185b99212170fe0ea46629dabbfef
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-08-05 13:38:34 +0200

    Added discos to init file

--- src/tudatpy/data/__init__.py
+++ src/tudatpy/data/__init__.py
@@ -1,4 +1,4 @@
 from tudatpy.kernel.data import *
 from ._support import save2txt, save_time_history_to_file
 from .processTrk234 import Trk234Processor
-from . import horizons, mpc, sbdb, spacetrack, mission_data_downloader
+from . import horizons, mpc, sbdb, spacetrack, discos, mission_data_downloader
