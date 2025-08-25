194f84794b5272d028114b0f3ad6bcd113090d05
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-08-25 16:32:01 +0200

    Consolidated Mars DTM test

--- tests/test_tudat/src/astro/aerodynamics/unitTestMarsDtmAtmosphere.cpp
+++ tests/test_tudat/src/astro/aerodynamics/unitTestMarsDtmAtmosphere.cpp
@@ -11,8 +11,8 @@
  *
  */
 
-//#define BOOST_TEST_DYN_LINK
-//#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
 
 #include <limits>
 #include "fstream"
@@ -34,7 +34,50 @@
 
 using namespace tudat::aerodynamics;
 
-int main( )
+
+namespace tudat
+{
+namespace unit_tests
+{
+
+    using namespace tudat;
+using namespace aerodynamics;
+using namespace simulation_setup;
+using namespace numerical_integrators;
+using namespace simulation_setup;
+using namespace basic_astrodynamics;
+using namespace propagators;
+using namespace basic_mathematics;
+using namespace basic_astrodynamics;
+
+BOOST_AUTO_TEST_SUITE(test_mars_dtm_atmosphere)
+
+BOOST_AUTO_TEST_CASE(testMarsDtmAtmosphere)
+{
+    // Define tolerance for equality
+    double tolerance = 1.0E-15;
+    std::shared_ptr<AtmosphereSettings> marsDtmAtmosphereSettings;
+    marsDtmAtmosphereSettings = std::make_shared<MarsDtmAtmosphereSettings>();
+    std::shared_ptr<aerodynamics::AtmosphereModel> marsAtmosphereModel = createAtmosphereModel(
+            marsDtmAtmosphereSettings, "Mars");
+    std::shared_ptr<MarsDtmAtmosphereModel> atmosphereModel =
+            std::dynamic_pointer_cast<MarsDtmAtmosphereModel>(
+                    createAtmosphereModel(marsDtmAtmosphereSettings, "Mars"));
+    int alt_km = 400E3;
+    int time = 86400;
+    double latitude = unit_conversions::convertDegreesToRadians(15.0);
+    double longitude = unit_conversions::convertDegreesToRadians(10.0);
+    double rho = atmosphereModel->getDensity(alt_km, longitude, latitude, time);
+
+    // reference density for this specific inputs
+    double rho_ref = 2.57862799E-14;
+    double rho_diff = std::abs(rho - rho_ref);
+    // Check density
+    BOOST_CHECK_SMALL(rho - rho_ref, tolerance);
+}
+
+// Test is Mars DTM model is being used properly (no crashes)
+BOOST_AUTO_TEST_CASE(testMarsDtmAtmosphereInPropagation)
 {
     using namespace tudat;
     using namespace aerodynamics;
@@ -122,21 +165,15 @@ int main( )
     SingleArcDynamicsSimulator<> dynamicsSimulator(
         bodies, translationalPropagatorSettings );
 
+    BOOST_CHECK_EQUAL( dynamicsSimulator.getSingleArcPropagationResults( )->getPropagationIsPerformed( ), true );
+    BOOST_CHECK_EQUAL( dynamicsSimulator.getSingleArcPropagationResults( )->integrationCompletedSuccessfully( ), true );
+
 }
-//
-//namespace tudat
-//{
-//namespace unit_tests
-//{
-//
-//BOOST_AUTO_TEST_SUITE( test_mars_dtm_atmosphere )
-//
-//BOOST_AUTO_TEST_CASE( testMarsDtmAtmosphere )
-//{
-//
-//}
-//
-//BOOST_AUTO_TEST_SUITE_END( )
-//
-//} // namespace unit_tests
-//} // namespace tudat
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} // namespace unit_tests
+
+}//namespace tudat
