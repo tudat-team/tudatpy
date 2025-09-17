1462dc10edb10aff0f865c91614fb7fb391b16ae
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-09-17 09:06:29 +0200

    Modified warnings; added int constructor for Time

--- src/tudatpy/numerical_simulation/propagation/__init__.py
+++ src/tudatpy/numerical_simulation/propagation/__init__.py
@@ -1,6 +1,6 @@
 import warnings
 warnings.warn(
-    "tudatpy.numerical_simulation.propagation is deprecated. Use tudatpy.dynamics.propagation instead.",
+    "tudatpy.numerical_simulation.propagation is deprecated as of v1.0 (see https://docs.tudat.space/en/latest/user-guide/project-updates/migration-guide.html). Use tudatpy.dynamics.propagation instead.",
     FutureWarning,
     stacklevel=1
 )
