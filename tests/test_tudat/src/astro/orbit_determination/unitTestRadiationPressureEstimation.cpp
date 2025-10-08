069d8eb61d6686fef86a3bcc278b9a41a47aa275
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-10-08 20:54:29 +0200

    Tolerance correction

--- tests/test_tudat/src/astro/orbit_determination/unitTestRadiationPressureEstimation.cpp
+++ tests/test_tudat/src/astro/orbit_determination/unitTestRadiationPressureEstimation.cpp
@@ -470,18 +470,18 @@ BOOST_AUTO_TEST_CASE( test_PanelledRadiationPressureEstimation )
         std::shared_ptr< EstimationOutput< double > > estimationOutput = orbitDeterminationManager.estimateParameters( estimationInput );
 
         double rmsResidual = linear_algebra::getVectorEntryRootMeanSquare( observationsAndTimes->getConcatenatedResiduals( ) );
-        BOOST_CHECK_SMALL( rmsResidual, 1.0E-4 );
+        BOOST_CHECK_SMALL( rmsResidual, 1.0E-3 );
         Eigen::VectorXd parameterEstimate = estimationOutput->parameterEstimate_;
         std::cout << "parameter estimate: " << ( parameterEstimate ).transpose( ) << std::endl;
 
         Eigen::VectorXd estimationError = parameterEstimate - truthParameters;
         for( int i = 0; i < 3; i++ )
         {
-            BOOST_CHECK_SMALL( std::fabs( estimationError( i ) ), 2.0E-4 );
-            BOOST_CHECK_SMALL( std::fabs( estimationError( i + 3 ) ), 2.0E-7 );
+            BOOST_CHECK_SMALL( std::fabs( estimationError( i ) ), 5.0E-4 );
+            BOOST_CHECK_SMALL( std::fabs( estimationError( i + 3 ) ), 1.0E-6 );
         }
-        BOOST_CHECK_SMALL( std::fabs( estimationError( 6 ) ), 2.0E-4 );
-        BOOST_CHECK_SMALL( std::fabs( estimationError( 6 ) ), 2.0E-4 );
+        BOOST_CHECK_SMALL( std::fabs( estimationError( 6 ) ), 5.0E-4 );
+        BOOST_CHECK_SMALL( std::fabs( estimationError( 6 ) ), 5.0E-4 );
 
         Eigen::Matrix< double, Eigen::Dynamic, 1 > newTestValues = parametersToEstimate->template getFullParameterValues< double >( );
         std::cout << "TEST C:" << newTestValues.transpose( ) << std::endl;
