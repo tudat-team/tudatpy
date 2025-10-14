14cc5faf2f232ede03a1ab52b8a29b62e6bde5c1
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-10-14 18:05:17 +1100

    Added 80col file parser and improved mpc.py

--- src/tudat/astro/orbit_determination/acceleration_partials/panelledRadiationPressureAccelerationPartial.cpp
+++ src/tudat/astro/orbit_determination/acceleration_partials/panelledRadiationPressureAccelerationPartial.cpp
@@ -130,8 +130,8 @@ void PanelledRadiationPressurePartial::wrtDiffuseReflectivity( Eigen::MatrixXd& 
     if( receivedIrradiance >= 0 )
     {
         Eigen::Vector3d forcePartialWrtDiffuseReflectivity = targetRotationFromLocalToGlobalFrameFunction( ) *
-                panelledTargetModel_->evaluateRadiationPressureForcePartialWrtDiffuseReflectivity( receivedIrradiance,
-                                                                                                   sourceToTargetDirectionLocalFrame );
+                panelledTargetModel_->evaluateRadiationPressureForcePartialWrtDiffuseReflectivity(
+                        receivedIrradiance, sourceToTargetDirectionLocalFrame, panelTypeId );
         partial += forcePartialWrtDiffuseReflectivity / spacecraftMass;
     }
 }
@@ -159,8 +159,8 @@ void PanelledRadiationPressurePartial::wrtSpecularReflectivity( Eigen::MatrixXd&
     if( receivedIrradiance >= 0 )
     {
         Eigen::Vector3d forcePartialWrtSpecularReflectivity = targetRotationFromLocalToGlobalFrameFunction( ) *
-                panelledTargetModel_->evaluateRadiationPressureForcePartialWrtSpecularReflectivity( receivedIrradiance,
-                                                                                                    sourceToTargetDirectionLocalFrame );
+                panelledTargetModel_->evaluateRadiationPressureForcePartialWrtSpecularReflectivity(
+                        receivedIrradiance, sourceToTargetDirectionLocalFrame, panelTypeId );
         partial += forcePartialWrtSpecularReflectivity / spacecraftMass;
     }
 }
