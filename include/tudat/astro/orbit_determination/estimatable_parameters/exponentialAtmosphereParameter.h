f6711de0e8e3b96e31f36a8c9a972021363e401d
Space Jonas <jonas.hener@web.de>
2025-12-03 15:59:07 +0100

    correct store by reference

--- include/tudat/astro/orbit_determination/estimatable_parameters/exponentialAtmosphereParameter.h
+++ include/tudat/astro/orbit_determination/estimatable_parameters/exponentialAtmosphereParameter.h
@@ -36,9 +36,9 @@ public:
      *  \param exponentialAtmosphereModel Class defining properties of exponentialAtmosphereModel used for body.
      *  \param associatedBody Body associated with atmosphere mdoel.
      */
-    ExponentialAtmosphereParameter( std::shared_ptr< aerodynamics::ExponentialAtmosphere > & exponentialAtmosphereModel,
-                                      const estimatable_parameters::EstimatebleParametersEnum parameterType,
-                                      const std::string& associatedBody ):
+    ExponentialAtmosphereParameter( const std::shared_ptr< aerodynamics::ExponentialAtmosphere >& exponentialAtmosphereModel,
+                                    const estimatable_parameters::EstimatebleParametersEnum parameterType,
+                                    const std::string& associatedBody ):
         EstimatableParameter< double >( parameterType, associatedBody ), exponentialAtmosphereModel_( exponentialAtmosphereModel ),
         parameterSize_( 1 )
     {
@@ -47,6 +47,7 @@ public:
             throw std::runtime_error( "Error when creating exponential atmosphere parameter, type is inconsistent: " +
                                       std::to_string( parameterType ) );
         }
+
     }
 
     //! Destructor
@@ -71,7 +72,8 @@ public:
                 break;
             }
             default:
-                break;
+                throw std::runtime_error( "Error when creating exponential atmosphere parameter, type is inconsistent: " +
+                          std::to_string( parameterName_.first ) );
         }
         return parameterValue;
     }
@@ -94,7 +96,8 @@ public:
                 break;
             }
             default:
-                break;
+                throw std::runtime_error( "Error when creating exponential atmosphere parameter, type is inconsistent: " +
+                    std::to_string( parameterName_.first ) );
         }
     }
 
@@ -107,7 +110,7 @@ public:
 
     std::string getParameterDescription( )
     {
-        std::string parameterDescription
+        std::string parameterDescription;
         switch( parameterName_.first )
         {
             case exponential_atmosphere_base_density: {
@@ -133,7 +136,7 @@ public:
 protected:
 private:
     //! Class defining properties of atmosphere model
-    std::shared_ptr< aerodynamics::ExponentialAtmosphere > & exponentialAtmosphereModel_;
+    const std::shared_ptr< aerodynamics::ExponentialAtmosphere > exponentialAtmosphereModel_;
 
     //! Number of rtg acceleration components that are to be estimated.
     int parameterSize_;
@@ -142,82 +145,6 @@ private:
     Eigen::Vector3i accelerationIndices_;
 };
 
-
-//
-////! Interface class for estimation the scale height of a body's exponential atmosphere model
-///*!
-// * Interface class for estimation the scale height of a body's exponential atmosphere model
-// */
-//class ExponentialAtmosphereScaleHeight : public EstimatableParameter< double >
-//{
-//public:
-//    //! Constructor
-//    /*!
-//     *  Constructor
-//     *  \param exponentialAtmosphereModel Class defining properties of exponentialAtmosphereModel used for body.
-//     *  \param associatedBody Body associated with atmosphere mdoel.
-//     */
-//
-//    ExponentialAtmosphereScaleHeight( std::shared_ptr< aerodynamics::ExponentialAtmosphere > & exponentialAtmosphereModel,
-//                                      const std::string& associatedBody ):
-//        EstimatableParameter< double >( exponential_atmosphere_base_density, associatedBody ), exponentialAtmosphereModel_( exponentialAtmosphereModel ),
-//        parameterSize_( 1 )
-//    {}
-//
-//    //! Destructor
-//    ~ExponentialAtmosphereScaleHeight( ) {}
-//
-//    //! Get value of atmosphere scale height
-//    /*!
-//     *  Get value of atmosphere scale height
-//     *  \return Value of atmosphere scale height
-//     */
-//    double getParameterValue( )
-//    {
-//        double parameterValue = exponentialAtmosphereModel_->getScaleHeight( );
-//        return parameterValue;
-//    }
-//
-//    //! Reset value of atmosphere scale height
-//    /*!
-//     *  Reset value of atmosphere scale height
-//     *  \param parameterValue New value of atmosphere scale height
-//     */
-//    void setParameterValue( const double parameterValue )
-//    {
-//        exponentialAtmosphereModel_->resetScaleHeight( parameterValue );
-//    }
-//
-//    //! Function to retrieve the size of the rtg force vector parameter (always 3)
-//
-//    int getParameterSize( )
-//    {
-//        return parameterSize_;
-//    }
-//
-//    std::string getParameterDescription( )
-//    {
-//        std::string parameterDescription = "Scale height of exponential atmosphere model";
-//        return parameterDescription;
-//    }
-//
-//    //! Function to retrieve list of components in rtg accelerations that are to be estimated it.
-//    Eigen::Vector3i getIndices( )
-//    {
-//        return accelerationIndices_;
-//    }
-//
-//protected:
-//private:
-//    //! Class defining properties of atmosphere model
-//    std::shared_ptr< aerodynamics::ExponentialAtmosphere > & exponentialAtmosphereModel_;
-//
-//    //! Number of rtg acceleration components that are to be estimated.
-//    int parameterSize_;
-//
-//    //! List of component indices in rtg accelerations that are to be estimated.
-//    Eigen::Vector3i accelerationIndices_;
-//};
 
 }  // namespace estimatable_parameters
 
