03ef35c021a979d9ccb40c32bac003a7bf77fe37
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2026-01-05 09:16:24 +0100

    Solves issue #593. It fixes discos docstring (query is by discos_id, not norad_id).

--- src/tudatpy/data/discos/discos.py
+++ src/tudatpy/data/discos/discos.py
@@ -13,19 +13,19 @@ class DiscosQuery:
             'DiscosWeb-Api-Version': self.api_version,
         }
 
-    def query_object(self, norad_id, verbose=True):
+    def query_object(self, discos_id, verbose=True):
         """
         Queries the DISCOS database for an object with the specified NORAD ID.
 
         Parameters:
-        - norad_id (int or str): NORAD catalog ID of the satellite
+        - discos_id (int or str): NORAD catalog ID of the satellite
         - verbose (bool): whether to print the results (default True)
 
         Returns:
         - dict: attributes of the queried object (e.g. mass, launch date, etc.)
         """
         response = requests.get(
-            f'{self.url}/api/objects/{norad_id}',
+            f'{self.url}/api/objects/{discos_id}',
             headers=self.headers
         )
 
