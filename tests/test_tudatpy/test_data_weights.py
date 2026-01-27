4612ef20b96a8d86dd98ca3cfbe24395d3baa81f
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2026-01-27 11:55:53 +0100

    Correct test_data_weights

--- tests/test_tudatpy/test_data_weights.py
+++ tests/test_tudatpy/test_data_weights.py
@@ -73,12 +73,12 @@ def test_MPC_weights_to_ObsCol(
     temp_table = (
         batch._table
         .query("observatory != @batch.space_telescopes")
-        .sort_values(["observatory", "epochJ2000secondsTDB"], ascending=True)
+        .sort_values(["observatory", "epoch_seconds_TDB"], ascending=True)
     )
 
     # concatted weights goes [RA1, DEC1, RA2, DEC2, ...]
     batch_weights = np.ravel(2 * [temp_table.weight.to_numpy()], "F")
-    batch_times = np.ravel(2 * [temp_table.epochJ2000secondsTDB.to_numpy()], "F")
+    batch_times = np.ravel(2 * [temp_table.epoch_seconds_TDB.to_numpy()], "F")
 
     # check if lengths match and if the difference is zero
     assert len(batch_weights) == len(observation_collection.concatenated_weights)
