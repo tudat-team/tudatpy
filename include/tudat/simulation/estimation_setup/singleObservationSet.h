1b3873e2b1f251057ff81c74bbd557d2acd5f4ff
Space Jonas <jonas.hener@web.de>
2026-02-13 12:08:06 +0100

    eliminate duplicates in SingleObservationSet construction

--- include/tudat/simulation/estimation_setup/singleObservationSet.h
+++ include/tudat/simulation/estimation_setup/singleObservationSet.h
@@ -48,12 +48,15 @@ public:
                           const std::shared_ptr< ObservationDependentVariableBookkeeping > dependentVariableBookkeeping = nullptr,
                           const std::shared_ptr< observation_models::ObservationAncillarySimulationSettings > ancillarySettings = nullptr,
                           const std::vector< Eigen::Matrix< double, Eigen::Dynamic, 1 > >& weights = { },
-                          const std::vector< Eigen::Matrix< ObservationScalarType, Eigen::Dynamic, 1 > >& residuals = { } ):
+                          const std::vector< Eigen::Matrix< ObservationScalarType, Eigen::Dynamic, 1 > >& residuals = { },
+                          const bool eraseDuplicates = true ):
         observableType_( observableType ), linkEnds_( linkEnds ), observations_( observations ), observationTimes_( observationTimes ),
         referenceLinkEnd_( referenceLinkEnd ), observationsDependentVariables_( observationsDependentVariables ),
         dependentVariableBookkeeping_( dependentVariableBookkeeping ), ancillarySettings_( ancillarySettings ),
         numberOfObservations_( observations_.size( ) ), weights_( weights ), residuals_( residuals )
     {
+
+
         if( dependentVariableBookkeeping_ != nullptr )
         {
             if( dependentVariableBookkeeping_->getObservableType( ) != observableType_ )
@@ -143,6 +146,12 @@ public:
 
         // Sort observations and metadata per observation time
         orderObservationsAndMetadata( );
+
+        // Erase duplicate observations if requested
+        if (eraseDuplicates) {
+            eraseDuplicateObservations( );
+        }
+
 
         // Initialise time bounds
         updateTimeBounds( );
@@ -695,6 +704,35 @@ public:
             counter += 1;
         }
     }
+
+
+    void eraseDuplicateObservations()
+    {
+        std::vector<unsigned int> indicesToRemove;
+
+        // Single pass through sorted observations
+        for(unsigned int i = 1; i < numberOfObservations_; i++)
+        {
+            // Check if current observation time equals previous observation time
+            if(observationTimes_[i] == observationTimes_[i-1])
+            {
+                // Check if observation values are also identical
+                if(observations_[i].isApprox(observations_[i-1]))
+                {
+                    // Mark current observation for removal
+                    indicesToRemove.push_back(i);
+                }
+            }
+        }
+
+        // Remove duplicates if any were found
+        if(indicesToRemove.size() > 0)
+        {
+            removeObservations(indicesToRemove);
+        }
+    }
+
+
 
     void filterObservations( const std::shared_ptr< ObservationFilterBase > observationFilter, const bool saveFilteredObservations = true )
     {
