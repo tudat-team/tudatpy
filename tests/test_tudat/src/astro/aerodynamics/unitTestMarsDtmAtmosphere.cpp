3353ae63886ecf3ab884e9933f66ae7968ded61e
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 20:25:41 +0100

    Migrate includes for test_aerodynamics_MarsDtmAtmosphere

--- tests/test_tudat/src/astro/aerodynamics/unitTestMarsDtmAtmosphere.cpp
+++ tests/test_tudat/src/astro/aerodynamics/unitTestMarsDtmAtmosphere.cpp
@@ -24,7 +24,7 @@
 #include "tudat/astro/aerodynamics/customAerodynamicCoefficientInterface.h"
 #include "tudat/astro/aerodynamics/aerodynamicAcceleration.h"
 #include "tudat/astro/reference_frames/aerodynamicAngleCalculator.h"
-#include "tudat/simulation/propagation_setup/dynamicsSimulator.h"
+#include "tudat/simulation/propagation_setup/singleArcDynamicsSimulator.h"
 #include "tudat/interface/spice/spiceEphemeris.h"
 #include "tudat/interface/spice/spiceRotationalEphemeris.h"
 #include "tudat/io/basicInputOutput.h"
