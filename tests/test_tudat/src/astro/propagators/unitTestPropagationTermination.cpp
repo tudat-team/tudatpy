7b1b7412a861b67bb767e2e0383f75eb4e7205da
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 19:36:32 +0100

    Migrate includes for test_propagators_PropagationTermination

--- tests/test_tudat/src/astro/propagators/unitTestPropagationTermination.cpp
+++ tests/test_tudat/src/astro/propagators/unitTestPropagationTermination.cpp
@@ -11,8 +11,17 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 
-#include "tudat/simulation/estimation.h"
 #include <boost/test/unit_test.hpp>
+
+#include "tudat/interface/spice/spiceInterface.h"
+#include "tudat/astro/basic_astro/physicalConstants.h"
+#include "tudat/math/integrators/createNumericalIntegrator.h"
+#include "tudat/simulation/environment_setup/defaultBodies.h"
+#include "tudat/simulation/environment_setup/createBodies.h"
+#include "tudat/simulation/propagation_setup/accelerationSettings.h"
+#include "tudat/simulation/propagation_setup/createAccelerationModels.h"
+#include "tudat/simulation/propagation_setup/propagationSettings.h"
+#include "tudat/simulation/propagation_setup/singleArcDynamicsSimulator.h"
 
 using namespace tudat;
 using namespace tudat::simulation_setup;
