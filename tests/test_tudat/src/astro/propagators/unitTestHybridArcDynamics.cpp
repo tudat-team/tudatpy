4c8597e1b80ab62d432eab2b85c4fbca834a1a8a
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 19:55:43 +0100

    Migrate includes for test_propagators_HybridArcDynamics

--- tests/test_tudat/src/astro/propagators/unitTestHybridArcDynamics.cpp
+++ tests/test_tudat/src/astro/propagators/unitTestHybridArcDynamics.cpp
@@ -11,9 +11,15 @@
 
 #include "tudat/interface/spice/spiceInterface.h"
 #include "tudat/math/integrators/rungeKuttaCoefficients.h"
+#include "tudat/math/integrators/createNumericalIntegrator.h"
 #include "tudat/astro/basic_astro/accelerationModel.h"
-#include "tudat/simulation/simulation.h"
-#include "tudat/simulation/propagation_setup/dynamicsSimulator.h"
+#include "tudat/simulation/environment_setup/defaultBodies.h"
+#include "tudat/simulation/environment_setup/createBodies.h"
+#include "tudat/simulation/propagation_setup/createAccelerationModels.h"
+#include "tudat/simulation/propagation_setup/propagationSettings.h"
+#include "tudat/simulation/propagation_setup/singleArcDynamicsSimulator.h"
+#include "tudat/simulation/propagation_setup/multiArcDynamicsSimulator.h"
+#include "tudat/simulation/propagation_setup/hybridArcDynamicsSimulator.h"
 
 namespace tudat
 {
