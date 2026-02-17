c7761416216e9df2b87921deda9215ee824be663
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 19:25:46 +0100

    Migrate includes for test_propagators_MultiArcVariationalEquations

--- tests/test_tudat/src/astro/propagators/unitTestMultiArcVariationalEquations.cpp
+++ tests/test_tudat/src/astro/propagators/unitTestMultiArcVariationalEquations.cpp
@@ -27,7 +27,7 @@
 #include "tudat/io/basicInputOutput.h"
 
 #include "tudat/simulation/environment_setup/body.h"
-#include "tudat/simulation/estimation_setup/variationalEquationsSolver.h"
+#include "tudat/simulation/estimation_setup/multiArcVariationalEquationsSolver.h"
 #include "tudat/simulation/environment_setup/defaultBodies.h"
 #include "tudat/simulation/environment_setup/createBodies.h"
 #include "tudat/simulation/estimation_setup/createNumericalSimulator.h"
