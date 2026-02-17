94eb1eaf8bea5077b9529880b1e7d7422d24d78e
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 19:22:02 +0100

    Migrate includes for test_propagators_HybridArcStateTransitionMatrixInterface

--- tests/test_tudat/src/astro/propagators/unitTestHybridArcStateTransitionMatrixInterface.cpp
+++ tests/test_tudat/src/astro/propagators/unitTestHybridArcStateTransitionMatrixInterface.cpp
@@ -17,8 +17,6 @@
 #include <boost/test/unit_test.hpp>
 #include "tudat/basics/testMacros.h"
 
-#include "tudat/simulation/estimation.h"
-
 #include "tudat/math/basic/linearAlgebra.h"
 #include "tudat/astro/basic_astro/physicalConstants.h"
 #include "tudat/astro/basic_astro/unitConversions.h"
@@ -29,7 +27,7 @@
 #include "tudat/io/basicInputOutput.h"
 
 #include "tudat/simulation/environment_setup/body.h"
-#include "tudat/simulation/estimation_setup/variationalEquationsSolver.h"
+#include "tudat/simulation/estimation_setup/hybridArcVariationalEquationsSolver.h"
 #include "tudat/simulation/environment_setup/defaultBodies.h"
 #include "tudat/simulation/environment_setup/createBodies.h"
 #include "tudat/simulation/estimation_setup/createNumericalSimulator.h"
