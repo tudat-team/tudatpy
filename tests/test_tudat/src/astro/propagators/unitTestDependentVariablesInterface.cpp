aaea0863737e87eba794b7ef417ff9056d136eba
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 19:34:09 +0100

    Migrate includes for test_propagators_DependentVariablesInterface

--- tests/test_tudat/src/astro/propagators/unitTestDependentVariablesInterface.cpp
+++ tests/test_tudat/src/astro/propagators/unitTestDependentVariablesInterface.cpp
@@ -25,8 +25,13 @@
 #include "tudat/math/integrators/rungeKuttaCoefficients.h"
 #include "tudat/astro/basic_astro/accelerationModel.h"
 #include "tudat/astro/basic_astro/keplerPropagator.h"
-#include "tudat/simulation/simulation.h"
-#include "tudat/simulation/propagation_setup/dynamicsSimulator.h"
+#include "tudat/simulation/environment_setup/defaultBodies.h"
+#include "tudat/simulation/environment_setup/createBodies.h"
+#include "tudat/simulation/propagation_setup/createAccelerationModels.h"
+#include "tudat/simulation/propagation_setup/propagationSettings.h"
+#include "tudat/simulation/propagation_setup/singleArcDynamicsSimulator.h"
+#include "tudat/simulation/propagation_setup/multiArcDynamicsSimulator.h"
+#include "tudat/simulation/propagation_setup/hybridArcDynamicsSimulator.h"
 #include "tudat/simulation/propagation_setup/dependentVariablesInterface.h"
 
 namespace tudat
