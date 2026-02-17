51557fc97521d1d1dcdd56a7bdefe5da23335b81
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 20:21:23 +0100

    Migrate includes for test_gravitation_SphericalHarmonicsPropagation

--- tests/test_tudat/src/astro/gravitation/unitTestSphericalHarmonicsPropagation.cpp
+++ tests/test_tudat/src/astro/gravitation/unitTestSphericalHarmonicsPropagation.cpp
@@ -27,12 +27,19 @@
 
 #include <boost/test/tools/floating_point_comparison.hpp>
 #include <boost/test/unit_test.hpp>
-#include "tudat/simulation/estimation_setup/variationalEquationsSolver.h"
+#include "tudat/interface/spice/spiceInterface.h"
+#include "tudat/math/integrators/createNumericalIntegrator.h"
+#include "tudat/simulation/environment_setup/defaultBodies.h"
+#include "tudat/simulation/environment_setup/createBodies.h"
+#include "tudat/simulation/environment_setup/createGravityField.h"
+#include "tudat/simulation/propagation_setup/accelerationSettings.h"
+#include "tudat/simulation/propagation_setup/createAccelerationModels.h"
+#include "tudat/simulation/propagation_setup/propagationSettings.h"
+#include "tudat/simulation/estimation_setup/singleArcVariationalEquationsSolver.h"
 #include "tudat/simulation/estimation_setup/createEstimatableParameters.h"
 
 #include "tudat/io/readHistoryFromFile.h"
 #include "tudat/simulation/estimation_setup/simulateObservations.h"
-#include "tudat/simulation/simulation.h"
 #include "tudat/simulation/environment_setup/createGroundStations.h"
 
 namespace tudat
