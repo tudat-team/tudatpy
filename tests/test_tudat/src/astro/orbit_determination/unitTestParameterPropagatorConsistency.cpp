004d6396c91348d394a5562585f59e768dd3eee2
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 20:06:51 +0100

    Migrate includes for test_orbit_determination_ParameterPropagatorConsistency

--- tests/test_tudat/src/astro/orbit_determination/unitTestParameterPropagatorConsistency.cpp
+++ tests/test_tudat/src/astro/orbit_determination/unitTestParameterPropagatorConsistency.cpp
@@ -27,7 +27,6 @@
 #include "tudat/io/basicInputOutput.h"
 
 #include "tudat/simulation/environment_setup/body.h"
-#include "tudat/simulation/estimation_setup/variationalEquationsSolver.h"
 #include "tudat/simulation/environment_setup/defaultBodies.h"
 #include "tudat/simulation/environment_setup/createBodies.h"
 #include "tudat/simulation/estimation_setup/createNumericalSimulator.h"
