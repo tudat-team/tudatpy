e4bcaa196f32476dfd83ae0c750bee50afa3bce1
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 20:05:30 +0100

    Migrate includes for test_orbit_determination_CustomAccelerationVariationalEquations

--- tests/test_tudat/src/astro/orbit_determination/unitTestCustomAccelerationVariationalEquations.cpp
+++ tests/test_tudat/src/astro/orbit_determination/unitTestCustomAccelerationVariationalEquations.cpp
@@ -19,7 +19,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "tudat/basics/testMacros.h"
-#include "tudat/simulation/estimation_setup/variationalEquationsSolver.h"
+#include "tudat/simulation/estimation_setup/singleArcVariationalEquationsSolver.h"
 #include "tudat/simulation/estimation_setup/createEstimatableParameters.h"
 #include "tudat/astro/propagators/propagateCovariance.h"
 #include "tudat/simulation/environment_setup/createGroundStations.h"
