700ba2ff2fca25cff5ec5d069bc2a108006004fa
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-02-17 21:03:17 +0100

    Migrate includes for test_observation_models_ObservationViabilityCalculators

--- tests/test_tudat/src/astro/observation_models/unitTestObservationViabilityCalculators.cpp
+++ tests/test_tudat/src/astro/observation_models/unitTestObservationViabilityCalculators.cpp
@@ -19,7 +19,8 @@
 #include "tudat/basics/testMacros.h"
 
 #include "tudat/astro/ground_stations/pointingAnglesCalculator.h"
-#include "tudat/simulation/estimation.h"
+#include "tudat/simulation/estimation_setup.h"
+#include "tudat/simulation/estimation_setup/simulateObservations.h"
 
 namespace tudat
 {
