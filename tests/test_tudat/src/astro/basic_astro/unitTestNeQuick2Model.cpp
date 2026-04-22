cfce5de27f381f132864077400419ab0ed395770
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-04-22 08:53:07 +0200

    Corrected test data

--- tests/test_tudat/src/astro/basic_astro/unitTestNeQuick2Model.cpp
+++ tests/test_tudat/src/astro/basic_astro/unitTestNeQuick2Model.cpp
@@ -30,7 +30,7 @@ namespace unit_tests
 {
 
 // Path to NeQuick-2 data (bundled in tudat resources)
-const std::string neQuick2DataPath = tudat::paths::getTudatTestDataPath( ) + "/nequick2/";
+const std::string neQuick2DataPath = tudat::paths::getTudatTestDataPath( ) + "nequick2/";
 
 BOOST_AUTO_TEST_SUITE( test_nequick2_model )
 
