f6f774861c3bccc21b58a621b1431e0d313eabb3
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-04-22 09:47:05 +0200

    Correct merge

--- include/tudat/simulation/environment_setup/createBodiesFactory.h
+++ include/tudat/simulation/environment_setup/createBodiesFactory.h
@@ -126,7 +126,7 @@ SystemOfBodies createSystemOfBodies( const BodyListSettings& bodySettings )
         {
             bodyList.at( orderedBodySettings.at( i ).first )
                     ->setAtmosphereModel( createAtmosphereModel( orderedBodySettings.at( i ).second->atmosphereSettings,
-                                                                 orderedBodySettings.at( i ).first ) );
+                                                                 orderedBodySettings.at( i ).first, bodyList ) );
         }
     }
 
