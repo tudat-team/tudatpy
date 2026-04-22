850a7354c7800d4a0ce16569cb87225af2590bec
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2026-04-22 18:28:52 +0200

    update DSN station positions to Revision O (2024) of DSN 810-005, 301 Coverage and Geometry

--- include/tudat/simulation/environment_setup/defaultGroundStationSettings.h
+++ include/tudat/simulation/environment_setup/defaultGroundStationSettings.h
@@ -29,7 +29,7 @@ class GroundStationSettings;
 /*!
  * Returns a map with the approximate positions of the DSN ground stations, having as key the ground station names. The
  * ground stations are named "DSS-id". The ground station positions are selected according to table 2 of DSN 810-005,
- * 301 Coverage and Geometry, Revision K (2016), DSN/JPL. The positions of the ground stations are specified at 2003.0
+ * 301 Coverage and Geometry, Revision O (2024), DSN/JPL. The positions of the ground stations are specified at 2003.0
  * with respect to ITRF93.
  *
  * @return Map with the ground station positions
@@ -50,7 +50,7 @@ inline std::map< int, std::vector< std::string > > getDefaultDsnStationNamesPerC
         "DSS-34", "DSS-35", "DSS-36", "DSS-43", "DSS-45"
     };  // DSS-47 is technically from different complex (ATAC Narrabri, not Canberra), but could be registered in this list too, since on
         // same plate...
-    stationsPerComplex[ 60 ] = { "DSS-54", "DSS-55", "DSS-63", "DSS-65" };
+    stationsPerComplex[ 60 ] = { "DSS-53", "DSS-54", "DSS-55", "DSS-56", "DSS-63", "DSS-65" };
 
     return stationsPerComplex;
 }
@@ -98,7 +98,6 @@ std::vector< std::shared_ptr< GroundStationSettings > > getEvnStationSettings( )
 std::vector< std::shared_ptr< GroundStationSettings > > getMPCStationSettings( );
 
 std::vector< std::shared_ptr< GroundStationSettings > > getRadioTelescopeStationSettings( );
-
 
 std::string getDefaultIlrsSinexStateFilePath( );
 
