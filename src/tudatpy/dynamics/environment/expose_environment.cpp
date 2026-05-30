714d3446105a201ed22c3f990430335272db9777
Diogo Spínola <diogogabrielspinola@gmail.com>
2026-05-30 17:14:40 +0200

    Use existing tudat function to convert quaternion to array

--- src/tudatpy/dynamics/environment/expose_environment.cpp
+++ src/tudatpy/dynamics/environment/expose_environment.cpp
@@ -48,6 +48,7 @@
 #include <tudat/astro/reference_frames/aerodynamicAngleCalculator.h>
 #include <tudat/astro/reference_frames/referenceFrameTransformations.h>
 #include <tudat/basics/deprecationWarnings.h>
+#include <tudat/math/basic/linearAlgebra.h>
 
 #include "scalarTypes.h"
 
@@ -2584,7 +2585,7 @@ bool
                     "quaternion",
                     []( const tsm::Camera& self ) -> Eigen::Vector4d {
                         Eigen::Quaterniond q = self.getRotationFromBodyFixedToCameraFrame( );
-                        return Eigen::Vector4d( q.w( ), q.x( ), q.y( ), q.z( ) );
+                        return tudat::linear_algebra::convertQuaternionToVectorFormat(q);
                     },
                     R"doc(
             **read-only**
