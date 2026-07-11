61b1cb734774e6c03eb9a5a3fa6bedc256b11830
PascalChristiaanse <pascal.christiaanse@gmail.com>
2026-07-11 23:24:46 +0200

    format

--- tests/test_tudat/src/io/unitTestObservationCollectionSerialization.cpp
+++ tests/test_tudat/src/io/unitTestObservationCollectionSerialization.cpp
@@ -56,12 +56,11 @@ std::shared_ptr< observation_models::SingleObservationSet< double, double > > cr
     }
 
     return std::make_shared< observation_models::SingleObservationSet< double, double > >(
-                observableType, linkEnds, observations, observationTimes, observation_models::receiver );
+            observableType, linkEnds, observations, observationTimes, observation_models::receiver );
 }
 
-void compareObservationCollections(
-    observation_models::ObservationCollection< double, double >& left,
-    observation_models::ObservationCollection< double, double >& right )
+void compareObservationCollections( observation_models::ObservationCollection< double, double >& left,
+                                    observation_models::ObservationCollection< double, double >& right )
 {
     BOOST_CHECK_EQUAL( left.getTotalObservableSize( ), right.getTotalObservableSize( ) );
     BOOST_CHECK_EQUAL( left.getConcatenatedObservationTimes( ).size( ), right.getConcatenatedObservationTimes( ).size( ) );
@@ -72,7 +71,8 @@ void compareObservationCollections(
     BOOST_REQUIRE_EQUAL( leftObservationVector.size( ), rightObservationVector.size( ) );
     for( int i = 0; i < leftObservationVector.size( ); ++i )
     {
-        BOOST_CHECK_CLOSE_FRACTION( leftObservationVector( i ), rightObservationVector( i ), std::numeric_limits< double >::epsilon( ) * 10.0 );
+        BOOST_CHECK_CLOSE_FRACTION(
+                leftObservationVector( i ), rightObservationVector( i ), std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 
     const std::vector< double > leftTimes = left.getConcatenatedDoubleObservationTimes( );
@@ -91,7 +91,8 @@ void compareObservationCollections(
         BOOST_REQUIRE_EQUAL( leftObservationTimes.at( i ).size( ), rightObservationTimes.at( i ).size( ) );
         for( unsigned int j = 0; j < leftObservationTimes.at( i ).size( ); ++j )
         {
-            BOOST_CHECK_CLOSE_FRACTION( leftObservationTimes.at( i ).at( j ), rightObservationTimes.at( i ).at( j ),
+            BOOST_CHECK_CLOSE_FRACTION( leftObservationTimes.at( i ).at( j ),
+                                        rightObservationTimes.at( i ).at( j ),
                                         std::numeric_limits< double >::epsilon( ) * 10.0 );
         }
     }
@@ -104,8 +105,8 @@ void compareObservationCollections(
         BOOST_REQUIRE_EQUAL( leftObservationSets.at( i ).size( ), rightObservationSets.at( i ).size( ) );
         for( int j = 0; j < leftObservationSets.at( i ).size( ); ++j )
         {
-            BOOST_CHECK_CLOSE_FRACTION( leftObservationSets.at( i )( j ), rightObservationSets.at( i )( j ),
-                                        std::numeric_limits< double >::epsilon( ) * 10.0 );
+            BOOST_CHECK_CLOSE_FRACTION(
+                    leftObservationSets.at( i )( j ), rightObservationSets.at( i )( j ), std::numeric_limits< double >::epsilon( ) * 10.0 );
         }
     }
 
@@ -123,7 +124,7 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
 
     // Create simple test data
     const ObservableType observableType = one_way_range;
-    
+
     // Create link definition: transmitter -> receiver
     LinkDefinition linkEnds;
     linkEnds[ transmitter ] = std::make_pair< std::string, std::string >( "Transmitter", "" );
@@ -146,12 +147,11 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
 
     // Create the original observation set
     std::shared_ptr< SingleObservationSet< double, double > > originalObservationSet =
-            std::make_shared< SingleObservationSet< double, double > >(
-                observableType,
-                linkEnds,
-                observations,
-                observationTimes,
-                receiver  // reference link end
+            std::make_shared< SingleObservationSet< double, double > >( observableType,
+                                                                        linkEnds,
+                                                                        observations,
+                                                                        observationTimes,
+                                                                        receiver  // reference link end
             );
 
     // Serialize to binary stream
@@ -172,43 +172,29 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
     BOOST_REQUIRE( deserializedObservationSet != nullptr );
 
     // Verify observable type
-    BOOST_CHECK_EQUAL( 
-        deserializedObservationSet->getObservableType( ),
-        originalObservationSet->getObservableType( ) );
+    BOOST_CHECK_EQUAL( deserializedObservationSet->getObservableType( ), originalObservationSet->getObservableType( ) );
 
     // Verify link ends
     LinkDefinition deserializedLinkEnds = deserializedObservationSet->getLinkEnds( );
-    BOOST_CHECK_EQUAL( 
-        deserializedLinkEnds[ transmitter ].getBodyName( ),
-        linkEnds[ transmitter ].getBodyName( ) );
-    BOOST_CHECK_EQUAL( 
-        deserializedLinkEnds[ receiver ].getBodyName( ),
-        linkEnds[ receiver ].getBodyName( ) );
+    BOOST_CHECK_EQUAL( deserializedLinkEnds[ transmitter ].getBodyName( ), linkEnds[ transmitter ].getBodyName( ) );
+    BOOST_CHECK_EQUAL( deserializedLinkEnds[ receiver ].getBodyName( ), linkEnds[ receiver ].getBodyName( ) );
 
     // Verify reference link end
-    BOOST_CHECK_EQUAL( 
-        deserializedObservationSet->getReferenceLinkEnd( ),
-        originalObservationSet->getReferenceLinkEnd( ) );
+    BOOST_CHECK_EQUAL( deserializedObservationSet->getReferenceLinkEnd( ), originalObservationSet->getReferenceLinkEnd( ) );
 
     // Verify number of observations
-    BOOST_CHECK_EQUAL( 
-        deserializedObservationSet->getNumberOfObservables( ),
-        originalObservationSet->getNumberOfObservables( ) );
+    BOOST_CHECK_EQUAL( deserializedObservationSet->getNumberOfObservables( ), originalObservationSet->getNumberOfObservables( ) );
 
     // Verify observation times
     std::vector< double > deserializedTimes = deserializedObservationSet->getObservationTimes( );
     BOOST_CHECK_EQUAL( deserializedTimes.size( ), observationTimes.size( ) );
     for( unsigned int i = 0; i < observationTimes.size( ); i++ )
     {
-        BOOST_CHECK_CLOSE_FRACTION( 
-            deserializedTimes[ i ],
-            observationTimes[ i ],
-            std::numeric_limits< double >::epsilon( ) * 10.0 );
+        BOOST_CHECK_CLOSE_FRACTION( deserializedTimes[ i ], observationTimes[ i ], std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 
     // Verify observation values
-    std::vector< Eigen::Matrix< double, Eigen::Dynamic, 1 > > deserializedObs = 
-        deserializedObservationSet->getObservations( );
+    std::vector< Eigen::Matrix< double, Eigen::Dynamic, 1 > > deserializedObs = deserializedObservationSet->getObservations( );
     BOOST_CHECK_EQUAL( deserializedObs.size( ), observations.size( ) );
     for( unsigned int i = 0; i < observations.size( ); i++ )
     {
@@ -216,10 +202,8 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
         BOOST_CHECK_EQUAL( deserializedObs[ i ].cols( ), observations[ i ].cols( ) );
         for( int j = 0; j < observations[ i ].rows( ); j++ )
         {
-            BOOST_CHECK_CLOSE_FRACTION( 
-                deserializedObs[ i ]( j ),
-                observations[ i ]( j ),
-                std::numeric_limits< double >::epsilon( ) * 10.0 );
+            BOOST_CHECK_CLOSE_FRACTION(
+                    deserializedObs[ i ]( j ), observations[ i ]( j ), std::numeric_limits< double >::epsilon( ) * 10.0 );
         }
     }
 
@@ -229,10 +213,7 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
         Eigen::Matrix< double, Eigen::Dynamic, 1 > originalObs = originalObservationSet->getObservation( i );
         Eigen::Matrix< double, Eigen::Dynamic, 1 > deserializedObs = deserializedObservationSet->getObservation( i );
 
-        BOOST_CHECK_CLOSE_FRACTION( 
-            deserializedObs( 0 ),
-            originalObs( 0 ),
-            std::numeric_limits< double >::epsilon( ) * 10.0 );
+        BOOST_CHECK_CLOSE_FRACTION( deserializedObs( 0 ), originalObs( 0 ), std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 
     // Verify observation time access (functional test)
@@ -241,10 +222,7 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
         double originalTime = originalObservationSet->getObservationTime( i );
         double deserializedTime = deserializedObservationSet->getObservationTime( i );
 
-        BOOST_CHECK_CLOSE_FRACTION( 
-            deserializedTime,
-            originalTime,
-            std::numeric_limits< double >::epsilon( ) * 10.0 );
+        BOOST_CHECK_CLOSE_FRACTION( deserializedTime, originalTime, std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 
     // Verify observations vector conversion
@@ -254,24 +232,16 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerialization )
     BOOST_CHECK_EQUAL( originalObsVector.size( ), deserializedObsVector.size( ) );
     for( int i = 0; i < originalObsVector.size( ); i++ )
     {
-        BOOST_CHECK_CLOSE_FRACTION( 
-            deserializedObsVector( i ),
-            originalObsVector( i ),
-            std::numeric_limits< double >::epsilon( ) * 10.0 );
+        BOOST_CHECK_CLOSE_FRACTION( deserializedObsVector( i ), originalObsVector( i ), std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 
     // Verify time bounds
     std::pair< double, double > originalTimeBounds = originalObservationSet->getTimeBounds( );
     std::pair< double, double > deserializedTimeBounds = deserializedObservationSet->getTimeBounds( );
 
-    BOOST_CHECK_CLOSE_FRACTION( 
-        deserializedTimeBounds.first,
-        originalTimeBounds.first,
-        std::numeric_limits< double >::epsilon( ) * 10.0 );
-    BOOST_CHECK_CLOSE_FRACTION( 
-        deserializedTimeBounds.second,
-        originalTimeBounds.second,
-        std::numeric_limits< double >::epsilon( ) * 10.0 );
+    BOOST_CHECK_CLOSE_FRACTION( deserializedTimeBounds.first, originalTimeBounds.first, std::numeric_limits< double >::epsilon( ) * 10.0 );
+    BOOST_CHECK_CLOSE_FRACTION(
+            deserializedTimeBounds.second, originalTimeBounds.second, std::numeric_limits< double >::epsilon( ) * 10.0 );
 }
 
 //! Test serialization with vector observations (3D)
@@ -282,7 +252,7 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerializationVector )
 
     // Create test data for 3D position observations
     const ObservableType observableType = position_observable;
-    
+
     LinkDefinition linkEnds;
     linkEnds[ transmitter ] = std::make_pair< std::string, std::string >( "Sat", "" );
     linkEnds[ receiver ] = std::make_pair< std::string, std::string >( "Station", "" );
@@ -304,12 +274,7 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerializationVector )
 
     std::shared_ptr< SingleObservationSet< double, double > > originalObservationSet =
             std::make_shared< SingleObservationSet< double, double > >(
-                observableType,
-                linkEnds,
-                observations,
-                observationTimes,
-                receiver
-            );
+                    observableType, linkEnds, observations, observationTimes, receiver );
 
     // Serialize
     std::stringstream stream;
@@ -327,29 +292,21 @@ BOOST_AUTO_TEST_CASE( test_SingleObservationSetSerializationVector )
 
     // Verify
     BOOST_REQUIRE( deserializedObservationSet != nullptr );
-    BOOST_CHECK_EQUAL( 
-        deserializedObservationSet->getSingleObservableSize( ),
-        originalObservationSet->getSingleObservableSize( ) );
-    BOOST_CHECK_EQUAL( 
-        deserializedObservationSet->getTotalObservationSetSize( ),
-        originalObservationSet->getTotalObservationSetSize( ) );
+    BOOST_CHECK_EQUAL( deserializedObservationSet->getSingleObservableSize( ), originalObservationSet->getSingleObservableSize( ) );
+    BOOST_CHECK_EQUAL( deserializedObservationSet->getTotalObservationSetSize( ), originalObservationSet->getTotalObservationSetSize( ) );
 
     // Check individual values
-    std::vector< Eigen::Matrix< double, Eigen::Dynamic, 1 > > deserializedObs = 
-        deserializedObservationSet->getObservations( );
-    
+    std::vector< Eigen::Matrix< double, Eigen::Dynamic, 1 > > deserializedObs = deserializedObservationSet->getObservations( );
+
     for( unsigned int i = 0; i < observations.size( ); i++ )
     {
         for( unsigned int j = 0; j < 3; j++ )
         {
-            BOOST_CHECK_CLOSE_FRACTION( 
-                deserializedObs[ i ]( j ),
-                observations[ i ]( j ),
-                std::numeric_limits< double >::epsilon( ) * 10.0 );
+            BOOST_CHECK_CLOSE_FRACTION(
+                    deserializedObs[ i ]( j ), observations[ i ]( j ), std::numeric_limits< double >::epsilon( ) * 10.0 );
         }
     }
 }
-
 
 BOOST_AUTO_TEST_SUITE( test_ObservationCollection_serialization )
 
@@ -361,8 +318,8 @@ BOOST_AUTO_TEST_CASE( test_ObservationCollectionSerialization )
     const auto firstSet = createSingleObservationSet( one_way_range, "TxA", "RxA", { 0.0, 10.0, 20.0 }, { 1.0, 2.0, 3.0 } );
     const auto secondSet = createSingleObservationSet( one_way_range, "TxB", "RxB", { 5.0, 15.0 }, { 4.0, 5.0 } );
 
-    const std::vector< std::shared_ptr< observation_models::SingleObservationSet< double, double > > > observationSets =
-            { firstSet, secondSet };
+    const std::vector< std::shared_ptr< observation_models::SingleObservationSet< double, double > > > observationSets = { firstSet,
+                                                                                                                           secondSet };
 
     ObservationCollection< double, double > originalCollection( observationSets );
 
@@ -382,7 +339,8 @@ BOOST_AUTO_TEST_CASE( test_ObservationCollectionSerialization )
 
     Eigen::VectorXd originalConcatenatedObservations = originalCollection.getConcatenatedObservations( );
     Eigen::VectorXd originalConcatenatedResiduals = originalCollection.getConcatenatedResiduals( );
-    Eigen::VectorXd newResiduals = Eigen::VectorXd::LinSpaced( originalConcatenatedResiduals.size( ), 10.0, 10.0 + originalConcatenatedResiduals.size( ) - 1 );
+    Eigen::VectorXd newResiduals =
+            Eigen::VectorXd::LinSpaced( originalConcatenatedResiduals.size( ), 10.0, 10.0 + originalConcatenatedResiduals.size( ) - 1 );
 
     originalCollection.setResiduals( newResiduals );
     deserializedCollection.setResiduals( newResiduals );
@@ -403,7 +361,6 @@ BOOST_AUTO_TEST_CASE( test_ObservationCollectionSerialization )
                                     std::numeric_limits< double >::epsilon( ) * 10.0 );
     }
 }
-
 
 BOOST_AUTO_TEST_SUITE_END( )
 
--- tests/test_tudat/src/io/unitTestPropagationResultsSerialization.cpp
+++ tests/test_tudat/src/io/unitTestPropagationResultsSerialization.cpp
@@ -80,8 +80,7 @@ std::shared_ptr< propagators::SingleArcSimulationResults< double, double > > cre
             integratedStateAndBodyList;
 
     auto rawSolutionConversionFunction = []( std::map< double, Eigen::Matrix< double, Eigen::Dynamic, 1 > >& processedSolution,
-                                             const std::map< double, Eigen::Matrix< double, Eigen::Dynamic, 1 > >& rawSolution )
-    {
+                                             const std::map< double, Eigen::Matrix< double, Eigen::Dynamic, 1 > >& rawSolution ) {
         processedSolution = rawSolution;
     };
 
@@ -101,20 +100,17 @@ std::shared_ptr< propagators::SingleArcSimulationResults< double, double > > cre
     return results;
 }
 
-std::shared_ptr< propagators::SingleArcVariationalSimulationResults< double, double > >
-createSingleArcVariationalSimulationResults( )
+std::shared_ptr< propagators::SingleArcVariationalSimulationResults< double, double > > createSingleArcVariationalSimulationResults( )
 {
     auto singleArcResults = createSingleArcSimulationResults( );
-    auto variationalResults = std::make_shared< propagators::SingleArcVariationalSimulationResults< double, double > >(
-            singleArcResults, 2, 1 );
+    auto variationalResults =
+            std::make_shared< propagators::SingleArcVariationalSimulationResults< double, double > >( singleArcResults, 2, 1 );
 
     std::map< double, Eigen::Matrix< double, Eigen::Dynamic, Eigen::Dynamic > > fullSolution;
     Eigen::Matrix< double, Eigen::Dynamic, Eigen::Dynamic > solutionAtT0( 2, 4 );
-    solutionAtT0 << 1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0;
+    solutionAtT0 << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0;
     Eigen::Matrix< double, Eigen::Dynamic, Eigen::Dynamic > solutionAtT1( 2, 4 );
-    solutionAtT1 << 9.0, 10.0, 11.0, 12.0,
-            13.0, 14.0, 15.0, 16.0;
+    solutionAtT1 << 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0;
     fullSolution[ 0.0 ] = solutionAtT0;
     fullSolution[ 10.0 ] = solutionAtT1;
 
@@ -130,12 +126,14 @@ createSingleArcVariationalSimulationResults( )
 std::shared_ptr< propagators::MultiArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >
 createMultiArcSimulationResults( )
 {
-    std::vector< std::shared_ptr< propagators::SingleArcSimulationResults< double, double > > > singleArcResults =
-            { createSingleArcSimulationResults( ), createSingleArcSimulationResults( ) };
+    std::vector< std::shared_ptr< propagators::SingleArcSimulationResults< double, double > > > singleArcResults = {
+        createSingleArcSimulationResults( ), createSingleArcSimulationResults( )
+    };
 
-    std::vector< std::shared_ptr< propagators::SingleArcDependentVariablesInterface< double > > > singleArcInterfaces =
-            { singleArcResults.at( 0 )->getSingleArcDependentVariablesInterface( ),
-              singleArcResults.at( 1 )->getSingleArcDependentVariablesInterface( ) };
+    std::vector< std::shared_ptr< propagators::SingleArcDependentVariablesInterface< double > > > singleArcInterfaces = {
+        singleArcResults.at( 0 )->getSingleArcDependentVariablesInterface( ),
+        singleArcResults.at( 1 )->getSingleArcDependentVariablesInterface( )
+    };
 
     auto dependentVariableInterface = std::make_shared< propagators::MultiArcDependentVariablesInterface< double > >(
             singleArcInterfaces, std::vector< double >( ), std::vector< double >( ) );
@@ -179,11 +177,12 @@ BOOST_AUTO_TEST_SUITE( test_PropagationResults_serialization )
 
 BOOST_AUTO_TEST_CASE( test_PropagationResultsSerialization )
 {
-    std::vector< std::shared_ptr< propagators::SimulationResults< double, double > > > resultsVector =
-            { createSingleArcSimulationResults( ),
-              createSingleArcVariationalSimulationResults( ),
-              createMultiArcSimulationResults( ),
-              createHybridArcSimulationResults( ) };
+    std::vector< std::shared_ptr< propagators::SimulationResults< double, double > > > resultsVector = {
+        createSingleArcSimulationResults( ),
+        createSingleArcVariationalSimulationResults( ),
+        createMultiArcSimulationResults( ),
+        createHybridArcSimulationResults( )
+    };
 
     for( const auto& results : resultsVector )
     {
@@ -199,55 +198,63 @@ BOOST_AUTO_TEST_CASE( test_PropagationResultsSerialization )
                 auto sdes = std::dynamic_pointer_cast< propagators::SingleArcSimulationResults< double, double > >( deserializedResults );
                 if( sdes )
                 {
-                    std::cout << "single: rawSolution size (orig/deser): " << single->getEquationsOfMotionNumericalSolutionRaw().size()
-                              << " / " << sdes->getEquationsOfMotionNumericalSolutionRaw().size() << "\n";
-                    std::cout << "single: dependentVar size (orig/deser): " << single->getDependentVariableHistoryDouble().size()
-                              << " / " << sdes->getDependentVariableHistoryDouble().size() << "\n";
-                    std::cout << "single: compTime size (orig/deser): " << single->getCumulativeComputationTimeHistory().size()
-                              << " / " << sdes->getCumulativeComputationTimeHistory().size() << "\n";
-                    std::cout << "single: funcEval size (orig/deser): " << single->getCumulativeNumberOfFunctionEvaluations().size()
-                              << " / " << sdes->getCumulativeNumberOfFunctionEvaluations().size() << "\n";
+                    std::cout << "single: rawSolution size (orig/deser): " << single->getEquationsOfMotionNumericalSolutionRaw( ).size( )
+                              << " / " << sdes->getEquationsOfMotionNumericalSolutionRaw( ).size( ) << "\n";
+                    std::cout << "single: dependentVar size (orig/deser): " << single->getDependentVariableHistoryDouble( ).size( ) << " / "
+                              << sdes->getDependentVariableHistoryDouble( ).size( ) << "\n";
+                    std::cout << "single: compTime size (orig/deser): " << single->getCumulativeComputationTimeHistory( ).size( ) << " / "
+                              << sdes->getCumulativeComputationTimeHistory( ).size( ) << "\n";
+                    std::cout << "single: funcEval size (orig/deser): " << single->getCumulativeNumberOfFunctionEvaluations( ).size( )
+                              << " / " << sdes->getCumulativeNumberOfFunctionEvaluations( ).size( ) << "\n";
                 }
             }
             // Variational
             if( auto var = std::dynamic_pointer_cast< propagators::SingleArcVariationalSimulationResults< double, double > >( results ) )
             {
-                auto vdes = std::dynamic_pointer_cast< propagators::SingleArcVariationalSimulationResults< double, double > >( deserializedResults );
+                auto vdes = std::dynamic_pointer_cast< propagators::SingleArcVariationalSimulationResults< double, double > >(
+                        deserializedResults );
                 if( vdes )
                 {
-                    std::cout << "variational: stateTrans size (orig/deser): " << var->getStateTransitionSolution().size()
-                              << " / " << vdes->getStateTransitionSolution().size() << "\n";
-                    std::cout << "variational: sens size (orig/deser): " << var->getSensitivitySolution().size()
-                              << " / " << vdes->getSensitivitySolution().size() << "\n";
-                    auto dyn = var->getDynamicsResults();
-                    auto ddyn = vdes->getDynamicsResults();
+                    std::cout << "variational: stateTrans size (orig/deser): " << var->getStateTransitionSolution( ).size( ) << " / "
+                              << vdes->getStateTransitionSolution( ).size( ) << "\n";
+                    std::cout << "variational: sens size (orig/deser): " << var->getSensitivitySolution( ).size( ) << " / "
+                              << vdes->getSensitivitySolution( ).size( ) << "\n";
+                    auto dyn = var->getDynamicsResults( );
+                    auto ddyn = vdes->getDynamicsResults( );
                     if( dyn && ddyn )
                     {
-                        std::cout << "variational->dynamics: rawSolution size (orig/deser): " << dyn->getEquationsOfMotionNumericalSolutionRaw().size()
-                                  << " / " << ddyn->getEquationsOfMotionNumericalSolutionRaw().size() << "\n";
+                        std::cout << "variational->dynamics: rawSolution size (orig/deser): "
+                                  << dyn->getEquationsOfMotionNumericalSolutionRaw( ).size( ) << " / "
+                                  << ddyn->getEquationsOfMotionNumericalSolutionRaw( ).size( ) << "\n";
                     }
                 }
             }
             // Multi-arc
-            if( auto multi = std::dynamic_pointer_cast< propagators::MultiArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( results ) )
+            if( auto multi = std::dynamic_pointer_cast<
+                        propagators::MultiArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( results ) )
             {
-                auto mdes = std::dynamic_pointer_cast< propagators::MultiArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( deserializedResults );
+                auto mdes = std::dynamic_pointer_cast<
+                        propagators::MultiArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >(
+                        deserializedResults );
                 if( mdes )
                 {
-                    std::cout << "multi: single-arc count (orig/deser): " << multi->getSingleArcResults().size()
-                              << " / " << mdes->getSingleArcResults().size() << "\n";
+                    std::cout << "multi: single-arc count (orig/deser): " << multi->getSingleArcResults( ).size( ) << " / "
+                              << mdes->getSingleArcResults( ).size( ) << "\n";
                 }
             }
             // Hybrid
-            if( auto hyb = std::dynamic_pointer_cast< propagators::HybridArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( results ) )
+            if( auto hyb = std::dynamic_pointer_cast<
+                        propagators::HybridArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( results ) )
             {
-                auto hdes = std::dynamic_pointer_cast< propagators::HybridArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >( deserializedResults );
+                auto hdes = std::dynamic_pointer_cast<
+                        propagators::HybridArcSimulationResults< propagators::SingleArcSimulationResults, double, double > >(
+                        deserializedResults );
                 if( hdes )
                 {
-                    auto s = hyb->getSingleArcResults();
-                    auto sd = hdes->getSingleArcResults();
-                    std::cout << "hybrid: single raw size (orig/deser): " << s->getEquationsOfMotionNumericalSolutionRaw().size()
-                              << " / " << sd->getEquationsOfMotionNumericalSolutionRaw().size() << "\n";
+                    auto s = hyb->getSingleArcResults( );
+                    auto sd = hdes->getSingleArcResults( );
+                    std::cout << "hybrid: single raw size (orig/deser): " << s->getEquationsOfMotionNumericalSolutionRaw( ).size( ) << " / "
+                              << sd->getEquationsOfMotionNumericalSolutionRaw( ).size( ) << "\n";
                 }
             }
         }
