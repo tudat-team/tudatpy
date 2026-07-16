6ce0ecda2fe6539dbb2b9604dc4bc9b24372b79d
PascalChristiaanse <pascal.christiaanse@gmail.com>
2026-07-16 11:41:04 +0200

    undo incorrect template instantiation fix

--- tests/test_tudat/src/astro/orbit_determination/executePlanetaryParameterEstimationTestCase.cpp
+++ tests/test_tudat/src/astro/orbit_determination/executePlanetaryParameterEstimationTestCase.cpp
@@ -16,9 +16,8 @@ namespace tudat
 namespace unit_tests
 {
 
-// Explicit instantiation removed to avoid conflict with PCH
-// The template is instantiated in the PCH via orbitDeterminationManager.h
-#if 0
+#if TUDAT_BUILD_EXPLICIT_INSTANTIATIONS
+
 template std::pair< std::shared_ptr< simulation_setup::EstimationOutput< double, double > >, Eigen::VectorXd >
 executePlanetaryParameterEstimation< double, double >( const int observableType,
                                                        Eigen::VectorXd parameterPerturbation,
