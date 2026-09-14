9f5ee4f30de9e9ecd77eb9523e156f57cdeba3a9
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-09-14 20:12:47 +0200

    Update Doppler transponder delay expectations

--- tests/test_tudat/src/astro/observation_models/unitTestDopplerModels.cpp
+++ tests/test_tudat/src/astro/observation_models/unitTestDopplerModels.cpp
@@ -714,7 +714,7 @@ BOOST_AUTO_TEST_CASE( testTwoWayDopplerVehicleSystemTransponderDelay )
 
     observationModel->computeIdealObservationsWithLinkEndData(
             observationTime, receiver, linkEndTimes, linkEndStates, getNWayRangeAncillarySettings( { ancillaryDelay } ) );
-    BOOST_CHECK_SMALL( std::fabs( linkEndTimes.at( 2 ) - linkEndTimes.at( 1 ) - ancillaryDelay ),
+    BOOST_CHECK_SMALL( std::fabs( linkEndTimes.at( 2 ) - linkEndTimes.at( 1 ) - vehicleSystemDelay ),
                        observationTime * std::numeric_limits< double >::epsilon( ) );
 }
 
