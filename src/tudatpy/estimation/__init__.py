21832aef5c1c15687670e2aaa005fe1092db59de
Dominic Dirkx <d.dirkx@tudelft.nl>
2026-09-22 12:37:09 +0200

    Fix estimation stub generation

--- src/tudatpy/estimation/__init__.py
+++ src/tudatpy/estimation/__init__.py
@@ -1,7 +1,4 @@
 from tudatpy.kernel.estimation import *
 
 # Select the Python package so fresh parent imports retain its legacy wrapper.
-from importlib import import_module as _import_module
-
-observations_setup = _import_module("tudatpy.estimation.observations_setup")
-del _import_module
+import tudatpy.estimation.observations_setup as observations_setup
